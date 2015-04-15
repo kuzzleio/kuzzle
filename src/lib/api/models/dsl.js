b8d13083b38deffcf8813d9dbcbdd4bdbd82ef2d
Anthony Sendra <asendra@kaliop.com>
2015-04-15 11:29:30 +0200

    init dsl file with basic filter functions

---
+++ src/lib/api/models/dsl.js
@@ -0,0 +1,56 @@
+var
+  _ = require('lodash');
+
+module.export = {
+
+  // Returns true only if the value in field has at least one non-null value
+  exists: function () {
+
+  },
+
+  // Return true only if the point in field is in the bouding box
+  geoBoundingBox: function () {
+
+  },
+
+  // Return true only if the point in field is in a specific distance from a geo point
+  geoDistance: function () {
+
+  },
+
+  // Return true only if the point in field is in a range from a specific point
+  geoDistanceRange: function () {
+
+  },
+
+  // Return true only if the point in field is in a polygon of points
+  geoPolygon: function () {
+
+  },
+
+  // Return true only if the point in field is in the square
+  geoShape: function() {
+
+  },
+
+  // Return true only if the value in field is in a certain range
+  range: function () {
+
+  },
+
+  // Return true only if the value in field pass the regexp test
+  regexp: function () {
+
+  },
+
+  // Return true only if the value in field match the provided term
+  term: function () {
+
+  },
+
+  // Return true only if the value in field match on any (configurable) of the provided terms
+  terms: function () {
+
+  }
+
+};
