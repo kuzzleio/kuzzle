d47d6fd5ff34e890911822feab8bd0c9c003b2c8
ballinette <gballini@kaliop.com>
2015-05-11 20:15:01 +0200

    Improve feedback messages : just notify the caller after broadcasting the message

--- src/lib/api/controllers/routerController.js
+++ src/lib/api/controllers/routerController.js
@@ -48,7 +48,7 @@ module.exports = function RouterController (kuzzle) {
             // Send response and close connection
             if (result.rooms) {
               async.each(result.rooms, function (roomName) {
-                routerCtrl.notify(roomName, result.data, result.connections);
+                routerCtrl.notify(roomName, result.data);
               });
             }
             response.writeHead(200, {'Content-Type': 'application/json'});
@@ -90,9 +90,10 @@ module.exports = function RouterController (kuzzle) {
           .then(function onExecuteSuccess (result) {
             if (result.rooms) {
               async.each(result.rooms, function (roomName) {
-                routerCtrl.notify(roomName, result.data, result.connections);
+                routerCtrl.notify(roomName, result.data);
               });
             }
+            routerCtrl.notify(data.requestId, result.data, connection);
           })
           .catch(function onExecuteError(error) {
             routerCtrl.notify(data.requestId, {error: error}, socket);
@@ -143,9 +144,10 @@ module.exports = function RouterController (kuzzle) {
           .then(function onExecuteSuccess (result) {
             if (result.rooms) {
               async.each(result.rooms, function (roomName) {
-                routerCtrl.notify(roomName, result.data, result.connections);
+                routerCtrl.notify(roomName, result.data);
               });
             }
+            routerCtrl.notify(data.requestId, result.data, connection);
           })
           .catch(function onExecuteError(error) {
             routerCtrl.notify(data.requestId, {error: error});
@@ -164,21 +166,19 @@ module.exports = function RouterController (kuzzle) {
    * @param {Object} data
    * @param {Object} connections
    */
-  this.notify = function (room, data, connections) {
-    if (connections) {
-      async.each(connections, function (connection) {
-        switch (connection.type) {
-          case 'websocket':
-            kuzzle.io.to(connection.id).emit(room, data);
-            break;
-          case 'amq':
-            broker.replyTo(connection.id, data);
-            break;
-          case "mqtt":
-            broker.addExchange(connection.id, data);
-            break;
-        }
-      });
+  this.notify = function (room, data, connection) {
+    if (connection) {
+      switch (connection.type) {
+        case 'websocket':
+          kuzzle.io.to(connection.id).emit(room, data);
+          break;
+        case 'amq':
+          broker.replyTo(connection.id, data);
+          break;
+        case "mqtt":
+          broker.addExchange(connection.id, data);
+          break;
+      }
     }
     else {
       kuzzle.io.emit(room, data);
