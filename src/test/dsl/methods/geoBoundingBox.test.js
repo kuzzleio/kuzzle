ffad40f49c8fbc49d984f0cd67c5e66c6f1d44f8
Anthony Sendra <asendra@kaliop.com>
2015-05-28 17:52:03 +0200

    unit test for geoboundingbox method

--- src/test/dsl/methods/geoBoundingBox.test.js
+++ src/test/dsl/methods/geoBoundingBox.test.js
@@ -10,51 +10,47 @@ describe('Test not method', function () {
     documentGrace = {
       firstName: 'Grace',
       lastName: 'Hopper',
-      location: {
-        lat: 32.692742,
-        lon: -97.114127
-      }
+      'location.lat': 32.692742, // we can't test with nested document here
+      'location.lon': -97.114127
     },
     documentAda = {
       firstName: 'Ada',
       lastName: 'Lovelace',
-      location: {
-        lat: 51.519291,
-        lon: -0.149817
-      }
+      'location.lat': 51.519291,
+      'location.lon': -0.149817
     },
 
     // Test all supported formats
     filterEngland = {
       location: {
-        top: 52.394484,
-        left: -2.939744,
-        bottom: 51.143628,
-        right: 1.180129
+        top: -2.939744,
+        left: 52.394484,
+        bottom: 1.180129,
+        right: 51.143628
       }
     },
     filterEngland2 = {
       location: {
         'top_left': {
-          lat: -2.939744,
-          lon: 52.394484
+          lat: 52.394484,
+          lon: -2.939744
         },
         'bottom_right': {
-          lat: 1.180129,
-          lon: 51.143628
+          lat: 51.143628,
+          lon: 1.180129
         }
       }
     },
     filterEngland3 = {
       location: {
-        'top_left': [52.394484, -2.939744],
-        'bottom_right': [51.143628, 1.180129]
+        'top_left': [ -2.939744, 52.394484 ],
+        'bottom_right': [ 1.180129, 51.143628 ]
       }
     },
     filterUSA = {
       location: {
-        'top_left': '48.478867, -125.074754',
-        'bottom_right': '24.874640, -62.980026'
+        'top_left': '-125.074754, 48.478867',
+        'bottom_right': '-62.980026, 24.874640'
       }
     },
     filterUSA2 = {
@@ -92,21 +88,22 @@ describe('Test not method', function () {
     // Coord are geoashed for build the curried function name
     // because we have many times the same coord in filters,
     // we must have only three functions (one for filterEngland, and two for filterUSA)
+
     should(Object.keys(methods.dsl.filtersTree[collection].location)).have.length(3);
-    should(methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxmpmr22b6xt0hqeycds).not.be.empty;
-    should(methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxj042p0j0phsc9wnc4v).not.be.empty;
+    should(methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxgcmfj457fu10ffy7m4).not.be.empty;
+    should(methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxc0x5cc73fds7jwb737).not.be.empty;
     should(methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxc0x5c7zzzds7jw7zzz).not.be.empty;
   });
 
   it('should construct the filterTree with correct room list', function () {
     var rooms;
 
-    rooms = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxmpmr22b6xt0hqeycds.rooms;
+    rooms = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxgcmfj457fu10ffy7m4.rooms;
     should(rooms).be.an.Array;
     should(rooms).have.length(1);
     should(rooms[0]).be.exactly(roomId);
 
-    rooms = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxj042p0j0phsc9wnc4v.rooms;
+    rooms = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxc0x5cc73fds7jwb737.rooms;
     should(rooms).be.an.Array;
     should(rooms).have.length(1);
     should(rooms[0]).be.exactly(roomId);
@@ -120,16 +117,16 @@ describe('Test not method', function () {
   it('should construct the filterTree with correct functions geoboundingbox', function () {
     var result;
 
-    // test filterEngland
-    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxmpmr22b6xt0hqeycds.fn(documentGrace);
+   // test filterEngland
+    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxgcmfj457fu10ffy7m4.fn(documentGrace);
     should(result).be.exactly(false);
-    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxmpmr22b6xt0hqeycds.fn(documentAda);
+    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxgcmfj457fu10ffy7m4.fn(documentAda);
     should(result).be.exactly(true);
 
     // test filterUSA
-    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxj042p0j0phsc9wnc4v.fn(documentGrace);
+    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxc0x5cc73fds7jwb737.fn(documentGrace);
     should(result).be.exactly(true);
-    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxj042p0j0phsc9wnc4v.fn(documentAda);
+    result = methods.dsl.filtersTree[collection].location.locationgeoBoundingBoxc0x5cc73fds7jwb737.fn(documentAda);
     should(result).be.exactly(false);
 
     // test filterUSA2
