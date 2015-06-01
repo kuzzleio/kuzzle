ddf8a645f560f0470ad48d0e91dd5b30e7ab2074
Anthony Sendra <asendra@kaliop.com>
2015-06-01 15:33:58 +0200

    unit test execute function in funnelController

--- src/test/controllers/funnelController/execute.test.js
+++ src/test/controllers/funnelController/execute.test.js
@@ -4,20 +4,10 @@ var
 
 require('should-promised');
 
-describe('Test addSubscription function in hotelClerk controller', function () {
+describe('Test execute function in funnel controller', function () {
 
   var
-    kuzzle,
-    roomId,
-    connection = {id: 'connectionid'},
-    roomName = 'roomName',
-    collection = 'user',
-    filter = {
-      term: {
-        firstName: 'Ada'
-      }
-    };
-
+    kuzzle;
 
   beforeEach(function () {
     kuzzle = {
@@ -32,49 +22,52 @@ describe('Test addSubscription function in hotelClerk controller', function () {
     kuzzle.start({}, {workers: false, servers: false});
   });
 
-  it('should has object filtersTree, customers and rooms empty', function () {
-    should(kuzzle.dsl.filtersTree).be.an.object;
-    should(kuzzle.dsl.filtersTree).be.empty;
+  it('should reject an error when the object doesn\'t contain the controller', function () {
+    var object = {
+      action: 'create'
+    };
 
-    should(kuzzle.dsl.rooms).be.an.object;
-    should(kuzzle.dsl.rooms).be.empty;
-
-    should(kuzzle.dsl.customers).be.an.object;
-    should(kuzzle.dsl.customers).be.empty;
+    return should(kuzzle.funnel.execute(object, {id: 'connectionid'})).be.rejected;
   });
 
-  it('should has the new room and customer', function () {
-    return kuzzle.hotelClerk.addSubscription(connection, roomName, collection, filter)
-      .then(function (result) {
-        should(kuzzle.dsl.filtersTree).be.an.object;
-        should(kuzzle.dsl.filtersTree).not.be.empty;
+  it('should reject an error when the object doesn\'t contain the action', function () {
+    var object = {
+      controller: 'write'
+    };
 
-        should(kuzzle.hotelClerk.rooms).be.an.object;
-        should(kuzzle.hotelClerk.rooms).not.be.empty;
-
-        should(kuzzle.hotelClerk.customers).be.an.object;
-        should(kuzzle.hotelClerk.customers).not.be.empty;
-
-        should(result).be.an.object;
-        should(result.data).be.a.String;
-        should(result).not.be.an.object;
-        should(kuzzle.hotelClerk.rooms[result.data]).be.an.object;
-        should(kuzzle.hotelClerk.rooms[result.data]).not.be.empty;
-
-        roomId = kuzzle.hotelClerk.rooms[result.data].id;
-
-        should(kuzzle.hotelClerk.customers[connection.id]).be.an.object;
-        should(kuzzle.hotelClerk.customers[connection.id]).not.be.empty;
-        should(kuzzle.hotelClerk.customers[connection.id][roomName]).be.exactly(roomId);
-      });
+    return should(kuzzle.funnel.execute(object, {id: 'connectionid'})).be.rejected;
   });
 
-  it('should return an error when the user has already subscribe to the filter', function () {
-    return kuzzle.hotelClerk.addSubscription(connection, roomName, collection, filter)
-      .then(function () {
-        return should(kuzzle.hotelClerk.addSubscription(connection, roomName, collection, filter)).be.rejected;
-      });
+  it('should reject an error when the controller doesn\'t exist', function () {
+    var object = {
+      controller: 'toto',
+      action: 'create'
+    };
+
+    return should(kuzzle.funnel.execute(object, {id: 'connectionid'})).be.rejected;
   });
 
+  it('should reject an error when the action doesn\'t exist', function () {
+    var object = {
+      controller: 'write',
+      action: 'toto'
+    };
+
+    return should(kuzzle.funnel.execute(object, {id: 'connectionid'})).be.rejected;
+  });
+
+  it('should resolve something when everything is ok', function () {
+    var object = {
+      requestId: 'requestId',
+      controller: 'write',
+      action: 'create',
+      collection: 'user',
+      content: {
+        firstName: 'Grace'
+      }
+    };
+
+    return should(kuzzle.funnel.execute(object, {id: 'connectionid'})).not.be.rejected;
+  });
 
 });
