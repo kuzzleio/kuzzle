a9d19368ea7470473e021fa44c326250335ec15c
Anthony Sendra <asendra@kaliop.com>
2015-10-02 09:35:07 +0200

    add tests

---
+++ test/api/core/pluginsManager.test.js
@@ -0,0 +1,135 @@
+var
+  should = require('should'),
+  PluginsManager = require('root-require')('lib/api/core/pluginsManager'),
+  EventEmitter = require('eventemitter2').EventEmitter2;
+
+describe('Test plugins manager', function () {
+
+  it('should do nothing on run if plugin is not activated', function () {
+    var
+      pluginsManager = new PluginsManager ({}),
+      isInitialized = false;
+
+    pluginsManager.plugins = [{
+      object: {
+        init: function () {
+          isInitialized = true;
+        }
+      },
+      activated: false
+    }];
+
+    pluginsManager.run();
+    should(isInitialized).be.false();
+  });
+
+  it('should attach event hook on kuzzle object', function (done) {
+    var
+      kuzzle = new EventEmitter({
+        wildcard: true,
+        maxListeners: 30,
+        delimiter: ':'
+      }),
+      pluginsManager = new PluginsManager (kuzzle);
+
+    pluginsManager.plugins = [{
+      object: {
+        init: function () {},
+        hooks: {
+          'test': 'myFunc'
+        },
+        myFunc: function () {
+          done();
+        }
+      },
+      activated: true
+    }];
+
+    pluginsManager.run();
+    kuzzle.emit('test');
+  });
+
+  it('should attach event hook with wildcard on kuzzle object', function (done) {
+    var
+      kuzzle = new EventEmitter({
+        wildcard: true,
+        maxListeners: 30,
+        delimiter: ':'
+      }),
+      pluginsManager = new PluginsManager (kuzzle);
+
+    pluginsManager.plugins = [{
+      object: {
+        init: function () {},
+        hooks: {
+          'foo:*': 'myFunc'
+        },
+        myFunc: function () {
+          done();
+        }
+      },
+      activated: true
+    }];
+
+    pluginsManager.run();
+    kuzzle.emit('foo:bar');
+  });
+
+  it('should trigger hooks event', function (done) {
+    var
+      kuzzle = new EventEmitter({
+        wildcard: true,
+        maxListeners: 30,
+        delimiter: ':'
+      }),
+      pluginsManager = new PluginsManager (kuzzle);
+
+    pluginsManager.plugins = [{
+      object: {
+        init: function () {},
+        hooks: {
+          'foo:*': 'myFunc'
+        },
+        myFunc: function () {
+          done();
+        }
+      },
+      activated: true
+    }];
+
+    pluginsManager.run();
+    pluginsManager.trigger('foo:bar');
+  });
+
+  it('should attach pipes event', function (done) {
+    var
+      kuzzle = new EventEmitter({
+        wildcard: true,
+        maxListeners: 30,
+        delimiter: ':'
+      }),
+      pluginsManager = new PluginsManager (kuzzle),
+      isCalled = false;
+
+    pluginsManager.plugins = [{
+      object: {
+        init: function () {},
+        pipes: {
+          'foo': 'myFunc'
+        },
+        myFunc: function (callback) {
+          isCalled = true;
+          callback();
+        }
+      },
+      activated: true
+    }];
+
+    pluginsManager.run();
+    pluginsManager.trigger('foo')
+      .then(function () {
+        should(isCalled).be.true();
+        done();
+      });
+  });
+});
