b13a54d7611c95d9afb9bdc02ecb87a1d790a2c4
Anthony Sendra <asendra@kaliop.com>
2015-10-02 12:12:49 +0200

    end unit test plugins manager

--- test/api/core/pluginsManager/constructList.test.js
+++ test/api/core/pluginsManager/constructList.test.js
@@ -1,12 +1,162 @@
 var
   should = require('should'),
   rewire = require('rewire'),
-  PluginsManager = rewire('../../../lib/api/core/pluginsManager'),
-  EventEmitter = require('eventemitter2').EventEmitter2;
+  PluginsManager = rewire('../../../../lib/api/core/pluginsManager');
 
-describe('Test plugins manager installation', function () {
+describe('Plugins manager constructList', function () {
 
-  it('should trigger hooks event', function (done) {
+  var constructList;
+
+  before(function () {
+    constructList = PluginsManager.__get__('constructList');
   });
 
+  it('should return an empty array if both plugins are empty', function () {
+    should(constructList({}, {})).empty().Object();
+  });
+
+  it('should merge default plugins and custom plugins', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'bar'
+        }
+      },
+      defaultPlugins = {
+        bar: {
+          url: 'qux'
+        }
+      },
+      mergedPlugins;
+
+    mergedPlugins = constructList(defaultPlugins, customPlugins, true);
+    should(mergedPlugins.foo).be.Object();
+    should(mergedPlugins.bar).be.Object();
+  });
+
+  it('should copy the default configuration into configuration', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'foo',
+          defaultConfig: {
+            bar: 'bar'
+          }
+        }
+      },
+      defaultPlugins = {
+        bar: {
+          url: 'bar',
+          defaultConfig: {
+            qux: 'qux'
+          }
+        }
+      },
+      mergedPlugins;
+
+    mergedPlugins = constructList(defaultPlugins, customPlugins, true);
+
+    should(mergedPlugins.foo.config).be.Object();
+    should(mergedPlugins.foo.config).have.property('bar');
+
+    should(mergedPlugins.bar.config).be.Object();
+    should(mergedPlugins.bar.config).have.property('qux');
+  });
+
+  it('should add the custom configuration from custom plugins', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'foo',
+          defaultConfig: {
+            bar: 'bar'
+          }
+        },
+        bar: {
+          url: 'bar',
+          defaultConfig: {
+            qux: 'qux'
+          },
+          customConfig: {
+            qux2: 'qux2'
+          }
+        }
+      },
+      defaultPlugins = {
+        bar: {
+          url: 'bar',
+          defaultConfig: {
+            qux: 'qux'
+          }
+        }
+      },
+      mergedPlugins;
+
+    mergedPlugins = constructList(defaultPlugins, customPlugins, true);
+
+    should(mergedPlugins.foo.config).be.Object();
+    should(mergedPlugins.foo.config).have.property('bar');
+
+    should(mergedPlugins.bar.config).be.Object();
+    should(mergedPlugins.bar.config).have.property('qux');
+    should(mergedPlugins.bar.config).have.property('qux2');
+  });
+
+  it('should override the default configuration by the custom configuration from custom plugins', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'foo',
+          defaultConfig: {
+            bar: 'bar'
+          }
+        },
+        bar: {
+          url: 'bar',
+          defaultConfig: {
+            qux: 'qux'
+          },
+          customConfig: {
+            qux: 'qux2'
+          }
+        }
+      },
+      defaultPlugins = {
+        bar: {
+          url: 'bar',
+          defaultConfig: {
+            qux: 'qux'
+          }
+        }
+      },
+      mergedPlugins;
+
+    mergedPlugins = constructList(defaultPlugins, customPlugins, true);
+
+    should(mergedPlugins.foo.config).be.Object();
+    should(mergedPlugins.foo.config).have.property('bar');
+
+    should(mergedPlugins.bar.config).be.Object();
+    should(mergedPlugins.bar.config).have.property('qux');
+    should(mergedPlugins.bar.config.qux).be.exactly('qux2');
+  });
+
+  it('should add custom config directly if there is no default config', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'foo',
+          customConfig: {
+            bar: 'bar'
+          }
+        }
+      },
+      defaultPlugins = {},
+      mergedPlugins;
+
+    mergedPlugins = constructList(defaultPlugins, customPlugins, true);
+
+    should(mergedPlugins.foo.config).be.Object();
+    should(mergedPlugins.foo.config).have.property('bar');
+  });
 });
--- test/api/core/pluginsManager/installPlugins.test.js
+++ test/api/core/pluginsManager/installPlugins.test.js
@@ -1,8 +1,7 @@
 var
   should = require('should'),
   rewire = require('rewire'),
-  PluginsManager = rewire('../../../../lib/api/core/pluginsManager'),
-  EventEmitter = require('eventemitter2').EventEmitter2;
+  PluginsManager = rewire('../../../../lib/api/core/pluginsManager');
 
 describe('Test plugins manager installation', function () {
 
@@ -11,7 +10,7 @@ describe('Test plugins manager installation', function () {
   before(function () {
     PluginsManager.__set__('initConfig', function () {});
     PluginsManager.__set__('npmInstall', function () {});
-    PluginsManager.__set__('kuzzle', {log: {info: function () {}}});
+    PluginsManager.__set__('kuzzle', {log: {info: function () {}, error: function () {}}});
     installPlugins = PluginsManager.__get__('installPlugins');
   });
 
@@ -20,39 +19,114 @@ describe('Test plugins manager installation', function () {
   });
 
   it('should return true if there is plugin to install with url', function () {
-    var plugins = [
-      {
-        url: 'foo',
-        name: 'bar'
+    var plugins = {
+      foo: {
+        url: 'bar'
       }
-    ];
+    };
 
     PluginsManager.__set__('needInstall', function () {return true;});
     should(installPlugins(plugins)).be.true();
   });
 
   it('should return true if there is plugin to install with name and version', function () {
-    var plugins = [
-      {
-        version: 3,
-        name: 'bar'
+    var plugins = {
+      foo: {
+        version: 3
       }
-    ];
+    };
 
     PluginsManager.__set__('needInstall', function () {return true;});
     should(installPlugins(plugins)).be.true();
   });
 
-  it('should return true if there is plugin but already installed', function () {
-    var plugins = [
-      {
-        url: 'foo',
-        name: 'bar'
+  it('should return false if there is plugin but without url and version', function () {
+    var plugins = {
+      foo: {
+        bar: 'qux'
       }
-    ];
+    };
+
+    PluginsManager.__set__('needInstall', function () {return true;});
+    should(installPlugins(plugins)).be.false();
+  });
+
+  it('should return false if there is plugin but already installed', function () {
+    var plugins = {
+      foo: {
+        url: 'bar'
+      }
+    };
 
     PluginsManager.__set__('needInstall', function () {return false;});
     should(installPlugins(plugins)).be.false();
   });
 
+  it('should set activated in the plugin to false', function () {
+    var plugins = {
+      foo: {
+        url: 'bar'
+      }
+    };
+
+    PluginsManager.__set__('needInstall', function () {return true;});
+    installPlugins(plugins);
+
+    should(plugins.foo.activated).be.false();
+  });
+
+  it('should let the plugin activated', function () {
+    var plugins = {
+      foo: {
+        url: 'bar',
+        activated: true
+      }
+    };
+
+    PluginsManager.__set__('needInstall', function () {return true;});
+    installPlugins(plugins);
+
+    should(plugins.foo.activated).be.true();
+  });
+
+  it('should copy the default plugin activation if there is no "activated" attribute', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'bar'
+        }
+      },
+      defaultPlugins = {
+        foo: {
+          url: 'bar',
+          activated: true
+        }
+      };
+
+    PluginsManager.__set__('needInstall', function () {return true;});
+    installPlugins(customPlugins, defaultPlugins);
+
+    should(customPlugins.foo.activated).be.true();
+  });
+
+  it('should not copy the default plugin activation if there is "activated" attribute', function () {
+    var
+      customPlugins = {
+        foo: {
+          url: 'bar',
+          activated: false
+        }
+      },
+      defaultPlugins = {
+        foo: {
+          url: 'bar',
+          activated: true
+        }
+      };
+
+    PluginsManager.__set__('needInstall', function () {return true;});
+    installPlugins(customPlugins, defaultPlugins);
+
+    should(customPlugins.foo.activated).be.false();
+  });
 });
