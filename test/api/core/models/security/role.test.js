8c3eb5364614669261d2bd81c3ba111203ef8f8b
Benoît Vidis <bvidis@kaliop.com>
2015-11-19 11:36:17 +0100

    authorization - add right functions

--- test/api/core/models/security/role.test.js
+++ test/api/core/models/security/role.test.js
@@ -1,5 +1,7 @@
 var
   should = require('should'),
+  BadRequestError = require.main.require('lib/api/core/errors/badRequestError'),
+  InternalError = require.main.require('lib/api/core/errors/internalError'),
   Role = require.main.require('lib/api/core/models/security/role');
 
 describe('Test: security/roleTest', function () {
@@ -166,4 +168,79 @@ describe('Test: security/roleTest', function () {
     should(role.isActionAllowed(requestObject, context)).be.false();
   });
 
+  it('should throw an error if the rights configuration is not either a boolean or a function', function () {
+    var role = new Role();
+    role.indexes = {
+      '*': {
+        collections: {
+          '*': {
+            controllers: {
+              '*': {
+                actions: {
+                  '*': {an: 'object'}
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+
+    should(function () { role.isActionAllowed(requestObject, context); }).throw(InternalError);
+  });
+
+  it('should throw an error if an invalid function is given', function () {
+    var role = new Role();
+    role.indexes = {
+      '*': {
+        collections: {
+          '*': {
+            controllers: {
+              '*': {
+                actions: {
+                  '*': '(some invalid code'
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+
+    should(function () { role.isActionAllowed(requestObject, context); }).throw(InternalError);
+  });
+
+  it('should handle a custom right function', function () {
+    var
+      role = new Role(),
+      noMatchRequestObject = {
+        collection: 'collection',
+        controller: 'controller',
+        action: 'noaction'
+      };
+
+    role.indexes = {
+      '*': {
+        collections: {
+          '*': {
+            controllers: {
+              '*': {
+                actions: {
+                  '*': 'return requestObject.action === \'action\'; '
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+
+    should(role.isActionAllowed(requestObject, context)).be.true();
+    should(role.isActionAllowed(noMatchRequestObject, context)).be.false();
+
+    role.closures = {};
+    role.indexes['*'].collections['*'].controllers['*'].actions['*'] = 'return requestObject.action !== \'action\'; ';
+    should(role.isActionAllowed(requestObject, context)).be.false();
+
+  });
 });
