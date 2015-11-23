26f6c583e3e18bd197dbe72b814120d6969be800
Jean-François Vial <jvial@kaliop.com>
2015-11-23 19:47:46 +0100

    fix unimplemented methods test

--- test/api/dsl/methods/unimplemented.test.js
+++ test/api/dsl/methods/unimplemented.test.js
@@ -7,12 +7,9 @@ var
 require('should-promised');
 
 describe('Test unimplemented methods', function () {
-  it('geoDistance call should return a rejected promise', function () {
-    should(methods.geoDistance()).be.rejectedWith(KuzzleError, {message: 'geoDistance is not implemented yet.'});
+  it('geoDistanceRange call should return a rejected promise', function () {
+    should(methods.geoDistanceRange()).be.rejectedWith(KuzzleError, {message: 'geoDistanceRange is not implemented yet.'});
   });
-  // it('geoDistanceRange call should return a rejected promise', function () {
-  //   should(methods.geoDistanceRange()).be.rejectedWith(KuzzleError, {message: 'geoDistanceRange is not implemented yet.'});
-  // });
   it('geoPolygon call should return a rejected promise', function () {
     should(methods.geoPolygon()).be.rejectedWith(KuzzleError, {message: 'geoPolygon is not implemented yet.'});
   });
