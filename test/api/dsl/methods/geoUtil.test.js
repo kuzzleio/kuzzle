6250f1f4321d1a8e4ebeb480cdd73ed48a6a52d3
Jean-François Vial <jvial@kaliop.com>
2015-11-24 12:02:29 +0100

    use rewire to test geoUtil functions

--- test/api/dsl/methods/geoUtil.test.js
+++ test/api/dsl/methods/geoUtil.test.js
@@ -12,43 +12,43 @@ describe('Test geoUtil methods included in the DSL methods file', function () {
     lon = 40.73;
 
   it('Point: form { lat: -74.1, lon: 40.73 }', function () {
-    result = methods._geoUtil.constructPoint({ lat: -74.1, lon: 40.73 });
+    result = methods.__get__('geoUtil').constructPoint({ lat: -74.1, lon: 40.73 });
     should(result.lat).be.exactly(lat);
     should(result.lon).be.exactly(lon);
   });
 
   it('Point: form { latLon: { lat: -74.1, lon: 40.73 } }', function () {
-    result = methods._geoUtil.constructPoint({ latLon: { lat: -74.1, lon: 40.73 }});
+    result = methods.__get__('geoUtil').constructPoint({ latLon: { lat: -74.1, lon: 40.73 }});
     should(result.lat).be.exactly(lat);
     should(result.lon).be.exactly(lon);
   });
 
   it('Point: form { latLon: [ -74.1, 40.73 ] }', function () {
-    result = methods._geoUtil.constructPoint({ latLon: [ -74.1, 40.73 ] });
+    result = methods.__get__('geoUtil').constructPoint({ latLon: [ -74.1, 40.73 ] });
     should(result.lat).be.exactly(lat);
     should(result.lon).be.exactly(lon);
   });
 
   it('Point: form { latLon: "-74.1, 40.73" }', function () {
-    result = methods._geoUtil.constructPoint({ latLon: '-74.1, 40.73' });
+    result = methods.__get__('geoUtil').constructPoint({ latLon: '-74.1, 40.73' });
     should(result.lat).be.exactly(lat);
     should(result.lon).be.exactly(lon);
   });
   it('Point: geohash form', function () {
-    result = methods._geoUtil.constructPoint({ latLon: 'dr5r9ydj2y73' });
+    result = methods.__get__('geoUtil').constructPoint({ latLon: 'dr5r9ydj2y73' });
     should(Math.round(result.lat)).be.exactly(Math.round(lon));
     should(Math.round(result.lon)).be.exactly(Math.round(lat));
   });
   it('should throw an error if bad coordinates are given', function () {
     try {
-      result = methods._geoUtil.constructPoint('foo', 'bar', {});
+      result = methods.__get__('geoUtil').constructPoint('foo', 'bar', {});
       return false;
     } catch(err) {
       return true;
     }
   });
   it('Point: should handle a [0,0] coordinates', function () {
-    result = methods._geoUtil.constructPoint({ lat: 0, lon: 0 });
+    result = methods.__get__('geoUtil').constructPoint({ lat: 0, lon: 0 });
     should(result.lat).be.exactly(0);
     should(result.lon).be.exactly(0);
   });
