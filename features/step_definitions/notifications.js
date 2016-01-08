32e20f97d75188c83160eff8f70a9dbaeb929161
Anthony Sendra <asendra@kaliop.com>
2016-01-08 18:05:04 +0100

    fix problem from functional testing

--- features/step_definitions/notifications.js
+++ features/step_definitions/notifications.js
@@ -11,8 +11,8 @@ var apiSteps = function () {
             return false;
           }
 
-          if (this.api.responses.result.action !== action) {
-            callbackAsync('Action "' + this.api.responses.result.action + '" received. Expected: "' + action + '"');
+          if (this.api.responses.action !== action) {
+            callbackAsync('Action "' + this.api.responses.action + '" received. Expected: "' + action + '"');
             return false;
           }
 
@@ -50,24 +50,24 @@ var apiSteps = function () {
     var
       diff = false;
 
-    if (!this.api.responses.result.metadata) {
+    if (!this.api.responses.metadata) {
       return callback('Expected metadata in the notification but none was found');
     }
 
-    diff = Object.keys(this.metadata).length !== Object.keys(this.api.responses.result.metadata).length;
+    diff = Object.keys(this.metadata).length !== Object.keys(this.api.responses.metadata).length;
 
     Object.keys(this.metadata).forEach(key => {
       if (!diff) {
-        if (!this.api.responses.result.metadata[key]) {
+        if (!this.api.responses.metadata[key]) {
           diff = true;
         } else {
-          diff = JSON.stringify(this.metadata[key]).localeCompare(JSON.stringify(this.api.responses.result.metadata[key])) !== 0;
+          diff = JSON.stringify(this.metadata[key]).localeCompare(JSON.stringify(this.api.responses.metadata[key])) !== 0;
         }
       }
     });
 
     if (diff) {
-      callback('Expected ' + JSON.stringify(this.api.responses.result.metadata) + ' to match ' + JSON.stringify(this.metadata));
+      callback('Expected ' + JSON.stringify(this.api.responses.metadata) + ' to match ' + JSON.stringify(this.metadata));
     } else {
       callback();
     }
