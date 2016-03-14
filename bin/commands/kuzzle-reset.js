008ed4046ba71bbd8c390b9e5c56c9c36142b3e4
Jeff <jeff@kuzzle.io>
2016-03-14 18:35:43 +0100

    wip --- fully functionnal with prompt

--- bin/commands/kuzzle-reset.js
+++ bin/commands/kuzzle-reset.js
@@ -1,11 +1,30 @@
 var
   rc = require('rc'),
   params = rc('kuzzle'),
-  Kuzzle = require('../../lib/api');
+  Kuzzle = require('../../lib/api'),
+  readlineSync = require('readline-sync'),
+  clc = require('cli-color'),
+  error = clc.red,
+  warn = clc.yellow,
+  question = clc.whiteBright,
+  notice = clc.cyanBright,
+  ok = clc.green.bold,
+  kuz = clc.greenBright.bold;
 
 module.exports = function () {
+  
+  console.log(warn('[ℹ] You are about to reset Kuzzle and make it like a virgin.'));
+  console.log(warn('[ℹ] This operation cannot be undone.\n'));
 
-  var kuzzle = new Kuzzle(false);
-  kuzzle.cleanAndPrepare(params, true);
+  if (readlineSync.question('[❓] Are you sure ? If so, please type "I am sure": ') === 'I am sure') {
+
+    var kuzzle = new Kuzzle(false);
+    kuzzle.cleanAndPrepare(params, true);
+
+  } else {
+
+    console.log(notice('[ℹ] Nothing have been done... you do not look that sure...'));
+
+  }
 
 };
