eead4d5397ff1f025a483163114e53de5bd948c9
Jeff <jeff@kuzzle.io>
2016-03-16 14:39:05 +0100

    fully functionnal

--- bin/commands/kuzzle-reset.js
+++ bin/commands/kuzzle-reset.js
@@ -3,6 +3,7 @@ var
   params = rc('kuzzle'),
   Kuzzle = require('../../lib/api'),
   readlineSync = require('readline-sync'),
+  fs = require('fs'),
   clc = require('cli-color'),
   error = clc.red,
   warn = clc.yellow,
@@ -12,11 +13,34 @@ var
   kuz = clc.greenBright.bold;
 
 module.exports = function () {
-  
+  var tmp;
+
+  // check, if files are provided, if they exists
+  if (params.fixtures) {
+    try {
+      tmp = JSON.parse(fs.readFileSync(params.fixtures, 'utf8'));
+    } catch(e) {
+      console.log(error('[✖] The file ' + params.fixtures + ' cannot be opened... aborting.'));
+      process.exit(1);
+    }
+  }
+
+  if (params.mappings) {
+    try {
+      tmp = JSON.parse(fs.readFileSync(params.mappings, 'utf8'));
+    } catch(e) {
+      console.log(error('[✖] The file ' + params.mappings + ' cannot be opened... aborting.'));
+      process.exit(1);
+    }
+  }
+
+
   console.log(warn('[ℹ] You are about to reset Kuzzle and make it like a virgin.'));
   console.log(warn('[ℹ] This operation cannot be undone.\n'));
 
-  if (readlineSync.question('[❓] Are you sure ? If so, please type "I am sure": ') === 'I am sure') {
+  if (readlineSync.question('[❓] Are you sure ? If so, please type "I am sure" (if not just press [Enter]): ') === 'I am sure') {
+
+    console.log(notice('[ℹ] Processing...\n'));
 
     var kuzzle = new Kuzzle(false);
     kuzzle.cleanAndPrepare(params, true);
