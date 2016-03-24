fbfab9e715c504d9c0d454ccec4077b9b9f0b5d2
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-03-24 10:57:55 +0100

    fix from/to operators

--- test/api/dsl/operators/from.test.js
+++ test/api/dsl/operators/from.test.js
@@ -4,15 +4,22 @@ var
   operators = require.main.require('lib/api/dsl/operators');
 
 describe('Test from operator', function () {
-  it('should call the gte() operator', function () {
+  it('should call the gte() operator with same arguments', function () {
     var
+      argumentsMatch = false,
       called = false,
       saved = operators.gte;
 
-    operators.gte = function () { called = true; };
+    operators.gte = function (field, value, document) {
+      if (field === 'foo' && value === 0 && document.toString() === {}.toString()) {
+        argumentsMatch = true;
+      }
+      called = true;
+    };
     operators.from('foo', 0, {});
     operators.gte = saved;
 
+    should(argumentsMatch).be.true();
     should(called).be.true();
   });
 });
