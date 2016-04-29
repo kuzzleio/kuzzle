bc528653c173d513bd2b7fe251f7e142979374a7
scottinet <scottinet@kaliop.com>
2016-04-29 10:30:58 +0200

    made kuzzle install stdout sync

--- bin/commands/install.js
+++ bin/commands/install.js
@@ -9,7 +9,5 @@ module.exports = function () {
     process.exit(1);
   }
 
-console.log(childProcess
-    .execSync(require.main.filename + ' plugins --install')
-    .toString());
+  childProcess.spawn(require.main.filename, ['plugins', '--install'], {stdio: 'inherit'});
 };
