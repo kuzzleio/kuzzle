4cbbc6911e4bb303d3456014b0a58fee886e8754
Jeff <jeff@kuzzle.io>
2016-06-02 11:00:21 +0200

    better tests

--- test/api/controllers/remoteActionsController/swagger.test.js
+++ test/api/controllers/remoteActionsController/swagger.test.js
@@ -42,11 +42,44 @@ describe('Test: Swagger files generation', () => {
       });
   });
 
+  it ('should create entries in the swagger files for the plugins', () => {
+
+    sandbox.stub(fs, 'writeFileSync', () => {
+      return true;
+    });
+
+    sandbox.stub(kuzzle.pluginsManager, 'routes', [
+      {verb: 'post', url: '/myplugin/bar', controller: 'myplugin/foo', action: 'bar'},
+      {verb: 'get', url: '/myplugin/bar/:name', controller: 'myplugin/foo', action: 'bar'}
+    ]);
+
+    swagger(kuzzle)
+      .then((result) => {
+        should(result.paths['/1.0/_plugin/myplugin/bar']).be.an.Object();
+        should(result.paths['/1.0/_plugin/myplugin/bar/:name']).be.an.Object();
+      });
+  });
+
   it ('should fail if something goes wrong with the swagger files writing', () => {
     sandbox.stub(fs, 'writeFileSync', () => {
       throw 'error';
     });
 
     should(swagger(kuzzle)).be.rejectedWith(InternalError);
+  });
+
+  it ('should do nothing if Kuzzle is a worker', () => {
+    sandbox.stub(fs, 'writeFileSync', () => {
+      return true;
+    });
+
+    sandbox.stub(kuzzle, 'isServer', false);
+    sandbox.stub(kuzzle, 'isWorker', true);
+
+    swagger(kuzzle)
+      .then((response) => {
+        should(fs.writeFileSync.called).be.false();
+        should(response.isWorker).be.true();
+      });
   });
 });
