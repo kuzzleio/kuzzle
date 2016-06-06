894730e27ad082a11ad9a31f314a3c8f57596f82
ballinette <gballini@kaliop.com>
2016-06-06 02:37:07 +0200

    KUZ-566: add unit tests for User and Profile models prototypes.

--- test/api/core/models/security/profile.test.js
+++ test/api/core/models/security/profile.test.js
@@ -64,4 +64,101 @@ describe('Test: security/profileTest', function () {
       })
       .then(isAllowed => should(isAllowed).be.false());
   });
+
+  it('should retrieve the good rights list', function () {
+    var
+      profile = new Profile(),
+      role1 = new Role(),
+      role2 = new Role(),
+      role3 = new Role();
+
+    role1.controllers = {
+      read: {
+        actions: { '*': true }
+      }
+    };
+    role1.restrictedTo = [{ index: 'index1', collections: ['collection1', 'collection2'] }];
+    profile.roles.push(role1);
+
+    role2.controllers = {
+      write: {
+        actions: { publish: true, create: true, update: true }
+      }
+    };
+    role2.restrictedTo = [{index: 'index2'}];
+    profile.roles.push(role2);
+
+    role3.controllers = {
+      read: {
+        actions: { get: true, count: true, search: true }
+      },
+      write: {
+        actions: { update: {test: 'return true;'}, create: true, delete: {test: 'return true;'} }
+      }
+    };
+    profile.roles.push(role3);
+
+    return profile.getRights()
+      .then(rights => {
+        var filteredItem;
+
+        should(rights).be.an.Object();
+        rights = Object.keys(rights).reduce((array, item) => array.concat(rights[item]), []);
+        should(rights).be.an.Array();
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'read' &&
+                  item.action === 'get';
+        });
+        should(filteredItem).length(1);
+        should(filteredItem[0].index).be.equal('*');
+        should(filteredItem[0].collection).be.equal('*');
+        should(filteredItem[0].value).be.equal('allowed');
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'read' &&
+                  item.action === '*';
+        });
+        should(filteredItem).length(2);
+        should(filteredItem.every(item => item.index === 'index1')).be.equal(true);
+        should(filteredItem.some(item => item.collection === 'collection1')).be.equal(true);
+        should(filteredItem.some(item => item.collection === 'collection2')).be.equal(true);
+        should(filteredItem.every(item => item.value === 'allowed')).be.equal(true);
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'write' &&
+                  item.action === 'publish';
+        });
+        should(filteredItem).length(1);
+        should(filteredItem[0].index).be.equal('index2');
+        should(filteredItem[0].collection).be.equal('*');
+        should(filteredItem[0].value).be.equal('allowed');
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'write' &&
+                  item.action === 'update';
+        });
+        should(filteredItem.every(item => {
+          return (item.index === '*' && item.collection === '*' && item.value === 'conditional' ) ||
+            (item.index === 'index2' && item.collection === '*' && item.value === 'allowed');
+        })).be.equal(true);
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'write' &&
+                  item.action === 'delete';
+        });
+        should(filteredItem).length(1);
+        should(filteredItem[0].index).be.equal('*');
+        should(filteredItem[0].collection).be.equal('*');
+        should(filteredItem[0].value).be.equal('conditional');
+
+        filteredItem = rights.filter(item => {
+          return item.controller === 'read' &&
+                  item.action === 'listIndexes';
+        });
+        should(filteredItem).length(0);
+
+      });
+  });
+
 });
