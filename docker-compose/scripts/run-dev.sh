a9ee11d2aed4742d06dcc094e138c0a0077745c4
Benoît Vidis <bvidis@kaliop.com>
2016-07-28 12:15:19 +0200

    trigger ES shards activation

--- docker-compose/scripts/run-dev.sh
+++ docker-compose/scripts/run-dev.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
-set -e
-
 ELASTIC=${READ_ENGINE_HOST:-elasticsearch:9200}
 
-npm install
-
 echo "Waiting for elasticsearch to be available"
-E=$(curl -s "http://${ELASTIC}/_cluster/health?wait_for_status=yellow&timeout=60s")
+while ! curl -f -s -o /dev/null "http://$ELASTIC"
+do
+    echo "$(date) - still trying connecting to http://$ELASTIC"
+    sleep 1
+done
+# create a tmp index just to force the shards to init
+curl -XPUT -s -o /dev/null "http://$ELASTIC/%25___tmp"
+echo "Elasticsearch is up. Waiting for shards to be active (can take a while)"
+E=$(curl -s "http://${ELASTIC}/_cluster/health?wait_for_status=yellow&wait_for_active_shards=1&timeout=60s")
+curl -XDELETE -s -o /dev/null "http://$ELASTIC/%25___tmp"
 
 if ! (echo ${E} | grep -E '"status":"(yellow|green)"' > /dev/null); then
     echo "Could not connect to elasticsearch in time. Aborting..."
     exit 1
 fi
 
+npm install
+
 echo "Starting Kuzzle..."
 
-node bin/kuzzle install && pm2 start /config/processes-dev.json
+node bin/kuzzle install && pm2 start /config/pm2-dev.json
 
 nohup node-inspector --web-port=8080 --debug-port=7000 > /dev/null 2>&1&
 nohup node-inspector --web-port=8081 --debug-port=7001 > /dev/null 2>&1&
--- docker-compose/scripts/run.sh
+++ docker-compose/scripts/run.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-set -e
-
 ELASTIC=${READ_ENGINE_HOST:-elasticsearch:9200}
 
-npm install
-
 echo "Waiting for elasticsearch to be available"
-E=$(curl -s "http://${ELASTIC}/_cluster/health?wait_for_status=yellow&timeout=60s")
+while ! curl -f -s -o /dev/null "http://$ELASTIC"
+do
+    echo "$(date) - still trying connecting to http://$ELASTIC"
+    sleep 1
+done
+# create a tmp index just to force the shards to init
+curl -XPUT -s -o /dev/null "http://$ELASTIC/%25___tmp"
+echo "Elasticsearch is up. Waiting for shards to be active (can take a while)"
+E=$(curl -s "http://${ELASTIC}/_cluster/health?wait_for_status=yellow&wait_for_active_shards=1&timeout=60s")
+curl -XDELETE -s -o /dev/null "http://$ELASTIC/%25___tmp"
 
 if ! (echo ${E} | grep -E '"status":"(yellow|green)"' > /dev/null); then
     echo "Could not connect to elasticsearch in time. Aborting..."
     exit 1
 fi
 
+npm install
+
 echo "Starting Kuzzle..."
 
-node bin/kuzzle install && pm2 start /config/processes-dev.json
+node bin/kuzzle install && pm2 start /config/pm2.json
 pm2 logs
