1956187e1f6ad4795e036e2acade23107e03f43d
Benoît Vidis <bvidis@kaliop.com>
2016-08-05 17:18:48 +0200

    kuz-606 kuzzlerc refactor - wip

---
+++ default.config.js
@@ -0,0 +1,175 @@
+module.exports = {
+  hooks: require('./lib/config/hooks'),
+
+  httpRoutes: require('./lib/config/httpRoutes'),
+
+  plugins: {
+    common: {
+      pipeWarnTime: 40,
+      pipeTimeout: 250
+    },
+
+    'kuzzle-plugin-logger': {
+      npmVersion: '2.0.4',
+      activated: true
+    },
+    'kuzzle-plugin-auth-passport-local': {
+      npmVersion: '2.0.4',
+      activated: true
+    }
+  },
+
+  queues: {
+    remoteActionsQueue: 'remote-actions-queue'
+  },
+
+  repositories: {
+    common: {
+      cacheTTL: 1440
+    }
+  },
+
+  security: {
+    jwt: {
+      algorithm: 'HS256',
+      expiresIn: '1h'
+    },
+    default: {
+      role: {
+        controllers: {
+          '*': {
+            actions: {
+              '*': true
+            }
+          }
+        }
+      }
+    },
+    standard: {
+      profiles: {
+        admin: {
+          policies: [ {roleId: 'admin', allowInternalIndex: true} ]
+        },
+        default: {
+          policies: [ {roleId: 'default'} ]
+        },
+        anonymous: {
+          policies: [ {roleId: 'anonymous'} ]
+        }
+      },
+      roles: {
+        admin: {
+          controllers: {
+            '*': {
+              actions: {
+                '*': true
+              }
+            }
+          }
+        },
+        default: {
+          controllers: {
+            auth: {
+              actions: {
+                checkToken: true,
+                getCurrentUser: true,
+                getMyRights: true,
+                login: true,
+                logout: true,
+                updateSelf: true
+              }
+            },
+            read: {
+              actions: {
+                serverInfo: true
+              }
+            }
+          }
+        },
+        anonymous: {
+          controllers: {
+            auth: {
+              actions: {
+                checkToken: true,
+                getCurrentUser: true,
+                getMyRights: true,
+                login: true,
+                logout: true
+              }
+            },
+            read: {
+              actions: {
+                serverInfo: true
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+
+  server: {
+    http: {
+      port: 7511
+    },
+    maxConcurrentRequests: 50,
+    maxRetainedRequests: 50000,
+    warningRetainedRequestsLimit: 5000
+  },
+
+  services: {
+    common: {
+      defaultInitTimeout: 10000,
+      retryInterval: 1000
+    },
+
+    cache: {
+      backend: 'redis',
+      aliases: [
+        'notificationCache',
+        'statsCache',
+        'securityCache',
+        'internalCache',
+        'tokenCache',
+        'memoryStorage'
+      ],
+      databases: [
+        'notificationCache',
+        'statsCache',
+        'securityCache',
+        'internalCache',
+        'tokenCache',
+        'memoryStorage'
+      ],
+      node: {
+        host: 'redis',
+        port: 6379
+      }
+    },
+    internalBroker: {
+      aliases: ['broker'],
+      host: 'localhost',
+      port: 7911,
+      retryInterval: 1000
+    },
+    proxyBroker: {
+      host: 'api',
+      port: 7331,
+      retryInterval: 1000
+    },
+    db: {
+      aliases: ['readEngine', 'writeEngine'],
+      backend: 'elasticsearch',
+      host: 'elasticsearch',
+      port: 9200,
+      apiVersion: '2.3'
+    }
+
+  },
+
+  stats: {
+    ttl: 3600,
+    statsInterval: 10
+  }
+
+};
