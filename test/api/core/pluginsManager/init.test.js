137e887e375901548f47187fdec805ae198ba9bf
scottinet <scottinet@kuzzle.io>
2016-09-29 14:16:05 +0200

    add unit test

--- test/api/core/pluginsManager/init.test.js
+++ test/api/core/pluginsManager/init.test.js
@@ -2,25 +2,41 @@ var
   should = require('should'),
   rewire = require('rewire'),
   Promise = require('bluebird'),
+  sinon = require('sinon'),
   Kuzzle = require.main.require('lib/api/kuzzle'),
   PluginsManager = rewire('../../../../lib/api/core/plugins/pluginsManager');
 
-describe('PluginsManager: init()', () => {
+describe.only('PluginsManager: init()', () => {
   var
     kuzzle,
-    loadPluginsCalled,
     pluginsManager;
 
   before(() => {
     kuzzle = new Kuzzle();
     pluginsManager = new PluginsManager(kuzzle);
-    PluginsManager.__set__('loadPlugins', () => {
-      loadPluginsCalled = true;
-    });
   });
 
   it('should load plugins at init', () => {
-    loadPluginsCalled = false;
+    var loadPluginsStub = sinon.stub();
+
+    return PluginsManager.__with__({
+      getPluginsList: () => {
+        return Promise.resolve({
+          plugin1: {foo: 'bar'},
+          plugin2: {foo: 'baz'}
+        });
+      },
+      loadPlugins: loadPluginsStub
+    })(() => {
+      return pluginsManager.init()
+        .then(() => {
+          should(loadPluginsStub.called).be.true();
+          should(pluginsManager.plugins).match({ plugin1: { foo: 'bar' }, plugin2: { foo: 'baz' } });
+        });
+    });
+  });
+
+  it('should discard a plugin if it fails to load', () => {
     return PluginsManager.__with__({
       getPluginsList: () => {
         return Promise.resolve({
@@ -31,8 +47,7 @@ describe('PluginsManager: init()', () => {
     })(() => {
       return pluginsManager.init()
         .then(() => {
-          should(loadPluginsCalled).be.true();
-          should(pluginsManager.plugins).match({ plugin1: { foo: 'bar' }, plugin2: { foo: 'baz' } });
+          should(pluginsManager.plugins).match({});
         });
     });
   });
