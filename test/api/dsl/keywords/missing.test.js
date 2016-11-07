857fe492dedf2458aa109f604562f9071f47b07f
scottinet <scottinet@kuzzle.io>
2016-11-07 15:41:33 +0100

    unit tests: ids, in and missing keywords

---
+++ test/api/dsl/keywords/missing.test.js
@@ -0,0 +1,30 @@
+var
+  should = require('should'),
+  sinon = require('sinon'),
+  DSL = require('../../../../lib/api/dsl');
+
+describe('DSL.keyword.missing', () => {
+  var dsl;
+
+  beforeEach(() => {
+    dsl = new DSL();
+  });
+
+  describe('#standardization', () => {
+    it('should return a "not exists" condition', () => {
+      let spy = sinon.spy(dsl.transformer.standardizer.dsl, 'exists');
+
+      return dsl.transformer.standardizer.standardize({missing: {field: 'foo'}})
+        .then(result => {
+          should(spy.called).be.true();
+          should(result).match({
+            not: {
+              exists: {
+                field: 'foo'
+              }
+            }
+          })
+        });
+    });
+  });
+});
