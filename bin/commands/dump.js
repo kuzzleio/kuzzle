25bfbf1a718418e29009955e258427f6f7240127
David Bengsch <dbengsch@kaliop.com>
2016-11-28 18:02:30 +0100

    Funnel refactor, some TODO hints and refactor in progress

--- bin/commands/dump.js
+++ bin/commands/dump.js
@@ -14,7 +14,7 @@ module.exports = function commandDump (options) {
 
   console.log(notice('[ℹ] Creating dump file...'));
 
-  kuzzle.cli.do('dump', {sufix: 'cli'})
+  kuzzle.cli.do('dump', {suffix: 'cli'})
     .then(dumpPath => {
       console.log(ok('[✔] Done!'));
       console.log('\n' + warn(`[ℹ] Dump has been successfully generated in "${dumpPath.data.body}" folder`));
