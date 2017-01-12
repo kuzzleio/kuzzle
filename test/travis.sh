b278d9872af93a5340fc0bc512deaa91a88bf795
Benoît Vidis <bvidis@kuzzle.io>
2017-01-12 17:59:00 +0100

    hotfix-travis-tests

---
+++ test/travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+sudo sysctl -w vm.max_map_count=262144
+
+if [ "${TRAVIS_BRANCH}" != "master" ]; then
+  export DOCKER_PROXY_TAG=":develop"
+fi
+
+docker-compose -f docker-compose/test.yml run kuzzle
