8d5684e41ed3782138e42af85c090d46df748ffc
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-06 10:43:41 +0100

    enable sonar in project
    enable sonar PR review - analysis on merge pending

---
+++ test/travis-bin/sonar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  echo "Running sonar scanner"
+
+  sonar-scanner \
+    -Dsonar.host.url=https://sonarqube.kaliop.net \
+    -Dsonar.analysis.mode=preview \
+    -Dsonar.profile=NodeJS \
+    -Dsonar.projectVersion="$PACKAGE_VERSION" \
+    -Dsonar.login="$SONAR_TOKEN" \
+    -Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
+    -Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
+    -Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+fi
+
