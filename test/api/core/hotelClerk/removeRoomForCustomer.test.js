fede29cb88300f0b808731c325a57028ea6e27f0
Benoît Vidis <bvidis@kuzzle.io>
2017-03-22 17:46:43 +0100

    fixes #733 - cleanup customer rooms on client disconnection

---
+++ test/api/core/hotelClerk/removeRoomForCustomer.test.js
@@ -0,0 +1,83 @@
+const
+  Promise = require('bluebird'),
+  rewire = require('rewire'),
+  sinon = require('sinon'),
+  should = require('should'),
+  HotelClerk = rewire('../../../../lib/api/core/hotelClerk'),
+  KuzzleMock = require('../../../mocks/kuzzle.mock'),
+  NotFoundError = require('kuzzle-common-objects').errors.NotFoundError,
+  RequestContext = require('kuzzle-common-objects').models.RequestContext;
+
+describe ('lib/core/hotelclerck:removeRoomForCustomer', () => {
+  let
+    context,
+    cleanupRooms,       // eslint-disable-line no-unused-vars
+    removeRoomForCustomers,
+    requestContext;
+
+  beforeEach(() => {
+    context = {
+      customers: {},
+      rooms: {
+        roomId: {
+          channels: {
+            ch1: true,
+            ch2: true
+          },
+          customers: ['connectionId']
+        }
+      },
+      kuzzle: new KuzzleMock()
+    };
+    cleanupRooms = sinon.stub().returns(Promise.resolve());
+
+    requestContext = new RequestContext({
+      connectionId: 'connectionId',
+      protocol: 'protocol'
+    });
+
+    removeRoomForCustomers = HotelClerk.__get__('removeRoomForCustomer').bind(context);
+  });
+
+  it('should reject if the connection cannot be found', () => {
+    return should(removeRoomForCustomers(requestContext, 'roomId'))
+      .be.rejectedWith(NotFoundError);
+  });
+
+  it('should reject if the customer did not subscribe to the room', () => {
+    context.customers.connectionId = {};
+
+    return should(removeRoomForCustomers(requestContext, 'roomId'))
+      .be.rejectedWith(NotFoundError);
+  });
+
+  it('should remove the room from the customer list and remove the connection entry if empty', () => {
+    context.customers.connectionId = {roomId: null};
+
+    return removeRoomForCustomers(requestContext, 'roomId', false)
+      .then(roomId => {
+        should(context.customers)
+          .be.empty();
+
+        should(roomId)
+          .be.eql('roomId');
+      });
+  });
+
+  it('should remove the room from the customer list and keep other existing rooms', () => {
+    context.customers.connectionId = {
+      roomId: null,
+      anotherRoom: null
+    };
+
+    return removeRoomForCustomers(requestContext, 'roomId', false)
+      .then(() => {
+        should(context.customers.connectionId)
+          .eql({
+            anotherRoom: null
+          });
+      });
+
+  });
+
+});
