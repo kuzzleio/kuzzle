95ce46312461497c970044bff90bfd6e1e2a90b8
scottinet <scottinet@kuzzle.io>
2017-04-24 15:46:18 +0200

    starting to clean up

--- test/travis-bin/sonar.sh
+++ test/travis-bin/sonar.sh
@@ -4,31 +4,12 @@ set -e
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-echo "CURRENT PATH = "$(pwd)
-echo "TRAVIS_REPO_SLUG ${TRAVIS_REPO_SLUG}"
-echo "TRAVIS PULL REQUEST ${TRAVIS_PULL_REQUEST}"
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
+then
   echo "Running sonar scanner"
 
-echo "==== TRAVIS SONAR CONFIG: "
-cat /home/travis/.sonarscanner/sonar-scanner-2.8/conf/sonar-scanner.properties
-
-
-echo sonar-scanner \
-    -Dsonar.host.url=https://sonarqube.kaliop.net \
-    -Dsonar.projectKey=kuzzle:server \
-    -Dsonar.sources=lib \
-    -Dsonar.analysis.mode=preview \
-    -Dsonar.projectVersion="$PACKAGE_VERSION" \
-    -Dsonar.login="..." \
-    -Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
-    -Dsonar.github.oauth="..." \
-    -Dsonar.github.repository="$TRAVIS_REPO_SLUG"
-
-echo "SONAR VERSION: "
-  sonar-scanner --version
-
-unset SONARQUBE_SCANNER_PARAMS
+  # Unset travis pre-set parameters, which are more harmful than anything
+  unset SONARQUBE_SCANNER_PARAMS
 
   sonar-scanner \
     -Dsonar.host.url=https://sonarqube.kaliop.net \
@@ -38,9 +19,8 @@ unset SONARQUBE_SCANNER_PARAMS
     -Dsonar.projectVersion="$PACKAGE_VERSION" \
     -Dsonar.login="$SONAR_TOKEN" \
     -Dsonar.language=js \
-    -X
-#    -Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
-#    -Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-#    -Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+    -Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
+    -Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
+    -Dsonar.github.repository="$TRAVIS_REPO_SLUG"
 fi
 
