87995d7258f0441509920685a916df66e0cf05ba
scottinet <scottinet@kuzzle.io>
2017-04-24 13:37:57 +0200

    once again, with feelings

--- test/travis-bin/sonar.sh
+++ test/travis-bin/sonar.sh
@@ -13,6 +13,5 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     -Dsonar.login="$SONAR_TOKEN" \
     -Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
     -Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-    -Dsonar.github.repository="$TRAVIS_REPO_SLUG" \
-    -Dsonar.branch="${TRAVIS_BRANCH}"
+    -Dsonar.github.repository="$TRAVIS_REPO_SLUG"
 fi
