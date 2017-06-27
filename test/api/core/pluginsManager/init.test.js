6c2d457c3db24a98676f6ee8f75b1bc1d9e13ef5
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-06-27 13:10:55 +0200

    manifest file improvment

--- test/api/core/pluginsManager/init.test.js
+++ test/api/core/pluginsManager/init.test.js
@@ -4,9 +4,12 @@ const
   should = require('should'),
   mockrequire = require('mock-require'),
   sinon = require('sinon'),
-  KuzzleMock = require('../../../mocks/kuzzle.mock');
+  KuzzleMock = require('../../../mocks/kuzzle.mock'),
+  {
+    PluginImplementationError
+  } = require('kuzzle-common-objects').errors;
 
-describe('PluginsManager', () => {
+describe.only('PluginsManager', () => {
   let
     PluginsManager,
     pluginsManager,
@@ -41,7 +44,7 @@ describe('PluginsManager', () => {
   });
 
   describe('#loadPlugins', () => {
-    it('should reject all plugin initialization if an error occurs when loading it from package.json', () => {
+    it('should reject all plugin initialization if an error occurs when loading a non requireable directory', () => {
       fsStub.readdirSync.returns(['kuzzle-plugin-test']);
       fsStub.statSync.returns({
         isDirectory: () => true
@@ -49,62 +52,93 @@ describe('PluginsManager', () => {
 
       should(() => pluginsManager.init()).throw();
       should(pluginsManager.plugins).be.empty();
-    });
-
-    it('should reject all plugin initialization if an error occurs when loading it from a directory', () => {
-      fsStub.accessSync.throws(new Error('package.json does not exist'));
-      fsStub.readdirSync.returns(['kuzzle-plugin-test']);
-      fsStub.statSync.returns({
-        isDirectory: () => true
-      });
-
-      should(() => pluginsManager.init()).throw();
-      should(pluginsManager.plugins).be.empty();
-    });
-
-    it('should return a well-formed plugin instance if a valid node-module plugin is enabled', () => {
-      fsStub.readdirSync.returns(['kuzzle-plugin-test']);
-      fsStub.statSync.returns({
-        isDirectory: () => true
-      });
-
-      const name = 'kuzzle-plugin-test';
-      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test', function () {});
-      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test/package.json', {name});
-      PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
-
-      pluginsManager = new PluginsManager(kuzzle);
-
-      pluginsManager.init();
-      should(pluginsManager.plugins[name]).be.Object();
-      should(pluginsManager.plugins[name]).have.keys('name', 'object', 'config', 'path');
-      should(pluginsManager.plugins[name].name).be.eql(name);
-      should(pluginsManager.plugins[name].object).be.ok();
-      should(pluginsManager.plugins[name].path).be.ok();
     });
 
     it('should return a well-formed plugin instance if a valid requireable plugin is enabled', () => {
-      const name = 'kuzzle-plugin-test';
+      const instanceName = 'kuzzle-plugin-test';
       fsStub.readdirSync.returns(['kuzzle-plugin-test']);
-      fsStub.accessSync.throws(new Error('package.json does not exist'));
       fsStub.statSync.returns({
         isDirectory: () => true
       });
 
       mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test', function () {});
-      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test/package.json', {name});
       PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
 
       pluginsManager = new PluginsManager(kuzzle);
 
       pluginsManager.init();
-      should(pluginsManager.plugins[name]).be.Object();
-      should(pluginsManager.plugins[name]).have.keys('name', 'object', 'config', 'path');
-      should(pluginsManager.plugins[name].name).be.eql(name);
-      should(pluginsManager.plugins[name].object).be.ok();
-      should(pluginsManager.plugins[name].path).be.ok();
+      should(pluginsManager.plugins[instanceName]).be.Object();
+      should(pluginsManager.plugins[instanceName]).have.keys('name', 'object', 'config', 'manifest', 'path');
+      should(pluginsManager.plugins[instanceName].name).be.eql(instanceName);
+      should(pluginsManager.plugins[instanceName].manifest).be.eql({
+        name: undefined,
+        version: '1.0.0',
+        threadable: true,
+        privileged: false,
+        kuzzleVersion: '1.x'
+      });
+      should(pluginsManager.plugins[instanceName].object).be.ok();
+      should(pluginsManager.plugins[instanceName].path).be.ok();
+    });
+
+    it('should load plugin manifest if exists', () => {
+      const instanceName = 'kuzzle-plugin-test';
+      const manifest = {
+        name: 'plugin-42',
+        version: '1.2.0',
+        threadable: false
+      };
+
+      fsStub.readdirSync.returns(['kuzzle-plugin-test']);
+      fsStub.statSync.returns({
+        isDirectory: () => true
+      });
+
+      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test', function () {});
+      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test/manifest.json', manifest);
+      PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
+
+      pluginsManager = new PluginsManager(kuzzle);
+
+      pluginsManager.init();
+      should(pluginsManager.plugins[instanceName]).be.Object();
+      should(pluginsManager.plugins[instanceName]).have.keys('name', 'object', 'config', 'manifest', 'path');
+      should(pluginsManager.plugins[instanceName].name).be.eql(instanceName);
+      should(pluginsManager.plugins[instanceName].manifest).be.eql({
+        name: manifest.name,
+        version: manifest.version,
+        threadable: manifest.threadable,
+        privileged: false,
+        kuzzleVersion: '1.x'
+      });
+      should(pluginsManager.plugins[instanceName].object).be.ok();
+      should(pluginsManager.plugins[instanceName].path).be.ok();
+    });
+
+    it('should reject plugin initialization if a plugin require another version of kuzzle core', () => {
+      const name = 'plugin-42';
+      const manifest = {
+        name,
+        kuzzleVersion: "5.x"
+      };
+
+      fsStub.readdirSync.returns(['kuzzle-plugin-test']);
+      fsStub.statSync.returns({
+        isDirectory: () => true
+      });
+
+      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test', function () {});
+      mockrequire('/kuzzle/plugins/enabled/kuzzle-plugin-test/manifest.json', manifest);
+      PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
+
+      kuzzle.config.version = '1.0.0';
+      pluginsManager = new PluginsManager(kuzzle);
+
+      should(() => pluginsManager.init()).throw(/required kuzzle version \(5\.x\) does not satisfies current: 1\.0\.0/);
     });
   });
+
+
   describe('Test plugins manager listStrategies', () => {
     it('should return a list of registrated authentication strategies', () => {
       pluginsManager.registeredStrategies = ['strategy'];
