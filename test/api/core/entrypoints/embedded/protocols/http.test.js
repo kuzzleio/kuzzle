8d0f83b60347fc1815aaccca0d86a81e45a7b085
Ballinette <gballini@kaliop.com>
2018-01-09 20:09:07 +0100

    Allow plugins to attach PATCH and HEAD verbs to their HTTP routes fix #1018

--- test/api/core/entrypoints/embedded/protocols/http.test.js
+++ test/api/core/entrypoints/embedded/protocols/http.test.js
@@ -394,7 +394,7 @@ describe('/lib/api/core/entrypoints/embedded/protocols/http', () => {
         .be.calledWith(123, {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods' : 'GET,POST,PUT,DELETE,OPTIONS',
+          'Access-Control-Allow-Methods' : 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
         });
     });
