5c9a1eadbe8bda5fa9c662d7dda9f4e6dffbb847
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-06-18 14:53:57 +0200

    Put back defaut config

--- default.config.js
+++ default.config.js
@@ -56,12 +56,9 @@ module.exports = {
   plugins: {
     common: {
       bootstrapLockTimeout: 5000,
-      pipeWarnTime: 5000,
-      pipeTimeout: 10000,
+      pipeWarnTime: 500,
+      pipeTimeout: 5000,
       initTimeout: 10000,
-    },
-    cluster: {
-      privileged: true
     }
   },
 
