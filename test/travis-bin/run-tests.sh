909c08e731489075a255b2aa038764a057b8b6b2
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-07-10 11:17:33 +0200

    Test Kuzzle against different node LTS versions

--- test/travis-bin/run-tests.sh
+++ test/travis-bin/run-tests.sh
@@ -8,5 +8,6 @@ if [ "${TRAVIS_BRANCH}" != "master" ]; then
   export DOCKER_PROXY_TAG=":develop"
 fi
 
-docker-compose -f docker-compose/test.yml run kuzzle
+NODE_LTS=6 docker-compose -f docker-compose/test.yml run kuzzle
 
+NODE_LTS=8 docker-compose -f docker-compose/test.yml run kuzzle
