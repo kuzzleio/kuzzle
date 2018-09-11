c22274fd2dcc68790efec407f136ab233606a7a6
scottinet <seb.cottinet@gmail.com>
2018-09-11 14:46:01 +0200

    remove debug config

--- test/api/core/pluginsManager/run.test.js
+++ test/api/core/pluginsManager/run.test.js
@@ -11,7 +11,7 @@ const
   } = require('kuzzle-common-objects').errors,
   PluginsManager = require('../../../../lib/api/core/plugins/pluginsManager');
 
-describe.only('PluginsManager.run', () => {
+describe('PluginsManager.run', () => {
   let
     plugin,
     pluginMock,
