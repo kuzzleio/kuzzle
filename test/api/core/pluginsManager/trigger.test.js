5f7be4c6768622c732c5a95d29ce4a8a9e8bad6d
scottinet <seb.cottinet@gmail.com>
2019-01-22 17:14:30 +0100

    [bugfix] a plugin pipe throwing an error should not bypass the triggerPipes error management

--- test/api/core/pluginsManager/trigger.test.js
+++ test/api/core/pluginsManager/trigger.test.js
@@ -1,30 +1,35 @@
 const
   mockrequire = require('mock-require'),
+  should = require('should'),
   ElasticsearchClientMock = require('../../../mocks/services/elasticsearchClient.mock'),
-  KuzzleMock = require('../../../mocks/kuzzle.mock');
-
+  KuzzleMock = require('../../../mocks/kuzzle.mock'),
+  {
+    errors: {
+      PluginImplementationError
+    }
+  } = require('kuzzle-common-objects');
 
 describe('Test plugins manager trigger', () => {
-  it('should trigger hooks with wildcard event', function (done) {
+  let pluginsManager;
+
+  beforeEach(() => {
     mockrequire('elasticsearch', {Client: ElasticsearchClientMock});
     mockrequire.reRequire('../../../../lib/services/internalEngine');
     mockrequire.reRequire('../../../../lib/api/core/plugins/pluginContext');
     mockrequire.reRequire('../../../../lib/api/core/plugins/privilegedPluginContext');
     const PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
 
-    const
-      kuzzle = new KuzzleMock(),
-      pluginsManager = new PluginsManager(kuzzle);
+    pluginsManager = new PluginsManager(new KuzzleMock());
+  });
 
+  it('should trigger hooks with wildcard event', done => {
     pluginsManager.plugins = [{
       object: {
         init: () => {},
         hooks: {
           'foo:*': 'myFunc'
         },
-        myFunc: () => {
-          done();
-        }
+        myFunc: done
       },
       config: {},
       activated: true,
@@ -39,4 +44,30 @@ describe('Test plugins manager trigger', () => {
       });
   });
 
+  it('should reject a pipe returned value if it is not a Request instance', () => {
+    const pluginMock = {
+      object: {
+        init: () => {},
+        pipes: {
+          'foo:bar': 'myFunc'
+        },
+        myFunc: () => {
+          throw new Error('foobar');
+        }
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    };
+
+    pluginsManager.object = [pluginMock];
+
+    pluginsManager.registerPipe(pluginMock, 50, 200, 'foo:bar', 'myFunc');
+
+    return should(pluginsManager.trigger('foo:bar')).rejectedWith(
+      PluginImplementationError,
+      {message: /^Plugin foo pipe for event 'foo:bar' throwed a non-Kuzzle error: Error: foobar.*/});
+  });
 });
