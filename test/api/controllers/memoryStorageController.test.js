ca1f701fa5b8f88d00a303807176a8ab006c8da1
Sébastien Cottinet <scottinet@protonmail.com>
2019-03-21 13:31:33 +0100

    Fix controllers action leak (#1266)
    Controllers actions were implicitly exposed: whenever an API request is submitted, the funnel checks if the requested action is exposed as a function by the requested controller, and if so, the action was allowed to be executed.
    This means that internal methods, which are obviously not API actions, are executable too.
    
    Hopefully, I've extensively tested this, and it appears that for now this has little consequence. But this is a major bug that needs to be fixed ASAP, just in case.
    
    This PR does that, by making controllers action exposure explicit.
    
    * [fix] make controllers actions exposure explicit to prevent leakage
    * [test fix] prevent mockrequire leak and fix randomly failing test
    * [test] add unit tests for the new base controller
    * [tests (+fix)] test the new isAction controllers method
    * [api] expose missing API methods
    * [janitoring] DRYify code

--- test/api/controllers/memoryStorageController.test.js
+++ test/api/controllers/memoryStorageController.test.js
@@ -5,8 +5,11 @@ const
   rewire = require('rewire'),
   KuzzleMock = require('../../mocks/kuzzle.mock'),
   RedisClientMock = require('../../mocks/services/redisClient.mock'),
-  BadRequestError = require('kuzzle-common-objects').errors.BadRequestError,
-  Request = require('kuzzle-common-objects').Request,
+  BaseController = require('../../../lib/api/controllers/controller'),
+  {
+    Request,
+    errors: { BadRequestError }
+  } = require('kuzzle-common-objects'),
   MemoryStorageController = rewire('../../../lib/api/controllers/memoryStorageController.js');
 
 describe('Test: memoryStorage controller', () => {
@@ -116,6 +119,16 @@ describe('Test: memoryStorage controller', () => {
       allowed.forEach(command => {
         should(msController[command]).be.a.Function();
       });
+    });
+
+    it('should inherit the base constructor', () => {
+      should(msController).instanceOf(BaseController);
+    });
+
+    it('should properly override the isAction method', () => {
+      msController._foobar = () => {};
+      should(msController.isAction('flushdb')).be.true();
+      should(msController.isAction('_foobar')).be.false();
     });
   });
 
