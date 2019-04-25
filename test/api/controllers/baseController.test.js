bcf21551c04bd00b76861e6dbec227f50a3cf059
Aschen <amaret93@gmail.com>
2019-04-25 05:49:51 +0200

    Extract ensureBooleanFlag in BaseController

--- test/api/controllers/baseController.test.js
+++ test/api/controllers/baseController.test.js
@@ -1,4 +1,5 @@
 const
+  { BadRequestError } = require('kuzzle-common-objects').errors,
   should = require('should'),
   BaseController = require('../../../lib/api/controllers/controller');
 
@@ -17,5 +18,65 @@ describe('#base controller', () => {
     should(base.isAction('foo')).be.true();
     should(base.isAction('bar')).be.true();
     should(base.isAction('qux')).be.false();
+  });
+
+  describe('#ensureBooleanFlag', () => {
+    let
+      baseController,
+      request;
+
+    beforeEach(() => {
+      request = {
+        context: {
+          connection: {
+            protocol: 'http'
+          }
+        },
+        input: {
+          args: {
+            doha: ''
+          }
+        }
+      };
+
+      baseController = new BaseController();
+    });
+
+    it('set the flag value to true if present in http', () => {
+      baseController.ensureBooleanFlag(request, 'input.args.doha');
+
+      should(request.input.args.doha).be.eql(true);
+    });
+
+    it('set the flag value to false if not present in http', () => {
+      delete request.input.args.doha;
+
+      baseController.ensureBooleanFlag(request, 'input.args.doha');
+
+      should(request.input.args.doha).be.eql(false);
+    });
+
+    it('does not nothing if the flag is already a boolean with other protocols', () => {
+      request.context.connection.protocol = 'ws';
+      request.input.args.doha = true;
+
+      baseController.ensureBooleanFlag(request, 'input.args.doha');
+
+      should(request.input.args.doha).be.eql(true);
+    });
+
+    it('throw an error if flag is not a boolean with other protocols', done => {
+      request.context.connection.protocol = 'ws';
+      request.input.args.doha = 'hamad';
+
+      try {
+        baseController.ensureBooleanFlag(request, 'input.args.doha');
+        done(new Error('Should throw BadRequestError'));
+      } catch (error) {
+        should(error).be.instanceOf(BadRequestError);
+        should(error.message).be.eql('Invalid \'doha\' value (hamad): boolean expected');
+        done();
+      }
+    });
   });
 });
