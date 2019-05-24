6593e9e3d079163f0008222379729f8fbdedb47b
scottinet <seb.cottinet@gmail.com>
2019-05-24 14:56:54 +0200

    [rebase] take #1305 as a workbase

--- test/api/core/pluginsManager/pipe.test.js
+++ test/api/core/pluginsManager/pipe.test.js
@@ -1,12 +1,14 @@
 const
   mockrequire = require('mock-require'),
   should = require('should'),
+  sinon = require('sinon'),
   ElasticsearchClientMock = require('../../../mocks/services/elasticsearchClient.mock'),
   KuzzleMock = require('../../../mocks/kuzzle.mock'),
   {
     errors: {
       PluginImplementationError
-    }
+    },
+    Request: KuzzleRequest
   } = require('kuzzle-common-objects');
 
 describe('pluginsManager.pipe', () => {
@@ -22,7 +24,10 @@ describe('pluginsManager.pipe', () => {
     const PluginsManager = mockrequire.reRequire('../../../../lib/api/core/plugins/pluginsManager');
 
     kuzzle = new KuzzleMock();
+    kuzzle.emit.restore();
+    kuzzle.pipe.restore();
     pluginsManager = new PluginsManager(kuzzle);
+    kuzzle.pluginsManager = pluginsManager;
   });
 
   it('should trigger hooks with wildcard event', done => {
@@ -44,6 +49,119 @@ describe('pluginsManager.pipe', () => {
     pluginsManager.run()
       .then(() => {
         kuzzle.emit('foo:bar');
+      });
+  });
+
+  it('should trigger hooks with before wildcard event', done => {
+    pluginsManager.plugins = [{
+      object: {
+        init: () => {},
+        hooks: {
+          'foo:before*': 'myFunc'
+        },
+        myFunc: done
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    }];
+
+    pluginsManager.run()
+      .then(() => {
+        kuzzle.emit('foo:beforeBar');
+      });
+  });
+
+  it('should trigger hooks with after wildcard event', done => {
+    pluginsManager.plugins = [{
+      object: {
+        init: () => {},
+        hooks: {
+          'foo:after*': 'myFunc'
+        },
+        myFunc: done
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    }];
+
+    pluginsManager.run()
+      .then(() => {
+        kuzzle.emit('foo:afterBar');
+      });
+  });
+
+  it('should trigger pipes with wildcard event', () => {
+    pluginsManager.plugins = [{
+      object: {
+        init: () => {},
+        pipes: {
+          'foo:*': 'myFunc'
+        },
+        myFunc: sinon.stub().callsArgWith(1, null, new KuzzleRequest({}))
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    }];
+
+    return pluginsManager.run()
+      .then(() => kuzzle.pipe('foo:bar'))
+      .then(() => {
+        should(pluginsManager.plugins[0].object.myFunc).be.calledOnce();
+      });
+  });
+
+  it('should trigger pipes with before wildcard event', () => {
+    pluginsManager.plugins = [{
+      object: {
+        init: () => {},
+        pipes: {
+          'foo:before*': 'myFunc'
+        },
+        myFunc: sinon.stub().callsArgWith(1, null, new KuzzleRequest({}))
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    }];
+
+    return pluginsManager.run()
+      .then(() => kuzzle.pipe('foo:beforeBar'))
+      .then(() => {
+        should(pluginsManager.plugins[0].object.myFunc).be.calledOnce();
+      });
+  });
+
+  it('should trigger pipes with after wildcard event', () => {
+    pluginsManager.plugins = [{
+      object: {
+        init: () => {},
+        pipes: {
+          'foo:after*': 'myFunc'
+        },
+        myFunc: sinon.stub().callsArgWith(1, null, new KuzzleRequest({}))
+      },
+      config: {},
+      activated: true,
+      manifest: {
+        name: 'foo'
+      }
+    }];
+
+    return pluginsManager.run()
+      .then(() => kuzzle.pipe('foo:afterBar'))
+      .then(() => {
+        should(pluginsManager.plugins[0].object.myFunc).be.calledOnce();
       });
   });
 
@@ -69,7 +187,7 @@ describe('pluginsManager.pipe', () => {
 
     pluginsManager.registerPipe(pluginMock, 50, 200, 'foo:bar', 'myFunc');
 
-    return should(pluginsManager.pipe('foo:bar')).rejectedWith(
+    return should(kuzzle.pipe('foo:bar')).rejectedWith(
       PluginImplementationError,
       {message: /^Plugin foo pipe for event 'foo:bar' threw a non-Kuzzle error: Error: foobar.*/});
   });
