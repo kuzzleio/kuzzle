kuzzleio/kuzzle

commit 430806b1d63f47e355b605b6183362fc137cecdd

Fix promise leaks (#1311)
The functions exposed by the notifier core generate many promises that are not awaited, and whose rejections might not be handled. And this is bad practice.

lib/api/core/notifier.js
@@ -36,6 +36,8 @@ const
 class NotifierController {
   constructor(kuzzle) {
     this.kuzzle = kuzzle;
+    this.cache = kuzzle.services.list.internalCache;
+    this.storage = kuzzle.services.list.storageEngine;
   }
 
   /**
@@ -48,10 +50,11 @@ class NotifierController {
    * @param  {string} state - 'pending' or 'done'
    * @param  {string} action - Notification type
    * @param  {object} content - Document or message
+   * @return {Promise}
    */
   notifyDocument (rooms, request, scope, state, action, content) {
     if (rooms.length === 0) {
-      return;
+      return Bluebird.resolve();
     }
 
     this.kuzzle.emit('core:notify:document', {
@@ -74,6 +77,7 @@ class NotifierController {
    * @param  {Request} request - User (un)subscription request
    * @param  {string} scope - 'in' or 'out'
    * @param  {object} content - Notification additional informations
+   * @return {Promise}
    */
   notifyUser (room, request, scope, content) {
     this.kuzzle.emit('core:notify:user', {
@@ -93,12 +97,13 @@ class NotifierController {
    * @param  {string} connectionId - User's connection identifier
    * @param  {string} type - Server notification type
    * @param  {string} message - Additional information
+   * @return {Promise}
    */
   notifyServer (rooms, connectionId, type, message) {
     const channels = [];
 
     if (rooms.length === 0) {
-      return;
+      return Bluebird.resolve();
     }
 
     for (const room of rooms) {
@@ -112,12 +117,13 @@ class NotifierController {
     if (channels.length > 0) {
       const notification = new Notification.Server(type, message);
 
-      this.kuzzle.pipe('notify:server', notification)
-        .then(updatedNotification => {
-          this._dispatch(channels, updatedNotification, connectionId);
-        })
+      return this.kuzzle.pipe('notify:server', notification)
+        .then(updatedNotification => this._dispatch(
+          channels, updatedNotification, connectionId))
         .catch(error => this.kuzzle.emit('log:error', error));
     }
+
+    return Bluebird.resolve();
   }
 
   /**
@@ -127,7 +133,7 @@ class NotifierController {
    * @param {Request} request
    * @param {string} scope - Scope of the notification
    * @param {string} state - State of the notification
-   * @returns {Object}
+   * @returns {Promise.<Object>}
    */
   publish (request, scope, state) {
     const rooms = this.kuzzle.realtime.test(
@@ -137,14 +143,11 @@ class NotifierController {
       request.input.resource._id
     );
 
-    if (rooms.length > 0) {
-      this.notifyDocument(rooms, request, scope, state, request.input.action, {
+    return this.notifyDocument(
+      rooms, request, scope, state, request.input.action, {
         _source: request.input.body,
         _id: request.input.resource._id
       });
-    }
-
-    return {published: true};
   }
 
   /**
@@ -164,15 +167,14 @@ class NotifierController {
         newDocument._id
       );
 
-    if (rooms.length > 0) {
-      this.notifyDocument(rooms, request, 'in', 'done', 'create', {
+    return this.notifyDocument(
+      rooms, request, 'in', 'done', 'create', {
         _meta: newDocument._meta || {},
         _source: newDocument._source,
         _id: newDocument._id
-      });
-
-      this._setCacheWithTTL(cachePrefix + newDocument._id, JSON.stringify(rooms));
-    }
+      })
+      .then(() => this._setCacheWithTTL(
+        cachePrefix + newDocument._id, JSON.stringify(rooms)));
   }
 
 
@@ -194,29 +196,32 @@ class NotifierController {
         request.input.resource._id
       );
 
-    if (rooms.length > 0) {
-      this.notifyDocument(rooms, request, 'in', 'done', 'replace', {
+    return this.notifyDocument(
+      rooms, request, 'in', 'done', 'replace', {
         _meta: request.input.body._kuzzle_info,
         _source: request.input.body,
         _id: request.input.resource._id
-      });
-    }
-
-    return this.kuzzle.services.list.internalCache.get(cachePrefix + request.input.resource._id)
+      })
+      .then(() => this.cache.get(cachePrefix + request.input.resource._id))
       .then(cachedRooms => {
         if (cachedRooms !== null) {
           const stopListening = _.difference(JSON.parse(cachedRooms), rooms);
 
-          this.notifyDocument(stopListening, request, 'out', 'done', 'replace', {
-            _id: request.input.resource._id
-          });
+          return this.notifyDocument(
+            stopListening, request, 'out', 'done', 'replace', {
+              _id: request.input.resource._id
+            });
         }
 
+        return null;
+      })
+      .then(() => {
         if (rooms.length === 0) {
-          return this.kuzzle.services.list.internalCache.del(cachePrefix + request.input.resource._id);
+          return this.cache.del(cachePrefix + request.input.resource._id);
         }
 
-        return this._setCacheWithTTL(cachePrefix + request.input.resource._id, JSON.stringify(rooms));
+        return this._setCacheWithTTL(
+          cachePrefix + request.input.resource._id, JSON.stringify(rooms));
       })
       .catch(error => this.kuzzle.emit('log:error', error));
   }
@@ -245,9 +250,11 @@ class NotifierController {
       cacheIds.push(prefix + documents[i]._id);
     }
 
-    return (cached ? this.kuzzle.services.list.internalCache.mget(cacheIds) : Bluebird.resolve([]))
+    return (cached ? this.cache.mget(cacheIds) : Bluebird.resolve([]))
       .then(hits => {
-        const idsToDelete = [];
+        const
+          idsToDelete = [],
+          promises = [];
 
         for (i = 0; i < documents.length; i++) {
           const
@@ -263,26 +270,33 @@ class NotifierController {
           if (hits[i] !== null && hits[i] !== undefined) {
             const stopListening = _.difference(JSON.parse(hits[i]), rooms);
 
-            this.notifyDocument(stopListening, request, 'out', 'done', documentAction, {
-              _id: documents[i]._id
-            });
+            promises.push(
+              this.notifyDocument(
+                stopListening, request, 'out', 'done', documentAction, {
+                  _id: documents[i]._id
+                }));
           }
 
           if (rooms.length > 0) {
-            this.notifyDocument(rooms, request, 'in', 'done', documentAction, {
-              _source: documents[i]._source,
-              _id: documents[i]._id
-            });
+            promises.push(
+              this.notifyDocument(
+                rooms, request, 'in', 'done', documentAction, {
+                  _source: documents[i]._source,
+                  _id: documents[i]._id
+                }));
 
-            this._setCacheWithTTL(cacheIds[i], JSON.stringify(rooms));
+            promises.push(
+              this._setCacheWithTTL(cacheIds[i], JSON.stringify(rooms)));
           } else if (hits[i] !== null && hits[i] !== undefined) {
             idsToDelete.push(cacheIds[i]);
           }
         }
 
         if (idsToDelete.length > 0) {
-          this.kuzzle.services.list.internalCache.del(idsToDelete);
+          promises.push(this.cache.del(idsToDelete));
         }
+
+        return Bluebird.all(promises);
       });
   }
 
@@ -292,6 +306,7 @@ class NotifierController {
    *    - a document they listened to left their scope
    *
    * @param {Request} request
+   * @return {Promise}
    */
   notifyDocumentUpdate (request) {
     let
@@ -309,7 +324,7 @@ class NotifierController {
         volatile: request.input.volatile
       });
 
-    return this.kuzzle.services.list.storageEngine.get(getRequest)
+    return this.storage.get(getRequest)
       .then(result => {
         updatedDocument = result;
 
@@ -319,28 +334,34 @@ class NotifierController {
           result._source,
           result._id);
 
-        this.notifyDocument(matchedRooms, request, 'in', 'done', 'update', {
-          _meta: updatedDocument._meta || {},
-          _id: updatedDocument._id,
-          _source: updatedDocument._source
-        });
-
-        return this.kuzzle.services.list.internalCache.get(cachePrefix + updatedDocument._id);
+        return this.notifyDocument(
+          matchedRooms, request, 'in', 'done', 'update', {
+            _meta: updatedDocument._meta || {},
+            _id: updatedDocument._id,
+            _source: updatedDocument._source
+          });
       })
+      .then(() => this.cache.get(cachePrefix + updatedDocument._id))
       .then(cachedRooms => {
         if (cachedRooms !== null) {
-          const stopListening = _.difference(JSON.parse(cachedRooms), matchedRooms);
+          const stopListening = _.difference(
+            JSON.parse(cachedRooms), matchedRooms);
 
-          this.notifyDocument(stopListening, request, 'out', 'done', 'update', {
-            _id: updatedDocument._id
-          });
+          return this.notifyDocument(
+            stopListening, request, 'out', 'done', 'update', {
+              _id: updatedDocument._id
+            });
         }
 
+        return null;
+      })
+      .then(() => {
         if (matchedRooms.length > 0) {
-          return this._setCacheWithTTL(cachePrefix + updatedDocument._id, JSON.stringify(matchedRooms));
+          return this._setCacheWithTTL(
+            cachePrefix + updatedDocument._id, JSON.stringify(matchedRooms));
         }
 
-        return this.kuzzle.services.list.internalCache.del(cachePrefix + updatedDocument._id);
+        return this.cache.del(cachePrefix + updatedDocument._id);
       })
       .catch(error => this.kuzzle.emit('log:error', error));
   }
@@ -350,6 +371,7 @@ class NotifierController {
    *
    * @param {Request} request
    * @param {Array} ids - list of deleted document IDs
+   * @return {Promise}
    */
   notifyDocumentMDelete (request, ids) {
     if (ids.length === 0) {
@@ -364,11 +386,12 @@ class NotifierController {
         includeTrash: true
       });
 
-    return this.kuzzle.services.list.storageEngine.mget(getRequest)
+    return this.storage.mget(getRequest)
       .then(deleted => {
         const
           cachePrefix = getCachePrefix(request),
-          cacheKeys = [];
+          cacheKeys = [],
+          promises = [];
 
         // Declaring "i" inside "for" statements downgrades
         // performances by a factor of 3 to 4
@@ -381,17 +404,21 @@ class NotifierController {
             deleted.hits[i]._source,
             deleted.hits[i]._id);
 
-          this.notifyDocument(matchedRooms, request, 'out', 'done', 'delete', {
-            _meta: deleted.hits[i]._meta || {},
-            _id: deleted.hits[i]._id
-          });
+          promises.push(
+            this.notifyDocument(
+              matchedRooms, request, 'out', 'done', 'delete', {
+                _meta: deleted.hits[i]._meta || {},
+                _id: deleted.hits[i]._id
+              }));
 
           cacheKeys.push(cachePrefix + deleted.hits[i]._id);
         }
 
         if (cacheKeys.length > 0) {
-          return this.kuzzle.services.list.internalCache.del(cacheKeys);
+          promises.push(this.cache.del(cacheKeys));
         }
+
+        return Bluebird.all(promises);
       });
   }
 
@@ -401,13 +428,17 @@ class NotifierController {
    *
    * @param  {Array} channels - Subscribers channels to notify
    * @param  {Notification.User|Notification.Document|Notification.Server} notification
-   * @param  {string} [connectionId] - Notify this connection, or broadcast if not provided
+   * @param  {string} [connectionId] - Notify this connection, or broadcast
+   *                                   if not provided
    * @param  {boolean} [trigger] - If set to true, triggers Kuzzle plugins
+   * @return {Promise}
    */
   _dispatch (channels, notification, connectionId) {
-    this.kuzzle.pipe('notify:dispatch', notification)
+    return this.kuzzle.pipe('notify:dispatch', notification)
       .then(updatedNotification => {
-        this.kuzzle.entryPoints.dispatch(connectionId ? 'notify' : 'broadcast', {
+        const action = connectionId ? 'notify' : 'broadcast';
+
+        this.kuzzle.entryPoints.dispatch(action, {
           channels,
           connectionId,
           payload: updatedNotification
@@ -426,6 +457,7 @@ class NotifierController {
    * @param  {string} state - 'pending' or 'done'
    * @param  {string} action - Notification type
    * @param  {object} content - Document or message
+   * @return {Promise}
    */
   _notifyDocument (rooms, request, scope, state, action, content) {
     const channels = [];
@@ -448,12 +480,15 @@ class NotifierController {
     }
 
     if (channels.length > 0) {
-      const notification = new Notification.Document(request, scope, state, action, content);
+      const notification = new Notification.Document(
+        request, scope, state, action, content);
 
-      this.kuzzle.pipe('notify:document', notification)
+      return this.kuzzle.pipe('notify:document', notification)
         .then(updatedNotification => this._dispatch(channels, updatedNotification))
         .catch(error => this.kuzzle.emit('log:error', error));
     }
+
+    return Bluebird.resolve();
   }
 
   /**
@@ -464,6 +499,7 @@ class NotifierController {
    * @param  {Request} request - User (un)subscription request
    * @param  {string} scope - 'in' or 'out'
    * @param  {object} content - Notification additional informations
+   * @return {Promise}
    */
   _notifyUser (room, request, scope, content) {
     const
@@ -483,26 +519,33 @@ class NotifierController {
     if (channels.length > 0) {
       const notification = new Notification.User(request, scope, content);
 
-      this.kuzzle.pipe('notify:user', notification)
+      return this.kuzzle.pipe('notify:user', notification)
         .then(updatedNotification => this._dispatch(channels, updatedNotification))
         .catch(error => this.kuzzle.emit('log:error', error));
     }
+
+    return Bluebird.resolve();
   }
 
   /**
    * Set something in Redis cache with a TTL if it's provided.
-   * Use the ttl passed in parameter or the default value set in limits.subscriptionDocumentTTL.
+   * Use the ttl passed in parameter or the default value set in
+   * limits.subscriptionDocumentTTL.
    *
    * @param {string} key - Redis key to use
    * @param {string} value - Value to store
-   * @param {integer} ttl - TTL value for the key, use limits.subscriptionDocumentTTL by default.
+   * @param {integer} ttl - TTL value for the key, use
+   *                        limits.subscriptionDocumentTTL by default.
+   * @return {Promise}
    */
-  _setCacheWithTTL (key, value, ttl = this.kuzzle.config.limits.subscriptionDocumentTTL) {
+  _setCacheWithTTL (
+    key, value, ttl = this.kuzzle.config.limits.subscriptionDocumentTTL
+  ) {
     if (ttl === 0) {
-      return this.kuzzle.services.list.internalCache.set(key, value);
+      return this.cache.set(key, value);
     }
 
-    return this.kuzzle.services.list.internalCache.setex(key, ttl, value);
+    return this.cache.setex(key, ttl, value);
   }
 }
 

lib/api/controllers/documentController.js
@@ -170,8 +170,7 @@ class DocumentController extends BaseController {
    * @returns {Promise<Object>}
    */
   create(request) {
-    /** @type KuzzleRequest */
-    let modifiedRequest;
+    let modifiedRequest, response;
 
     assertHasBody(request);
     assertHasIndexAndCollection(request);
@@ -182,15 +181,16 @@ class DocumentController extends BaseController {
         modifiedRequest = newRequest;
 
         // @deprecated - Notifications before action will be removed in Kuzzle v2
-        this.kuzzle.notifier.publish(modifiedRequest, 'unknown', 'pending');
-
-        return this.engine.create(modifiedRequest);
+        return this.kuzzle.notifier.publish(
+          modifiedRequest, 'unknown', 'pending');
       })
-      .then(response => {
-        this.kuzzle.notifier.notifyDocumentCreate(modifiedRequest, response);
-
-        return response;
-      });
+      .then(() => this.engine.create(modifiedRequest))
+      .then(_response => {
+        response = _response;
+        return this.kuzzle.notifier.notifyDocumentCreate(
+          modifiedRequest, _response);
+      })
+      .then(() => response);
   }
 
   /**
@@ -211,8 +211,7 @@ class DocumentController extends BaseController {
    * @returns {Promise<Object>}
    */
   createOrReplace(request) {
-    /** @type KuzzleRequest */
-    let modifiedRequest;
+    let modifiedRequest, response;
 
     assertHasBody(request);
     assertHasIndexAndCollection(request);
@@ -224,22 +223,20 @@ class DocumentController extends BaseController {
         modifiedRequest = newRequest;
 
         // @deprecated - Notifications before action will be removed in Kuzzle v2
-        this.kuzzle.notifier.publish(modifiedRequest, 'unknown', 'pending');
-
-        return this.engine.createOrReplace(modifiedRequest);
+        return this.kuzzle.notifier.publish(
+          modifiedRequest, 'unknown', 'pending');
       })
-      .then(response => {
-        this.kuzzle.indexCache.add(modifiedRequest.input.resource.index, modifiedRequest.input.resource.collection);
+      .then(() => this.engine.createOrReplace(modifiedRequest))
+      .then(_response => {
+        response = _response;
 
         if (response.created) {
-          this.kuzzle.notifier.notifyDocumentCreate(request, response);
-        }
-        else {
-          this.kuzzle.notifier.notifyDocumentReplace(request);
+          return this.kuzzle.notifier.notifyDocumentCreate(request, response);
         }
 
-        return response;
-      });
+        return this.kuzzle.notifier.notifyDocumentReplace(request);
+      })
+      .then(() => response);
   }
 
   /**
@@ -260,8 +257,7 @@ class DocumentController extends BaseController {
    * @returns {Promise<Object>}
    */
   update(request) {
-    /** @type KuzzleRequest */
-    let modifiedRequest;
+    let modifiedRequest, response;
 
     assertHasBody(request);
     assertHasIndexAndCollection(request);
@@ -273,11 +269,11 @@ class DocumentController extends BaseController {
 
         return this.engine.update(modifiedRequest);
       })
-      .then(response => {
-        this.kuzzle.notifier.notifyDocumentUpdate(modifiedRequest);
-
-        return response;
-      });
+      .then(_response => {
+        response = _response;
+        return this.kuzzle.notifier.notifyDocumentUpdate(modifiedRequest);
+      })
+      .then(() => response);
   }
 
   /**
@@ -298,8 +294,7 @@ class DocumentController extends BaseController {
    * @returns {Promise<Object>}
    */
   replace(request) {
-    /** @type KuzzleRequest */
-    let modifiedRequest;
+    let modifiedRequest, response;
 
     assertHasBody(request);
     assertHasIndexAndCollection(request);
@@ -310,15 +305,15 @@ class DocumentController extends BaseController {
         modifiedRequest = newRequest;
 
         // @deprecated - Notifications before action will be removed in Kuzzle v2
-        this.kuzzle.notifier.publish(modifiedRequest, 'unknown', 'pending');
-
-        return this.engine.replace(modifiedRequest);
+        return this.kuzzle.notifier.publish(
+          modifiedRequest, 'unknown', 'pending');
       })
-      .then(response => {
-        this.kuzzle.notifier.notifyDocumentReplace(modifiedRequest);
-
-        return response;
-      });
+      .then(() => this.engine.replace(modifiedRequest))
+      .then(_response => {
+        response = _response;
+        return this.kuzzle.notifier.notifyDocumentReplace(modifiedRequest);
+      })
+      .then(() => response);
   }
 
   /**
@@ -342,15 +337,17 @@ class DocumentController extends BaseController {
     assertHasIndexAndCollection(request);
     assertHasId(request);
 
+    let response;
+
     // @deprecated - Notifications before action will be removed in Kuzzle v2
-    this.kuzzle.notifier.publish(request, 'unknown', 'pending');
-
-    return this.engine.delete(request)
-      .then(response => {
-        this.kuzzle.notifier.notifyDocumentMDelete(request, [response._id]);
-
-        return response;
-      });
+    return this.kuzzle.notifier.publish(request, 'unknown', 'pending')
+      .then(() => this.engine.delete(request))
+      .then(_response => {
+        response = _response;
+        return this.kuzzle.notifier.notifyDocumentMDelete(
+          request, [response._id]);
+      })
+      .then(() => response);
   }
 
   /**
@@ -366,15 +363,20 @@ class DocumentController extends BaseController {
     assertBodyAttributeType(request, 'ids', 'array');
     assertHasIndexAndCollection(request);
 
+    let response;
+
     return this.engine.mdelete(request)
-      .then(response => {
+      .then(_response => {
+        response = _response;
+
         if (response.error.length > 0) {
           request.setError(new PartialError('Some document creations failed', response.error));
         }
 
-        this.kuzzle.notifier.notifyDocumentMDelete(request, response.result);
-        return response.result;
-      });
+        return this.kuzzle.notifier.notifyDocumentMDelete(
+          request, response.result);
+      })
+      .then(() => response.result);
   }
 
   /**
@@ -390,11 +392,9 @@ class DocumentController extends BaseController {
     assertHasIndexAndCollection(request);
 
     return this.engine.deleteByQuery(request)
-      .then(response => {
-        this.kuzzle.notifier.notifyDocumentMDelete(request, response.ids);
-
-        return response;
-      });
+      .then(response =>
+        this.kuzzle.notifier.notifyDocumentMDelete(request, response.ids)
+          .then(() => response));
   }
 
   /**
@@ -425,15 +425,21 @@ class DocumentController extends BaseController {
     assertBodyAttributeType(request, 'documents', 'array');
     assertHasIndexAndCollection(request);
 
+    let response;
+
     return this.engine[methodName](request)
-      .then(response => {
+      .then(_response => {
+        response = _response;
+
         if (response.error.length > 0) {
-          request.setError(new PartialError('Some document creations failed', response.error));
+          request.setError(
+            new PartialError('Some document creations failed', response.error));
         }
 
-        this.kuzzle.notifier.notifyDocumentMChanges(request, response.result, cached);
-        return {hits: response.result, total: response.result.length};
-      });
+        return this.kuzzle.notifier.notifyDocumentMChanges(
+          request, response.result, cached);
+      })
+      .then(() => ({hits: response.result, total: response.result.length}));
   }
 }
 

lib/api/controllers/realtimeController.js
@@ -70,10 +70,13 @@ class RealtimeController extends BaseController {
     assertHasBody(request);
     assertBodyHasAttribute(request, 'roomId');
 
-    const result = this.kuzzle.hotelClerk.join(request);
-    this.kuzzle.tokenManager.link(request.context.token, request.context.connection.id, result.roomId);
+    return this.kuzzle.hotelClerk.join(request)
+      .then(result => {
+        this.kuzzle.tokenManager.link(
+          request.context.token, request.context.connection.id, result.roomId);
 
-    return Bluebird.resolve(result);
+        return result;
+      });
   }
 
   /**
@@ -84,9 +87,11 @@ class RealtimeController extends BaseController {
     assertHasBody(request);
     assertBodyHasAttribute(request, 'roomId');
 
-    const roomId = this.kuzzle.hotelClerk.removeSubscription(request);
-    this.kuzzle.tokenManager.unlink(request.context.token, roomId);
-    return Bluebird.resolve(roomId);
+    return this.kuzzle.hotelClerk.removeSubscription(request)
+      .then(roomId => {
+        this.kuzzle.tokenManager.unlink(request.context.token, roomId);
+        return roomId;
+      });
   }
 
   /**
@@ -127,7 +132,8 @@ class RealtimeController extends BaseController {
         };
 
         return this.kuzzle.notifier.publish(newRequest, 'in', 'done');
-      });
+      })
+      .then(() => ({published: true}));
   }
 
   /**

lib/api/core/auth/tokenManager.js
@@ -22,7 +22,7 @@
 'use strict';
 
 const
-  RequestContext = require('kuzzle-common-objects').models.RequestContext,
+  { models: { RequestContext } } = require('kuzzle-common-objects'),
   SortedArray = require('sorted-array');
 
 /*
@@ -192,26 +192,30 @@ class TokenManager {
   }
 
   checkTokensValidity() {
-    const now = Date.now();
+    const arr = this.tokens.array;
 
-    while (this.tokens.array.length > 0 && this.tokens.array[0].expiresAt < now) {
-      const connectionId = this.tokens.array[0].connectionId;
+    if (arr.length > 0 && arr[0].expiresAt < Date.now()) {
+      const connectionId = arr[0].connectionId;
+
+      arr.shift();
 
       // Send a token expiration notification to the user
-      if (this.kuzzle.hotelClerk.customers[connectionId]) {
-        const rooms = Object.keys(this.kuzzle.hotelClerk.customers[connectionId]);
-        this.kuzzle.notifier.notifyServer(rooms, connectionId, 'TokenExpired', 'Authentication Token Expired');
-        this.kuzzle.hotelClerk.removeCustomerFromAllRooms(new RequestContext({
-          connection: {
-            id: connectionId
-          }
-        }));
+      const customer = this.kuzzle.hotelClerk.customers[connectionId];
+      if (customer) {
+        const rooms = Object.keys(customer);
+
+        return this.kuzzle.notifier
+          .notifyServer(
+            rooms, connectionId, 'TokenExpired', 'Authentication Token Expired')
+          .then(() => this.kuzzle.hotelClerk.removeCustomerFromAllRooms(
+            new RequestContext({ connection: { id: connectionId } })))
+          .then(() => this.checkTokensValidity());
       }
 
-      this.tokens.array.shift();
+      return this.checkTokensValidity();
     }
 
-    if (this.tokens.array.length > 0) {
+    if (arr.length > 0) {
       this.runTimer();
     }
   }

commit 0a5774f1ab5fcbbbf0fef68a2884bb1c0f6c71d7

[hotfix] notifier `cache` and `storage` set to `undefined` (#1316)
## What does this PR do ?

Fix notifier issue introduced by https://github.com/kuzzleio/kuzzle/pull/1311
Kuzzle notifier is instanciated before the services are and `notifier.cache` and `storage` properties were set to `undefined`.

### How should this be manually tested?

```
bash features/run.sh
```

.travis.yml
@@ -192,7 +192,7 @@ jobs:
       script:
         - docker-compose -f .ci/test.yml run kuzzle
 
-    - stage: "Redis old versions tests"
+    - stage: "Redis tests"
       if: type = cron
       name: "Redis 4"
 
@@ -260,6 +260,6 @@ jobs:
 notifications:
   slack:
     rooms:
-    - secure: "MlWnTEJwmn4Wq6sH2PRfrLRPhwIiqPUNB9AuulKKXheTfY74zHvlCe0TAUM5x8IaccJT4GKG7tnpLU4ujzNyL4AbqzCkUjZvaq2+NT1YJ4xJ2AXe4NDjuj8Cr0GyeotJA6FMHLAyDtGbVvuomktUp49pL7+QbG5kcUMtZ2wQ1swfWlk9zf8xH4lpO7Nyd96xXj7fpZaWGM06VCloJ45wxLmvUOEY5nDEoZ0LyOFyH8zx4WuNZH7jkp0JFQwiuFp+VpyPhnynBcMyyUi7rlrJxfqD3XfffQ0foE1TH2S5TuWZ3IuMjZUaxZ0xNA42hVfmvPLGppLoNNuaL1rPsVHSvSNWkdAKbFQDhi/O3PskOFKiMzXkPgSFNY4CyHiCVI78s3Gi1qWK9StD/Pzu1vs7mWFh7ysQeDZnrK1sz/IDA/sZPfF4ZK6ZKCYqGOZtH3wuc8AsGcyrgigT1cRxlXUblI8z0Phu1D3gCETCpqrcngT6cxlpy/PnBm3upLE1dfN/1OzBP+wwGgblLmAN47O5HsA4R76VNR6gJ55A5hwdGmLqe4Vc0yUE5EC88OfnSZ3YHSqNTjF7gko4hsoRNqPZBon7WX0HQwo0SjO8HyzW+kOVzMcTt6Csbdt1j96Jo/AqQNrl4HNXLXdRJzQAsSUtlUSe4UPS5GIKHQns0Cffvls="
+    - secure: "nx2W8a0wTPUfMFfR0TQxoA+M0ExUAJxIBy1AfKRlJXHaXN3al+SjLNXDU/OdplN9doae8BhcpHJBLiCDfQFY5Wmmbuxq2wAWhrPduIMPZFttBuxkoWkAbrhzYPsk5t7vERrJbSAynNbKeVL1gj7zitStJxzJzT2Z8y//9KYwXty3hPMJei2R1GCLTIOYp0ddq1Uu02Sdhkg07IO+bCrv5q6NFOpnx27SjYEBIOX66bVnGUcUzrbFQM1WToHwFHU3ylZUbj+zJHu+njj9SNTaoR7nIl6oedHCtloagTxyqjLQ0k3E9O4D5fuVvanKDAnLXrlPHWwNoEjffUMQ/b8OtTpgjB6pTr8Xu5oMN4tb5AbofdFOYIyumhHEN7Yt9t1sI6mYMEPu8GfAUTNHS41QfJakGRjYWOMoWQD4sOwRi8VRx+wOkvGSF75wsIrRM30qOkU4cXjoe44My7OkJKycz5AlvR8Drow5d8FO7N4C8olNM4MyP1Ofs2ulXYVEXEXV9NxuRIpcejx8UUZfBNjHnLsAjgq4sWyey22vTxIlmzc1dN3AngiScNdU7u2emZVBmC9VSLN2hAduA1y7HxESpqxd7+9v8qKxeSYw2j1V1sh2nvOvKah2Y61erDWjOI/eAxQPnT/uF4yoHC3dFnW9j9tCej0vZ0PeIBCUIKb5mx8="
     on_success: never
     on_failure: always

lib/api/core/notifier.js
@@ -36,8 +36,14 @@ const
 class NotifierController {
   constructor(kuzzle) {
     this.kuzzle = kuzzle;
-    this.cache = kuzzle.services.list.internalCache;
-    this.storage = kuzzle.services.list.storageEngine;
+  }
+
+  get cache () {
+    return this.kuzzle.services.list.internalCache;
+  }
+
+  get storage () {
+    return this.kuzzle.services.list.storageEngine;
   }
 
   /**