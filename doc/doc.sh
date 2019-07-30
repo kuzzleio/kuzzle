6379b14b81285d4b60fd3ca230cfda0309cd8a25
Aschen <amaret93@gmail.com>
2019-07-30 18:43:57 +0200

    rm old framework

--- doc/doc.sh
+++ doc/doc.sh
@@ -21,6 +21,7 @@ fi
 case $1 in
   prepare)
     echo "Clone documentation framework"
+    rm -rf framework/
     git clone --depth 10 --single-branch --branch master https://github.com/kuzzleio/documentation.git framework/
 
     echo "Link local doc for dead links checking"
