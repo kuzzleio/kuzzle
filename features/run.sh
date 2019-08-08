42eeca6c95b42a09dd6c02c4c121cc7ecebcbf93
Aschen <amaret93@gmail.com>
2019-08-08 15:59:00 +0200

    revert deleting proxy test

--- features/run.sh
+++ features/run.sh
@@ -4,13 +4,24 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 set -e
 
-host="${CUCUMBER_EMBEDDED_HOST:-localhost}"
-port="${CUCUMBER_EMBEDDED_PORT:-7512}"
+proxy_host="${CUCUMBER_PROXY_HOST:-localhost}"
+embedded_host="${CUCUMBER_EMBEDDED_HOST:-localhost}"
+proxy_port="${CUCUMBER_PROXY_PORT:-7513}"
+embedded_port="${CUCUMBER_EMBEDDED_PORT:-7512}"
 
-for protocol in websocket http socketio; do
+for endpoint in Embedded Proxy; do
+  for protocol in websocket http socketio; do
+    if [ "$endpoint" == "Proxy" ]; then
+      host="$proxy_host"
+      port="$proxy_port"
+    else
+      host="$embedded_host"
+      port="$embedded_port"
+    fi
 
-  # profiles are defined in the cucumber.js file at the root of this project
-  ./node_modules/.bin/cucumber-js --format progress-bar --profile "${protocol}" --world-parameters "{\"host\": \"${host}\", \"port\": \"${port}\"}"
+    # profiles are defined in the cucumber.js file at the root of this project
+    ./node_modules/.bin/cucumber-js --format progress-bar --profile "${protocol}${endpoint}" --world-parameters "{\"host\": \"${host}\", \"port\": \"${port}\"}"
+  done
 done
 
 # mqtt
