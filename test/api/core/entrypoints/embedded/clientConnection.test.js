8d09b5ad4d19ebf4f3f98aa9b42b7ad1d2dcb863
Aschen <amaret93@gmail.com>
2019-08-28 14:00:25 +0200

    frozen connection: unit tests

--- test/api/core/entrypoints/embedded/clientConnection.test.js
+++ test/api/core/entrypoints/embedded/clientConnection.test.js
@@ -4,18 +4,34 @@ const
 
 describe('core/clientConnection', () => {
   describe('#constructor', () => {
+    let
+      headers,
+      connection;
+
+    beforeEach(() => {
+      headers = { foo: 'bar' };
+      connection =
+        new ClientConnection('protocol', ['ip1', 'ip2'], headers);
+    });
+
     it('should throw if ips is not an array', () => {
       return should(() => new ClientConnection('protocol', 'ips'))
         .throw(TypeError, {message: 'Expected ips to be an Array, got string'});
     });
 
     it('should set headers', () => {
-      const
-        headers = {foo: 'bar'},
-        connection = new ClientConnection('protocol', ['ip1', 'ip2'], headers);
-
-      should(connection.headers)
-        .be.exactly(headers);
+      should(connection.headers).be.exactly(headers);
     });
+
+    it('should be frozen', () => {
+      connection.id = 'not-set';
+      connection.protocol = 'not-set';
+      connection.headers.foo = 'not-set'
+
+      should(() => connection.ips.push('not-pushed')).throw(TypeError);
+      should(connection.headers.foo).not.be.eql('not-set');
+      should(connection.id).not.be.eql('not-set');
+      should(connection.protocol).not.be.eql('not-set');
+    })
   });
 });
