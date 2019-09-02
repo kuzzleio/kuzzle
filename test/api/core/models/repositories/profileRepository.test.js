bc7cf0cad72cc3de5e37d7379c1e978b64322bc6
Yoann Abbes <yoann1.abbes@epitech.eu>
2019-09-02 11:51:17 +0200

    unit test

--- test/api/core/models/repositories/profileRepository.test.js
+++ test/api/core/models/repositories/profileRepository.test.js
@@ -414,10 +414,11 @@ describe('Test: repositories/profileRepository', () => {
       invalidProfile.policies = [{roleId: 'notSoAwesomeRole'}];
 
       kuzzle.repositories.role.loadRoles = sinon.stub().rejects();
-      return profileRepository.validateAndSaveProfile(invalidProfile)
-        .catch(e => {
-          should(e).be.instanceOf(NotFoundError, { message: 'Unable to hydrate the profile awesomeRole: missing role(s) in the database'});
-        });
+
+      return should(profileRepository.validateAndSaveProfile(invalidProfile)).be.rejectedWith(
+        NotFoundError, { message: 'Unable to hydrate the profile awesomeProfile: missing role(s) in the database'
+      });
+
     });
 
     it('should properly persist the profile and trigger a "core:profileRepository:save" event when ok', () => {
