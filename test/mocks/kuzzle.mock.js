63404dfc8326cccc09b4cbf0da692d33a0bcb94c
jenow <jenova.ke20@gmail.com>
2019-09-02 10:22:39 +0200

    add methods in mock

--- test/mocks/kuzzle.mock.js
+++ test/mocks/kuzzle.mock.js
@@ -373,6 +373,8 @@ class KuzzleMock extends Kuzzle {
 
     this.vault = {
       init: this.sandbox.stub(),
+      encryptObject: this.sandbox.stub(),
+      decryptObject: this.sandbox.stub(),
       prepareCrypto: this.sandbox.stub(),
       secrets: {
         aws: {
