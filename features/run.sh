c874ba8b66666e58797bf942d2bdbc06f9c669f3
Aschen <amaret93@gmail.com>
2019-10-14 08:21:10 +0200

    Fix functionnal test

--- features/run.sh
+++ features/run.sh
@@ -4,19 +4,13 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 set -e
 
-host="${CUCUMBER_EMBEDDED_HOST:-localhost}"
-port="${CUCUMBER_EMBEDDED_PORT:-7512}"
+# profiles are defined in the cucumber.js file at the root of this project
 
-for protocol in websocket http socketio; do
-  # profiles are defined in the cucumber.js file at the root of this project
-  ./node_modules/.bin/cucumber-js \
-    --format progress-bar \
-    --profile "${protocol}" \
-    --world-parameters "{\"host\": \"${host}\", \"port\": \"${port}\"}"
-done
+# http
+./node_modules/.bin/cucumber-js --format progress-bar --profile "http"
+
+# websocket
+./node_modules/.bin/cucumber-js --format progress-bar --profile "websocket"
 
 # mqtt
-./node_modules/.bin/cucumber-js \
-  --format progress-bar \
-  --profile "mqtt" \
-  --world-parameters "{\"host\": \"${host}\"}"
+./node_modules/.bin/cucumber-js --format progress-bar --profile "mqtt"
