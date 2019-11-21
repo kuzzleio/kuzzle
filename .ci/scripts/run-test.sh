d04aa723add31afd33cd9fb031909d3b22b8f9dd
scottinet <scottinet@protonmail.com>
2019-11-21 12:02:38 +0100

    nothing to see here -_-

--- .ci/scripts/run-test.sh
+++ .ci/scripts/run-test.sh
@@ -34,7 +34,7 @@ timeout 30 bash -c "
   done"
 
 echo "[$(date --rfc-3339 seconds)] - Elasticsearch is up. Waiting for shards to be active (can take a while)"
-E=$(curl -s "$elastic_host/_cluster/health?wait_for_status=yellow=1&timeout=60s")
+E=$(curl -s "$elastic_host/_cluster/health?wait_for_status=yellow&timeout=60s")
 
 if ! (echo ${E} | grep -E '"status":"(yellow|green)"' > /dev/null); then
     echo "============ Cluster health response"
