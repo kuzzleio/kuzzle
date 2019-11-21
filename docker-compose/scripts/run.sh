0e4345112c16c55a7153cc8135e5df8cd9946e53
scottinet <scottinet@protonmail.com>
2019-11-21 11:07:23 +0100

    Remove superfluous shard check when checking connection to the ES stack

--- docker-compose/scripts/run.sh
+++ docker-compose/scripts/run.sh
@@ -14,11 +14,9 @@ do
     log "Still trying to connect to $elastic_host"
     sleep 1
 done
-# create a tmp index just to force the shards to init
-curl -XPUT -s -o /dev/null "$elastic_host/%25___tmp"
+
 log "Elasticsearch is up. Waiting for shards..."
-E=$(curl -s "$elastic_host/_cluster/health?wait_for_status=yellow&wait_for_active_shards=1&timeout=60s")
-curl -XDELETE -s -o /dev/null "$elastic_host/%25___tmp"
+E=$(curl -s "$elastic_host/_cluster/health?wait_for_status=yellow&timeout=60s")
 
 if ! (echo ${E} | grep -E '"status":"(yellow|green)"' > /dev/null); then
     log "Could not connect to elasticsearch in time. Aborting..."
