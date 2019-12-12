f98d7d13dd5fb07f8f9ca39c01422bb608cd5537
scottinet <scottinet@protonmail.com>
2019-12-12 15:07:30 +0100

    [sonar] testing new parameters

--- .ci/sonar.sh
+++ .ci/sonar.sh
@@ -7,6 +7,8 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 echo "=== Running sonar scanner"
 
 # Unset travis pre-set parameters, which are more harmful than anything
+echo $SONARQUBE_SCANNER_PARAMS
+
 unset SONARQUBE_SCANNER_PARAMS
 unset SONARQUBE_SKIPPED
 
@@ -15,24 +17,22 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 		-Dsonar.host.url=https://sonarqube.kaliop.net \
 		-Dsonar.projectKey=kuzzle:server \
 		-Dsonar.sources=lib \
-		-Dsonar.analysis.mode=preview \
 		-Dsonar.projectVersion="$PACKAGE_VERSION" \
 		-Dsonar.login="$SONAR_TOKEN" \
 		-Dsonar.language=js \
-		-Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
+		-Dsonar.pullrequest.key="$TRAVIS_PULL_REQUEST" \
 		-Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-		-Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+		-Dsonar.github.repository="$TRAVIS_REPO_SLUG" \
+		-X
 else
 	sonar-scanner \
 		-Dsonar.host.url=https://sonarqube.kaliop.net \
 		-Dsonar.projectKey=kuzzle:server \
 		-Dsonar.sources=lib \
-		-Dsonar.analysis.mode=preview \
 		-Dsonar.projectVersion="$PACKAGE_VERSION" \
 		-Dsonar.login="$SONAR_TOKEN" \
 		-Dsonar.language=js \
 		-Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-		-Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+		-Dsonar.github.repository="$TRAVIS_REPO_SLUG" \
+		-X
 fi
-
-
