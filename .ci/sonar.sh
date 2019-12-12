bf5929434326e89c2abe86b1d002b23120bde064
scottinet <scottinet@protonmail.com>
2019-12-12 10:09:29 +0100

    [sonar] update cli parameters

--- .ci/sonar.sh
+++ .ci/sonar.sh
@@ -15,24 +15,22 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 		-Dsonar.host.url=https://sonarqube.kaliop.net \
 		-Dsonar.projectKey=kuzzle:server \
 		-Dsonar.sources=lib \
-		-Dsonar.analysis.mode=preview \
 		-Dsonar.projectVersion="$PACKAGE_VERSION" \
 		-Dsonar.login="$SONAR_TOKEN" \
 		-Dsonar.language=js \
 		-Dsonar.github.pullRequest="$TRAVIS_PULL_REQUEST" \
 		-Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-		-Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+		-Dsonar.github.repository="$TRAVIS_REPO_SLUG" \
+		-X
 else
 	sonar-scanner \
 		-Dsonar.host.url=https://sonarqube.kaliop.net \
 		-Dsonar.projectKey=kuzzle:server \
 		-Dsonar.sources=lib \
-		-Dsonar.analysis.mode=preview \
 		-Dsonar.projectVersion="$PACKAGE_VERSION" \
 		-Dsonar.login="$SONAR_TOKEN" \
 		-Dsonar.language=js \
 		-Dsonar.github.oauth="$SONAR_GITHUB_TOKEN" \
-		-Dsonar.github.repository="$TRAVIS_REPO_SLUG"
+		-Dsonar.github.repository="$TRAVIS_REPO_SLUG" \
+		-X
 fi
-
-
