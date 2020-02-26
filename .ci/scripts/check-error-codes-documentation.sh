1fb5b34f64ff8e17e151b735a66acbf7585dfee3
scottinet <scottinet@protonmail.com>
2020-02-26 08:43:33 +0100

    fix test script -_-

--- .ci/scripts/check-error-codes-documentation.sh
+++ .ci/scripts/check-error-codes-documentation.sh
@@ -5,12 +5,12 @@ set -e
 echo "Checking error codes..."
 
 TMPDIR=$TRAVIS_BUILD_DIR/codes
-
 mkdir $TMPDIR
 
+cd $TRAVIS_BUILD_DIR
 npm run doc-error-codes -- --output $TMPDIR
 
-for f in doc/2/api/essentials/error-codes/*
+for f in $TRAVIS_BUILD_DIR/doc/2/api/essentials/error-codes/*
 do
   if [ -d "$f" ]; then
     diff $f $TMPDIR/$(basename $f)
