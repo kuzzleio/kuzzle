41afc9e74beed6576b0e3fdf8afc2fd518bf49d2
Aschen <adrien@maret.email>
2020-03-03 06:16:42 +0100

    Revert "Add beforeDocumentWrite"
    This reverts commit d53ac00d6e476b57491621d82cdbb5097a377417.

--- plugins/available/functional-test-plugin/index.js
+++ plugins/available/functional-test-plugin/index.js
@@ -42,21 +42,9 @@ class FunctionalTestPlugin {
     this.pipes['generic:document:afterDelete'] = (...args) => this.genericDocumentEvent('afterDelete', ...args);
   }
 
-  async init (config, context) {
+  init (config, context) {
     this.config = config;
     this.context = context;
-
-    // context.onDocument* related declarations ================================
-
-    const filters = { exists: 'name' };
-
-    await this.context.accessors.beforeDocumentWrite('test', 'test', filters, async (document, request) => {
-      console.log({ document });
-
-      document._source.age = 42;
-
-      return document;
-    });
   }
 
   // context.constructor.ESClient related methods ============================
