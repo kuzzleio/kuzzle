a66d149f26bf741962ff04309289ba99dac16451
Sébastien Cottinet <scottinet@protonmail.com>
2020-03-05 11:35:56 +0100

    Split error codes documentation (#1575)
    # Description
    
    Splits the error codes documentation to make it compatible with algolia (the current page is too large for it).
    
    Current page: https://docs.kuzzle.io/core/2/api/essentials/errors/codes/
    
    Changes details:
    
    * The "Error handling" page is now directly available under the "Essentials" group: https://docs.kuzzle.io/core/2/api/essentials/error-handling
    * The "Error codes" is now directly available under the "Essentials" group: https://docs.kuzzle.io/core/2/api/essentials/error-codes
      * The "Error codes" now contains 1 section per error domain: "api", "core", "services", ...
      * Error codes pages are sorted by domain code
    * A new CI script has been added to make sure that the documentation is up-to-date (replacing the current check which only had a diff on a single file to do)
    
    # How to review
    
    Most of the changes are from auto-generated documentation. 
    You should only review non-markdown files. For markdown ones, sync the branch and run `npm run doc-dev` to have a better view of what changes in the documentation.

--- doc/build-error-codes.js
+++ doc/build-error-codes.js
@@ -2,7 +2,7 @@
  * Kuzzle, a backend software, self-hostable and ready to use
  * to power modern apps
  *
- * Copyright 2015-2018 Kuzzle
+ * Copyright 2015-2020 Kuzzle
  * mailto: support AT kuzzle.io
  * website: http://kuzzle.io
  *
@@ -19,70 +19,84 @@
  * limitations under the License.
  */
 
-const
-  codes = require(`${__dirname}/../lib/config/error-codes/`),
-  fs = require('fs'),
-  { errors } = require('kuzzle-common-objects');
+'use strict';
 
-const header = `---
-code: false
+const path = require('path');
+const fs = require('fs');
+const { domains } = require(`${__dirname}/../lib/config/error-codes/`);
+const { errors } = require('kuzzle-common-objects');
+
+function getHeader(title) {
+  return `---
+code: true
 type: page
-title: Codes
+title: "${title}"
 description: error codes definitions
-order: 500
 ---
 
 [//]: # (This documentation is auto-generated)
 [//]: # (If you need to update this page, execute: npm run doc-error-codes)
 
-# Error codes
-
+# ${title}
 
 `;
+}
 
-function buildErrorCodes(domains) {
-  const
-    buffer = Buffer.allocUnsafe(4),
-    domainKeys = Object
-      .keys(domains)
-      .sort((a, b) => domains[a].code - domains[b].code);
-  let doc = header;
+function rimraf (dir) {
+  for (const entry of fs.readdirSync(dir, {withFileTypes: true})) {
+    const fulldir = path.join(dir, entry.name);
 
-  for (const domainName of domainKeys) {
-    const domain = domains[domainName];
+    if (entry.isDirectory()) {
+      rimraf(fulldir);
+    }
+    else {
+      fs.unlinkSync(fulldir);
+    }
+  }
 
-    if (domain.deprecated) {
+  fs.rmdirSync(dir);
+}
+
+function clearCodeDirectories (target) {
+  for (const entry of fs.readdirSync(target, {withFileTypes: true})) {
+    if (entry.isDirectory()) {
+      rimraf(path.join(target, entry.name));
+    }
+  }
+}
+
+function buildErrorCodes (name) {
+  const domain = domains[name];
+
+  if (domain.deprecated) {
+    return null;
+  }
+
+  const buffer = Buffer.allocUnsafe(4);
+  buffer.writeUInt8(domain.code, 3);
+
+  let doc = getHeader(`0x${buffer.toString('hex', 3)}: ${name}`);
+
+  for (const [subname, subdomain] of Object.entries(domain.subdomains)) {
+    if (subdomain.deprecated) {
       continue;
     }
 
-    buffer.writeUInt8(domain.code, 3);
-    doc += `\n## 0x${buffer.toString('hex', 3)}: ${domainName}\n\n`;
+    buffer.writeUInt16BE(domain.code << 8 | subdomain.code, 2);
 
-    for (const subdomainName of Object.keys(domain.subdomains)) {
-      const subdomain = domain.subdomains[subdomainName];
+    doc += `\n\n### Subdomain: 0x${buffer.toString('hex', 2)}: ${subname}\n\n`;
+    doc += '| Id | Error Type (Status Code)             | Message           |\n| ------ | -----------------| ------------------ | ------------------ |\n';
 
-      if (subdomain.deprecated) {
-        continue;
+    for (const [errname, error] of Object.entries(subdomain.errors)) {
+      if (!error.deprecated) {
+        const fullName = `${name}.${subname}.${errname}`;
+        const status = (new errors[error.class]()).status;
+
+        buffer.writeUInt32BE(
+          domain.code << 24 | subdomain.code << 16 | error.code,
+          0);
+        doc += `| ${fullName}<br/><pre>0x${buffer.toString('hex')}</pre> | [${error.class}](/core/2/api/essentials/error-handling#${error.class.toLowerCase()}) <pre>(${status})</pre> | ${error.description} |\n`;
       }
-
-      buffer.writeUInt16BE(domain.code << 8 | subdomain.code, 2);
-
-      doc += `\n\n### Subdomain: 0x${buffer.toString('hex', 2)}: ${subdomainName}\n\n`;
-      doc += '| Id | Error Type (Status Code)             | Message           |\n| ------ | -----------------| ------------------ | ------------------ |\n';
-
-      for (const [errorName, error] of Object.entries(subdomain.errors)) {
-        const
-          fullName = `${domainName}.${subdomainName}.${errorName}`,
-          status = (new errors[error.class]()).status;
-
-        if (!error.deprecated) {
-          buffer.writeUInt32BE(
-            domain.code << 24 | subdomain.code << 16 | error.code,
-            0);
-          doc += `| ${fullName}<br/><pre>0x${buffer.toString('hex')}</pre> | [${error.class}](/core/2/api/essentials/errors/handling#${error.class.toLowerCase()}) <pre>(${status})</pre> | ${error.description} |\n`;
-        }
-      }
-      doc += '\n---\n';
     }
     doc += '\n---\n';
   }
@@ -90,10 +104,23 @@ function buildErrorCodes(domains) {
   return doc;
 }
 
-const output = process.argv[2] === '-o' || process.argv[2] === '--output'
-  ? process.argv[3]
-  : `${__dirname}/2/api/essentials/errors/codes/index.md`;
+function run () {
+  const output = process.argv[2] === '-o' || process.argv[2] === '--output'
+    ? process.argv[3]
+    : `${__dirname}/2/api/essentials/error-codes`;
 
-const doc = buildErrorCodes(codes.domains);
+  clearCodeDirectories(output);
 
-fs.writeFileSync(output, doc);
+  for (const name of Object.keys(domains)) {
+    const doc = buildErrorCodes(name);
+
+    if (doc !== null) {
+      const dirpath = path.join(output, name);
+
+      fs.mkdirSync(dirpath);
+      fs.writeFileSync(path.join(dirpath, 'index.md'), doc);
+    }
+  }
+}
+
+run();
