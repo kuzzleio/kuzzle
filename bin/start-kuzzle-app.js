0cb7f96c05203c777641229958f881a2482360cc
Aschen <adrien@maret.email>
2020-05-26 12:29:24 +0200

    wip

--- bin/start-kuzzle-app.js
+++ bin/start-kuzzle-app.js
@@ -1,44 +1,70 @@
+const S3Plugin = require('kuzzle-plugin-s3');
 const { Application, errors } = require('../index');
 
 const app = new Application('omniscient');
 
-app.version = '1.42.3'
+app.version = '1.42.21'
 
-app.mappings = {
+// Loguer toutes les actions réalisées pour la déclaration de l'app
+app.fixture.import()
+app.fixture.add({
+  // ...
+})
+
+const idx = await sdk.index.list()
+
+for (const index of idx) {
+  if (!index.startsWith('customer'))
+    continue
+  const count = await sdk.document.count(index, 'asset-locations')
+  console.log(index, count)
+}
+
+app.right.import()
+app.user.import('./users.json')
+
+app.index.import('./indexes.json')
+app.index.add({
   'nyc-open-data': {
     'green-taxi': {
       'dynamic': false
-    }
+    },
+    'yellow-taxi': {}
   }
+})
+
+app.pipe.add('server:afterNow', async request => {
+  app.context.log.info(`Server INFO: ${app.name}`);
+
+  request.result.now = (new Date()).toUTCString();
+  return request;
+})
+
+app.hook.add('server:beforeNow', () => {
+  console.log('server before now');
+})
+
+app.api.add('asset', {
+  url: '/asset' // /_/asset
+})
+
+const redisHost = await getRedisHostFromK8S()
+app.config.set('services.redis.host', redisHost)
+
+app.config.merge({
+  realtime: {
+    pcreSupport: true
+  }
+})
+
+const s3Conf = {
+  bucker: 'gordon'
 };
 
-app.pipes = {
-  'server:afterNow': async request => {
-    app.context.log.info(`Server INFO: ${app.name}`);
+const s3Plugin = new S3Plugin(s3Conf)
 
-    request.result.now = (new Date()).toUTCString()
-    return request;
-  },
-  'auth:beforeLogin': () => {
-    throw errors.BadRequestError('Invalid');
-  }
-};
-
-app.hooks = {
-  'server:beforeNow': () => {
-    console.log('server before now');
-  }
-};
-
-app.config.realtime.pcreSupport = true
-
-app.controllers = {
-  'customer': {
-    'create': async request => {
-      console.log(`Create customer ${request.input.args.name}`);
-    }
-  }
-};
+// optional name
+app.plugin.use(s3Plugin, { name: 's3-plugin-eu-west-1' })
 
 app.start()
   .then(() => {
