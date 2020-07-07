e95130a7fb4a1259566b705424fbc3d1d0b77a7b
Aschen <adrien@maret.email>
2020-07-07 07:52:30 +0200

    tests

--- test/core/plugin/context/context.test.js
+++ test/core/plugin/context/context.test.js
@@ -216,7 +216,7 @@ describe('Plugin Context', () => {
 
         should(kuzzle.log[level])
           .calledOnce()
-          .calledWithExactly('[plugin-pluginName] test');
+          .calledWithExactly('[pluginName]: test');
       });
 
       should(context.accessors).be.an.Object().and.not.be.empty();
@@ -230,7 +230,7 @@ describe('Plugin Context', () => {
       process.nextTick(() => {
         should(kuzzle.log.info)
           .be.calledOnce()
-          .be.calledWith('[plugin-pluginName] foobar');
+          .be.calledWith('[pluginName]: foobar');
 
         done();
       });
