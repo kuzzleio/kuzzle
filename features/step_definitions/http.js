eb089b82af59b55fdb6592aef55fcc81638f4e2b
scottinet <scottinet@protonmail.com>
2020-07-10 09:56:08 +0200

    [func tests] add missing await

--- features/step_definitions/http.js
+++ features/step_definitions/http.js
@@ -5,7 +5,7 @@ const { execSync } = require('child_process');
 const { Then } = require('cucumber');
 
 Then('I send the crafted HTTP multipart request', async function () {
-  execSync('cat features/fixtures/bad-multipart.req | nc localhost 7512');
+  await execSync('cat features/fixtures/bad-multipart.req | nc localhost 7512');
 });
 
 Then('Kuzzle is still up', async function () {
