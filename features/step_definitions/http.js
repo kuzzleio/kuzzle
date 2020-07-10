45bdb42506aeb55fbd705e92255d09e292ebd0b3
scottinet <scottinet@protonmail.com>
2020-07-10 10:31:52 +0200

    [func tests] use a nodejs socket instead of "nc"

--- features/step_definitions/http.js
+++ features/step_definitions/http.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const { execSync } = require('child_process');
+const net = require('net');
+const fs = require('fs');
 
 const { Then } = require('cucumber');
 
-Then('I send the crafted HTTP multipart request', async function () {
-  await execSync('cat features/fixtures/bad-multipart.req | nc localhost 7512');
+Then('I send the crafted HTTP multipart request', function (done) {
+  const socket = net.createConnection(7512, () => {
+    const rq = fs.readFileSync('./features/fixtures/bad-multipart.req');
+
+    socket.write(rq.toString(), error => {
+      socket.end();
+      done(error);
+    });
+  });
+
+  socket.on('error', done);
 });
 
 Then('Kuzzle is still up', async function () {
