50faee4ca5e74a0790a46547bd79be98be75cbaf
scottinet <scottinet@protonmail.com>
2020-09-09 10:45:09 +0200

    [ci] execute cluster functional tests OUTSIDE containers

---
+++ .ci/scripts/run-test-cluster.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$NODE_VERSION" ];
+then
+  echo "Missing NODE_VERSION, use default NODE_12_VERSION"
+  export NODE_VERSION=$NODE_12_VERSION
+fi
+
+echo "Testing Kuzzle against node v$NODE_VERSION"
+n $NODE_VERSION
+
+echo "Installing dependencies..."
+npm ci --silent --unsafe-perm
+
+./ci/scripts/install-plugins.sh
+
+echo "[$(date)] - Starting Kuzzle Cluster..."
+
+docker-compose -f ./docker-compose.yml up -d
+
+./bin/wait-kuzzle
+
+npm run test:functional:legacy:cluster
