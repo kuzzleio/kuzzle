dccfc164d91d6fd75ee802cc46ca98ea563555fb
scottinet <scottinet@protonmail.com>
2020-10-09 18:15:49 +0200

    [dev] replace nodemon with a custom reloader

---
+++ docker/scripts/reloader.js
@@ -0,0 +1,137 @@
+/*
+ * For development purposes only: this script is meant to be run on top
+ * of a Kuzzle starter script. It watches source files (json, ts, js) from
+ * Kuzzle core, plugins, configuration files.
+ * When a file changes, it automatically shuts the current process down, WAIT
+ * until it has correctly been stopped (or kills it after some delay), and only
+ * then restarts it.
+ *
+ * This script is meant to replace solutions like nodemon, which have a few
+ * cumbersome caveats.
+ *
+ * Usage: node reloader.js <config file> <node args> <starter script> <args>
+ *
+ * With:
+ *   - <config file>: this script configuration file
+ *   - <node args>: arguments to pass to the node interpreter
+ *   - <starter script>: the script to execute using node
+ *   - <args>: script arguments
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { fork } = require('child_process');
+
+const chokidar = require('chokidar');
+const jsonStrip = require('strip-json-comments');
+const clc = require('cli-color');
+
+const stateEnum = Object.freeze({
+  RUNNING: 1,
+  STOPPED: 2,
+  STOPPING: 3,
+});
+
+const config = loadConfig();
+const { nodeArgs, script, scriptArgs } = parseArgs();
+
+let childProcess;
+let state = stateEnum.STOPPED;
+
+function parseArgs () {
+  const args = process.argv.slice(3);
+  const idx = args.findIndex(arg => arg.endsWith('.js') || arg.endsWith('.ts'));
+
+  return {
+    nodeArgs: args.slice(0, idx),
+    script: args[idx],
+    scriptArgs: args.slice(idx + 1),
+  };
+}
+
+function loadConfig () {
+  const cfg = JSON.parse(jsonStrip(fs.readFileSync(process.argv[2]).toString()));
+
+  return Object.assign(
+    {
+      cwd: '.',
+      directories: [],
+      files: [],
+      waitExit: 5000,
+    },
+    cfg);
+}
+
+function startProcess () {
+  childProcess = fork(script, scriptArgs, {
+    detached: true,
+    execArgv: nodeArgs,
+  });
+
+  childProcess.on('exit', (code, signal) => {
+    const msg = code !== null
+      ? `code ${code}`
+      : `signal ${signal}`;
+
+    console.log(clc.red(`[RELOADER] Process exited with ${msg}. Waiting for a file change to restart it.`));
+    childProcess = null;
+  });
+
+  state = stateEnum.RUNNING;
+}
+
+async function stopProcess () {
+  if (childProcess === null) {
+    state = stateEnum.STOPPED;
+  }
+
+  if (state !== stateEnum.RUNNING) {
+    return;
+  }
+
+  state = stateEnum.STOPPING;
+
+  childProcess.removeAllListeners();
+
+  let exited = false;
+
+  childProcess.on('exit', () => {
+    exited = true;
+  });
+
+  childProcess.kill();
+
+  // Wait for config.waitExit for the process to stop itself, after that: kill it
+  const now = Date.now();
+  let forced = false;
+
+  while (!exited) {
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    if (!forced && (Date.now() - now) > config.waitExit) {
+      console.log(clc.red(`[RELOADER] Process still here after ${config.waitExit}ms. Sending a SIGKILL signal`));
+      childProcess.kill('SIGKILL');
+      forced = true;
+    }
+  }
+
+  state = stateEnum.STOPPED;
+}
+
+const watcher = chokidar.watch(script);
+watcher.add(config.directories.map(dir => path.join(config.cwd, dir)));
+watcher.add(config.files.map(file => path.join(config.cwd, file)));
+
+watcher.on('change', async file => {
+  if (state === stateEnum.RUNNING) {
+    console.log(clc.green(`[RELOADER] Change detected on ${path.relative(config.cwd, file)}. Reloading...`));
+    await stopProcess();
+    startProcess();
+  }
+});
+
+startProcess();
