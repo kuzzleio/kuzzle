25c44b02bc746f3efc78e7fc9f52dea5f09a7361
Michele <bluedraggy@gmail.com>
2021-01-20 18:34:54 +0100

    [unit] update funnel tests to comply to new controllers paths

--- test/api/funnel/init.test.js
+++ test/api/funnel/init.test.js
@@ -6,16 +6,18 @@ const sinon = require('sinon');
 const KuzzleMock = require('../../mocks/kuzzle.mock');
 
 const Funnel = require('../../../lib/api/funnel');
-const AuthController = require('../../../lib/api/controllers/authController');
-const BulkController = require('../../../lib/api/controllers/bulkController');
-const CollectionController = require('../../../lib/api/controllers/collectionController');
-const DocumentController = require('../../../lib/api/controllers/documentController');
-const IndexController = require('../../../lib/api/controllers/indexController');
-const MSController = require('../../../lib/api/controllers/memoryStorageController');
-const RealtimeController = require('../../../lib/api/controllers/realtimeController');
-const SecurityController = require('../../../lib/api/controllers/securityController');
-const ServerController = require('../../../lib/api/controllers/serverController');
-const AdminController = require('../../../lib/api/controllers/adminController');
+const {
+  AdminController,
+  AuthController,
+  BulkController,
+  CollectionController,
+  DocumentController,
+  IndexController,
+  MemoryStorageController,
+  RealtimeController,
+  SecurityController,
+  ServerController,
+} = require('../../../lib/api/controllers');
 
 describe('funnel.init', () => {
   it('should initialize API and plugins controller', async () => {
@@ -35,8 +37,8 @@ describe('funnel.init', () => {
     should(funnel.controllers.get('collection')).be.instanceOf(CollectionController);
     should(funnel.controllers.get('document')).be.instanceOf(DocumentController);
     should(funnel.controllers.get('index')).be.instanceOf(IndexController);
-    should(funnel.controllers.get('memoryStorage')).be.instanceOf(MSController);
-    should(funnel.controllers.get('ms')).be.instanceOf(MSController);
+    should(funnel.controllers.get('memoryStorage')).be.instanceOf(MemoryStorageController);
+    should(funnel.controllers.get('ms')).be.instanceOf(MemoryStorageController);
     should(funnel.controllers.get('realtime')).be.instanceOf(RealtimeController);
     should(funnel.controllers.get('security')).be.instanceOf(SecurityController);
     should(funnel.controllers.get('server')).be.instanceOf(ServerController);
