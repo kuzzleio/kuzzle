59aec911d5c65c9e81665e372c126215544376bb
Yoann Abbes <yoann1.abbes@epitech.eu>
2021-02-09 10:21:24 +0100

    add wait websocket message step

--- features/Websocket.feature
+++ features/Websocket.feature
@@ -5,5 +5,6 @@ Feature: WebSocket
   Scenario: Check reponse message from Kuzzle with a specific payload
     Given I open a new local websocket connection
     When I send the message '{"p":1}' to Kuzzle through websocket
+    And I wait to receive a websocket response from Kuzzle
     Then I should receive a response message from Kuzzle through websocket matching:
       |  p  |  2  |
--- features/step_definitions/websocket-steps.js
+++ features/step_definitions/websocket-steps.js
@@ -7,9 +7,6 @@ const ws = require('ws');
 Given('I open a new local websocket connection', function () {
   return new Promise((resolve) => {
     this.props.client = new ws('ws://localhost:7512');
-    this.props.client.on('message', data => {
-      this.props.result = JSON.parse(data);
-    });
     this.props.client.on('open', () =>{
       return resolve();
     });
@@ -18,6 +15,15 @@ Given('I open a new local websocket connection', function () {
 
 When('I send the message {string} to Kuzzle through websocket', function (message) {
   this.props.client.send(message);
+});
+
+Then('I wait to receive a websocket response from Kuzzle', function() {
+  return new Promise((resolve) => {
+    this.props.client.on('message', data => {
+      this.props.result = JSON.parse(data);
+      return resolve();
+    });
+  });
 });
 
 Then('I should receive a response message from Kuzzle through websocket matching:', function (dataTable) {
