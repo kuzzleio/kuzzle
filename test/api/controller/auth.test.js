9a79fb63ed23867fa808a2b1b05cdc084884de6c
Shiranuit <shiranui007@gmail.com>
2021-02-12 13:20:15 +0100

    add auth:login, auth:logout, auth:checkToken and auth:refreshToken unit tests

--- test/api/controller/auth.test.js
+++ test/api/controller/auth.test.js
@@ -21,6 +21,7 @@ const { NativeController } = require('../../../lib/api/controller/base');
 
 describe('Test the auth controller', () => {
   let request;
+  let requestCookieOnly;
   let kuzzle;
   let user;
   let authController;
@@ -28,6 +29,7 @@ describe('Test the auth controller', () => {
   beforeEach(() => {
     kuzzle = new KuzzleMock();
     kuzzle.config.security.jwt.secret = 'test-secret';
+    kuzzle.config.http.supportCookieAuthentication = false;
     kuzzle.ask.withArgs('core:security:user:anonymous:get').resolves({_id: '-1'});
 
     user = new User();
@@ -42,6 +44,19 @@ describe('Test the auth controller', () => {
       },
       foo: 'bar'
     });
+
+    requestCookieOnly = new Request({
+      controller: 'auth',
+      action: 'login',
+      strategy: 'mockup',
+      body: {
+        username: 'jdoe'
+      },
+      foo: 'bar',
+      cookieOnly: true
+    });
+
+    requestCookieOnly.input.headers = {cookie: 'authToken=;'};
 
     authController = new AuthController();
 
@@ -265,6 +280,190 @@ describe('Test the auth controller', () => {
     });
   });
 
+  describe('#login with cookies', () => {
+    let createTokenStub;
+
+    beforeEach(() => {
+      kuzzle.config.http.supportCookieAuthentication = true;
+      createTokenStub = kuzzle.ask.withArgs('core:security:token:create');
+    });
+
+    it('should resolve to a valid jwt token in the header if authentication succeed ', async () => {
+      const token = new Token({
+        _id: 'foobar#bar',
+        jwt: 'bar',
+        userId: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+      createTokenStub.resolves(token);
+      kuzzle.tokenManager.getConnectedUserToken.resolves(null);
+
+      const response = await authController.login(requestCookieOnly);
+
+      should(kuzzle.pipe).calledWith('auth:strategyAuthenticated', {
+        strategy: 'mockup',
+        content: user
+      });
+
+      should.exists(requestCookieOnly.response.headers);
+      should.exists(requestCookieOnly.response.headers['Set-Cookie'])
+      should(requestCookieOnly.response.headers['Set-Cookie']).be.an.Array()
+        .and.match(/authToken=bar; Path=\/; Expires=[^;]+; HttpOnly; SameSite=Strict/);
+
+      should(response).be.deepEqual({
+        _id: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+      should(createTokenStub).calledOnce();
+    });
+
+    it('should refresh the token if it already exists', async () => {
+      const existingToken = new Token({
+        _id: 'foobar#foo',
+        jwt: 'foo',
+        userId: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+      const token = new Token({
+        _id: 'foobar#bar',
+        jwt: 'bar',
+        userId: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+      createTokenStub.resolves(token);
+      kuzzle.tokenManager.getConnectedUserToken.returns(existingToken);
+
+      const response = await authController.login(requestCookieOnly);
+
+      should(kuzzle.tokenManager.getConnectedUserToken).be.called();
+      should(kuzzle.tokenManager.refresh).be.calledWith(existingToken, token);
+
+      should.exists(requestCookieOnly.response.headers);
+      should.exists(requestCookieOnly.response.headers['Set-Cookie'])
+      should(requestCookieOnly.response.headers['Set-Cookie']).be.an.Array()
+        .and.match(/authToken=bar; Path=\/; Expires=[^;]+; HttpOnly; SameSite=Strict/);
+
+      should(response).be.deepEqual({
+        _id: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+    });
+
+    it('should modify the result according to auth:strategyAuthenticated pipe events', async () => {
+      kuzzle.pipe
+        .withArgs('auth:strategyAuthenticated')
+        .resolves({strategy: 'foobar', content: {foo: 'bar'}});
+
+      const response = await authController.login(requestCookieOnly);
+
+      should(kuzzle.pipe).calledWith('auth:strategyAuthenticated', {
+        strategy: 'mockup',
+        content: user
+      });
+      should(response).match({foo: 'bar'});
+      should(createTokenStub).not.be.called();
+    });
+
+    it('should handle strategy\'s headers and status code in case of multi-step authentication strategy', async () => {
+      const redir = {headers: {Location: 'http://github.com'}, statusCode: 302};
+
+      kuzzle.passport.authenticate.resolves(redir);
+
+      const response = await authController.login(requestCookieOnly);
+
+      should(kuzzle.pipe).not.be.called();
+      should(response.headers.Location).be.equal('http://github.com');
+      should(response.statusCode).be.equal(302);
+      should(requestCookieOnly.status).be.equal(302);
+      should(requestCookieOnly.response).match({
+        status: 302,
+        result: response,
+        headers: {Location: 'http://github.com'}
+      });
+
+      should(kuzzle.ask.withArgs('core:security:token:create')).not.be.called();
+    });
+
+    it('should call passport.authenticate with input body and query string', async () => {
+      createTokenStub.resolves(new Token());
+      await authController.login(requestCookieOnly);
+
+      should(kuzzle.passport.authenticate)
+        .be.calledOnce()
+        .be.calledWithMatch({
+          body: { username: 'jdoe' },
+          query: { foo: 'bar' }
+        });
+    });
+
+    it('should reject if no strategy is specified', () => {
+      delete requestCookieOnly.input.args.strategy;
+
+      return should(authController.login(requestCookieOnly))
+        .rejectedWith(BadRequestError, {
+          id: 'api.assert.missing_argument',
+          message: 'Missing argument "strategy".'
+        });
+    });
+
+    it('should be able to set authentication expiration', async () => {
+      const token = new Token({
+        _id: 'foobar#bar',
+        jwt: 'bar',
+        userId: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+      createTokenStub.resolves(token);
+      kuzzle.passport.authenticate.resolves(user);
+
+      requestCookieOnly.input.args.expiresIn = '1s';
+
+      const response = await authController.login(requestCookieOnly);
+
+      should.exists(requestCookieOnly.response.headers);
+      should.exists(requestCookieOnly.response.headers['Set-Cookie'])
+      should(requestCookieOnly.response.headers['Set-Cookie']).be.an.Array()
+        .and.match(/authToken=bar; Path=\/; Expires=[^;]+; HttpOnly; SameSite=Strict/);
+
+      should(response).be.deepEqual({
+        _id: 'foobar',
+        expiresAt: 4567,
+        ttl: 1234
+      });
+
+      should(createTokenStub).be.calledWith(
+        'core:security:token:create',
+        user,
+        { expiresIn: '1s' });
+    });
+
+    it('should reject if authentication fails', () => {
+      kuzzle.passport.authenticate.rejects(new Error('error'));
+
+      return should(authController.login(requestCookieOnly)).be.rejected();
+    });
+
+    it('should reject in case of unknown strategy', () => {
+      requestCookieOnly.input.args.strategy = 'foobar';
+
+      return should(authController.login(requestCookieOnly))
+        .rejectedWith(BadRequestError, {
+          id: 'security.credentials.unknown_strategy'
+        });
+    });
+  });
+
   describe('#logout', () => {
     beforeEach(() => {
       const signedToken = jwt.sign(
@@ -286,6 +485,76 @@ describe('Test the auth controller', () => {
         token: t,
         user: { _id: 'foo' }
       });
+    });
+
+    it('should expire token', async () => {
+      const response = await authController.logout(request);
+
+      should(kuzzle.ask)
+        .calledWith('core:security:token:delete', request.context.token);
+
+      should(response.responseObject).be.instanceof(Object);
+    });
+
+    it('should expire all tokens at once', async () => {
+      request.input.args.global = true;
+
+      await authController.logout(request);
+
+      should(kuzzle.ask).calledWith('core:security:token:deleteByKuid', 'foo');
+    });
+
+    it('should emit an error if the token cannot be expired', () => {
+      const error = new Error('Mocked error');
+
+      kuzzle.ask.withArgs('core:security:token:delete').rejects(error);
+
+      return should(authController.logout(request)).be.rejectedWith(error);
+    });
+
+    it('should reject if invoked by an anonymous user', () => {
+      request.context.user._id = '-1';
+
+      return should(authController.logout(request)).rejectedWith(
+        UnauthorizedError,
+        {id: 'security.rights.unauthorized'});
+    });
+  });
+
+  describe('#logout with cookies', () => {
+    beforeEach(() => {
+      kuzzle.config.http.supportCookieAuthentication = true;
+
+      const signedToken = jwt.sign(
+        {_id: 'admin'},
+        kuzzle.config.security.jwt.secret,
+        {algorithm: kuzzle.config.security.jwt.algorithm});
+      const t = new Token({
+        _id: 'foo#' + signedToken,
+        userId: 'foo',
+        jwt: signedToken
+      });
+
+      request = new Request({
+        controller: 'auth',
+        action: 'logout',
+        cookieOnly: true,
+      }, {
+        connectionId: 'papagaya',
+        token: t,
+        user: { _id: 'foo' }
+      });
+
+      request.input.headers = {cookie: `authToken=${signedToken};`};
+    });
+
+    it('should nullify the authToken cookie', async () => {
+      await authController.logout(request);
+
+      should.exists(request.response.headers);
+      should.exists(request.response.headers['Set-Cookie']);
+      should(request.response.headers['Set-Cookie']).be.an.Array()
+        .and.match(/authToken=null; Path=\/; HttpOnly; SameSite=Strict/);
     });
 
     it('should expire token', async () => {
@@ -407,6 +676,72 @@ describe('Test the auth controller', () => {
     });
   });
 
+  describe('#checkToken with cookies', () => {
+    let testToken;
+
+    beforeEach(() => {
+      kuzzle.config.http.supportCookieAuthentication = true;
+
+      request = new Request(
+        {
+          action: 'checkToken',
+          controller: 'auth',
+          body: {},
+          cookieOnly: true,
+        },
+        {});
+      
+      request.input.jwt = 'foobar';
+      testToken = new Token({ expiresAt: 42, userId: 'durres' });
+    });
+
+    it('should reject an error if no token is provided in the cookie', () => {
+      return should(authController.checkToken(new Request({body: {}})))
+        .rejectedWith(BadRequestError, {
+          id: 'api.assert.missing_argument',
+          message: 'Missing argument "body.token".'
+        });
+    });
+
+    it('should return a valid response if the token is valid', async () => {
+      const verifyStub = kuzzle.ask
+        .withArgs('core:security:token:verify', request.input.jwt)
+        .resolves(testToken);
+
+      const response = await authController.checkToken(request);
+
+      should(verifyStub).calledOnce();
+      should(response).be.instanceof(Object);
+      should(response.kuid).be.eql('durres');
+      should(response.valid).be.true();
+      should(response.state).be.undefined();
+      should(response.expiresAt).be.eql(testToken.expiresAt);
+    });
+
+    it('should return a valid response if the token is not valid', async () => {
+      const verifyStub = kuzzle.ask
+        .withArgs('core:security:token:verify', request.input.jwt)
+        .rejects(new UnauthorizedError('foobar'));
+
+      const response = await authController.checkToken(request);
+
+      should(verifyStub).calledOnce();
+      should(response).be.instanceof(Object);
+      should(response.valid).be.false();
+      should(response.state).be.eql('foobar');
+      should(response.expiresAt).be.undefined();
+    });
+
+    it('should return a rejected promise if an error occurs', () => {
+      const error = new KuzzleInternalError('Foobar');
+      kuzzle.ask
+        .withArgs('core:security:token:verify', request.input.jwt)
+        .rejects(error);
+
+      return should(authController.checkToken(request)).be.rejectedWith(error);
+    });
+  });
+
   describe('#refreshToken', () => {
     it('should reject if the user is not authenticated', () => {
       return should(authController.refreshToken(new Request(
@@ -451,6 +786,70 @@ describe('Test the auth controller', () => {
         expiresAt: 42,
         ttl: 'ttl'
       });
+
+      should(kuzzle.ask).calledWith(
+        'core:security:token:refresh',
+        req.context.user,
+        req.context.token,
+        req.input.args.expiresIn);
+    });
+  });
+
+  describe('#refreshToken with cookies', () => {
+    beforeEach(() => {
+      kuzzle.config.http.supportCookieAuthentication = true;
+    })
+
+    it('should reject if the user is not authenticated', () => {
+      return should(authController.refreshToken(new Request(
+        {cookieOnly: true},
+        {token: {userId: 'anonymous', _id: '-1'}, user: {_id: '-1'}}
+      )))
+        .rejectedWith(
+          UnauthorizedError,
+          {id: 'security.rights.unauthorized'});
+    });
+
+    it('should provide a new jwt and expire the current one ', async () => {
+      const newToken = {
+        _id: '_id',
+        jwt: 'new-token',
+        userId: 'userId',
+        ttl: 'ttl',
+        expiresAt: 42
+      };
+      const req = new Request(
+        {
+          expiresIn: '42h',
+          cookieOnly: true
+        },
+        {
+          token: {
+            userId: 'user',
+            _id: '_id',
+            jwt: 'jwt',
+            refreshed: false
+          },
+          user: {
+            _id: 'user'
+          }
+        }
+      );
+
+      kuzzle.ask.withArgs('core:security:token:refresh').resolves(newToken);
+
+      const response = await authController.refreshToken(req);
+
+      should(response).eql({
+        _id: 'userId',
+        expiresAt: 42,
+        ttl: 'ttl'
+      });
+
+      should.exists(req.response.headers);
+      should.exists(req.response.headers['Set-Cookie'])
+      should(req.response.headers['Set-Cookie']).be.an.Array()
+        .and.match(/authToken=new-token; Path=\/; Expires=[^;]+; HttpOnly; SameSite=Strict/);
 
       should(kuzzle.ask).calledWith(
         'core:security:token:refresh',
