93a210e4e892f34f6f4f114b3a8c3858e4e7cccb
Leodau <bluedraggy@gmail.com>
2021-03-15 18:43:16 +0100

    test(controller): migrate more unit tests + remove search implementation

--- test/api/controllers/userController.test.js
+++ test/api/controllers/userController.test.js
@@ -13,7 +13,7 @@ const KuzzleMock = require('../../mocks/kuzzle.mock');
 const UserController = require('../../../lib/api/controllers/userController');
 const User = require('../../../lib/model/security/user');
 
-describe('UserController', () => {
+describe.only('UserController', () => {
   let kuzzle;
   let request;
   let userController;
@@ -318,6 +318,394 @@ describe('UserController', () => {
         'users');
 
       should(response).match({ mapping: { foo: 'bar' } });
+    });
+  });
+
+  describe('#strategies', () => {
+    const getEvent = 'core:security:user:get';
+    const exampleStrategy = 'someStrategy';
+    const returnedUser = new User();
+    let getStub;
+
+    beforeEach(() => {
+      request.input.resource._id = 'test';
+      returnedUser._id = request.input.resource._id;
+      getStub = kuzzle.ask
+        .withArgs(getEvent, request.input.resource._id)
+        .resolves(returnedUser);
+
+      kuzzle.pluginsManager.listStrategies.returns([exampleStrategy]);
+
+      kuzzle.pluginsManager.getStrategyMethod
+        .withArgs(exampleStrategy, 'exists')
+        .returns(sinon.stub().resolves(true));
+    });
+
+    it('should return a list of strategies', async () => {
+      const response = await userController.strategies(request);
+
+      should(response).be.an.Object().and.not.empty();
+      should(response.strategies).be.an.Array().and.have.length(1);
+      should(response.strategies.includes(exampleStrategy)).be.true();
+      should(response.total).eql(1);
+    });
+
+    it('should return empty when anonymous id is provided', async () => {
+      request.input.resource._id = '-1';
+
+      const response = await userController.strategies(request);
+
+      should(response).be.an.Object().and.not.empty();
+      should(response.strategies).be.an.Array().and.have.length(0);
+      should(response.total).eql(0);
+    });
+
+    it('should reject if user is not found', async () => {
+      const error = new Error('foo');
+      request.input.resource._id = 'alyx';
+
+      getStub
+        .withArgs(getEvent, request.input.resource._id)
+        .rejects(error);
+
+      await should(userController.strategies(request))
+        .rejectedWith(error);
+    });
+
+    it('should reject if no id is provided', async () => {
+      request.input.resource._id = null;
+
+      await should(userController.strategies(request))
+        .rejectedWith(BadRequestError, {
+          id: 'api.assert.missing_argument',
+          message: 'Missing argument "_id".'
+        });
+
+      should(getStub).not.called();
+    });
+  });
+
+  describe('#mGet', () => {
+    const mGetEvent = 'core:security:user:mGet';
+    let mGetResult;
+    let mGetStub;
+
+    beforeEach(() => {
+      request.input.body = { ids: ['foo', 'bar'] };
+
+      mGetResult = [new User(), new User(), new User()];
+      mGetResult[0]._id = 'foo';
+      mGetResult[1]._id = 'bar';
+      mGetResult[2]._id = 'baz';
+
+      mGetStub = kuzzle.ask.withArgs(mGetEvent).resolves(mGetResult);
+    });
+
+    it('should reject if no ids are given', async () => {
+      delete request.input.body.ids;
+
+      await should(userController.mGet(request))
+        .rejectedWith(BadRequestError, {
+          id: 'api.assert.missing_argument',
+          message: 'Missing argument "ids".'
+        });
+
+      should(mGetStub).not.called();
+    });
+
+    it('should return a valid response', async () => {
+      const result = await userController.mGet(request);
+
+      should(mGetStub).calledWith(mGetEvent, request.input.body.ids);
+
+      should(result).match({
+        hits: [
+          { _id: 'foo', _source: { profileIds: [] } },
+          { _id: 'bar', _source: { profileIds: [] } },
+          { _id: 'baz', _source: { profileIds: [] } },
+        ],
+      });
+    });
+
+    it('should accept ids given as an args string', async () => {
+      request.input.body = null;
+      request.input.args.ids = 'user1,user2';
+
+      const result = await userController.mGet(request);
+
+      should(mGetStub).calledWithMatch(mGetEvent, ['user1', 'user2']);
+
+      should(result).match({
+        hits: [
+          { _id: 'foo', _source: { profileIds: [] } },
+          { _id: 'bar', _source: { profileIds: [] } },
+          { _id: 'baz', _source: { profileIds: [] } },
+        ],
+      });
+    });
+  });
+
+  describe('#replace', () => {
+    const replaceEvent = 'core:security:user:replace';
+    let replaceStub;
+    let replacedUser;
+
+    beforeEach(() => {
+      request.input.resource._id = 'test';
+      request.input.body = { foo: 'bar', profileIds: ['qux'] };
+
+      replacedUser = new User();
+
+      replaceStub = kuzzle.ask
+        .withArgs(replaceEvent, request.input.resource._id)
+        .resolves(replacedUser);
+    });
+
+    it('should reject if the request does not have a body', async () => {
+      request.input.body = null;
+
+      await should(userController.replace(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.body_required' });
+
+      should(replaceStub).not.called();
+    });
+
+    it('should reject if there is no id provided', async () => {
+      request.input.resource._id = null;
+
+      await should(userController.replace(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.missing_argument' });
+
+      should(replaceStub).not.called();
+    });
+
+    it('should reject if the content does not have a profileIds attribute', async () => {
+      request.input.body.profileIds = null;
+
+      await should(userController.replace(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.missing_argument' });
+    });
+
+    it('should reject if the provided profileIds attribute is not an array', async () => {
+      request.input.body.profileIds = {};
+
+      await should(userController.replace(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.invalid_type' });
+    });
+
+    it('should reject if the security module throws', async () => {
+      const error = new Error('foo');
+
+      replaceStub.rejects(error);
+
+      await should(userController.replace(request)).rejectedWith(error);
+    });
+
+    it('should correctly process the request', async () => {
+      const replacedUserContent = {
+        baz: 'qux',
+        foo: 'bar',
+        profileIds: request.input.body.profileIds,
+      };
+
+      Object.assign(
+        replacedUser,
+        {_id: request.input.resource._id},
+        replacedUserContent);
+
+      const response = await userController.replace(request);
+
+      should(replaceStub).calledWithMatch(
+        replaceEvent,
+        request.input.resource._id,
+        request.input.body.profileIds,
+        request.input.body,
+        {
+          refresh: 'wait_for',
+          userId: request.context.user._id,
+        });
+
+      should(response).be.an.Object().and.not.instanceof(User);
+      should(response).match({
+        _id: request.input.resource._id,
+        _source: replacedUserContent
+      });
+    });
+
+    it('should handle request options', async () => {
+      request.input.args.refresh = false;
+
+      await userController.replace(request);
+
+      should(replaceStub).calledWithMatch(
+        replaceEvent,
+        request.input.resource._id,
+        request.input.body.profileIds,
+        request.input.body,
+        {
+          refresh: 'false',
+          userId: request.context.user._id,
+        });
+    });
+  });
+
+  describe('#scroll', () => {
+    const scrollEvent = 'core:security:user:scroll';
+    let scrollStub;
+
+    beforeEach(() => {
+      request.input.args.scrollId = 'foobar';
+      scrollStub = kuzzle.ask
+        .withArgs(scrollEvent)
+        .resolves({
+          hits: [{ _id: 'admin', _source: { profileIds: ['admin'] } }],
+          total: 2,
+          scrollId: 'foobar'
+        });
+    });
+
+    it('should reject if no scrollId is provided', () => {
+      request.input.args.scrollId = null;
+
+      return should(userController.scroll(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.missing_argument' });
+    });
+
+    it('should reformat search results correctly', async () => {
+      const response = await userController.scroll(request);
+
+      should(scrollStub).be.calledWith(scrollEvent, 'foobar', undefined);
+      should(response).match({
+        hits: [{_id: 'admin'}],
+        scrollId: 'foobar',
+        total: 2,
+      });
+    });
+
+    it('should handle the scroll argument', async () => {
+      request.input.args.scroll = 'qux';
+
+      const response = await userController.scroll(request);
+
+      should(scrollStub).be.calledWith(scrollEvent, 'foobar', 'qux');
+      should(response).match({
+        hits: [{_id: 'admin'}],
+        scrollId: 'foobar',
+        total: 2,
+      });
+    });
+  });
+
+  describe('#update', () => {
+    const updateEvent = 'core:security:user:update';
+    let updateStub;
+    let updatedUser;
+
+    beforeEach(() => {
+      request.input.resource._id = 'test';
+      request.input.body = { foo: 'bar' };
+
+      updatedUser = new User();
+      updatedUser._id = request.input.resource._id;
+
+      updateStub = kuzzle.ask
+        .withArgs(updateEvent, request.input.resource._id)
+        .resolves(updatedUser);
+    });
+
+    it('should return a valid response and use default options', async () => {
+      const updatedUserContent = {foo: 'bar', baz: 'qux'};
+
+      Object.assign(updatedUser, updatedUserContent);
+
+      const response = await userController.update(request);
+
+      should(updateStub).calledWithMatch(
+        updateEvent,
+        'test',
+        null,
+        { foo: 'bar' },
+        {
+          refresh: 'wait_for',
+          retryOnConflict: 10,
+          userId: request.context.user._id,
+        });
+
+      should(response).be.an.Object().and.not.instanceof(User);
+      should(response).match({
+        _id: updatedUser._id,
+        _source: updatedUserContent,
+      });
+    });
+
+    it('should reject if no id is given', async () => {
+      request.input.resource._id = null;
+
+      await should(userController.update(request))
+        .rejectedWith(BadRequestError, {
+          id: 'api.assert.missing_argument',
+          message: 'Missing argument "_id".'
+        });
+
+      should(updateStub).not.called();
+    });
+
+    it('should reject if no body is provided', async () => {
+      request.input.body = null;
+
+      await should(userController.update(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.body_required' });
+
+      should(updateStub).not.called();
+    });
+
+    it('should forward the provided options to the security module', async () => {
+      request.input.args.refresh = false;
+      request.input.args.retryOnConflict = 123;
+
+      await userController.update(request);
+
+      should(updateStub).calledWithMatch(
+        updateEvent,
+        'test',
+        null,
+        { foo: 'bar' },
+        {
+          refresh: 'false',
+          retryOnConflict: 123,
+          userId: request.context.user._id,
+        });
+    });
+
+    it('should reject if the security module throws', () => {
+      const error = new Error('foo');
+      updateStub.rejects(error);
+
+      return should(userController.update(request)).rejectedWith(error);
+    });
+  });
+
+  describe('#updateMapping', () => {
+    const foo = { foo: 'bar' };
+
+    it('should reject if the body is missing', () => {
+      return should(userController.updateMapping(request))
+        .rejectedWith(BadRequestError, { id: 'api.assert.body_required'});
+    });
+
+    it('should update the user mapping', async () => {
+      request.input.body = foo;
+      kuzzle.ask.withArgs('core:storage:private:mappings:update').resolves(foo);
+
+      const response = await userController.updateMapping(request);
+
+      should(kuzzle.ask).be.calledWith(
+        'core:storage:private:mappings:update',
+        kuzzle.internalIndex.index,
+        'users',
+        request.input.body);
+
+      should(response).eql(foo);
     });
   });
 
