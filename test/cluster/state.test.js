1e8c74de994e985520d5c0817eef616806b55fd8
scottinet <scottinet@protonmail.com>
2021-03-30 14:08:35 +0200

    test: fullstate unit tests & bug fixes

--- test/cluster/state.test.js
+++ test/cluster/state.test.js
@@ -8,7 +8,7 @@ const Kuzzle = require('../mocks/kuzzle.mock');
 
 const State = require('../../lib/cluster/state');
 
-describe('#Cluster Full State', () => {
+describe.only('#Cluster Full State', () => {
   let kuzzle;
   let state;
 
@@ -95,6 +95,23 @@ describe('#Cluster Full State', () => {
           roomId: 'roomid3',
         },
       ]);
+    });
+
+    it('should throw if adding a new node when it already exists', () => {
+      const filters = { oh: { hai: { can: { I: 'haz cheezburgers?' } } } };
+      const node = {
+        messageId: 456,
+        nodeId: 'nodeid',
+        subscribers: 123,
+      };
+
+      state.addRealtimeRoom('roomid', 'index', 'collection', filters, node);
+
+      should(() => state.addRealtimeRoom('roomid', 'index', 'collection', filters, node))
+        .throw({
+          id: 'cluster.fatal.desync',
+          message: /duplicate node/,
+        });
     });
 
     it('should remove the entire room when the last node using it removes it', () => {
@@ -279,7 +296,256 @@ describe('#Cluster Full State', () => {
       should(() => state.addRealtimeSubscription('roomid', 'nodeid', Long.fromInt(1)))
         .throw(InternalError, {
           id: 'cluster.fatal.desync',
+          message: /room doesn't exist/,
         });
+    });
+
+    it('should throw when attempting to add a new subscription to a non-existing node', () => {
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      should(() => state.addRealtimeSubscription('roomid', 'ohnoes', Long.fromInt(1)))
+        .throw(InternalError, {
+          id: 'cluster.fatal.desync',
+          message: /unknown node ohnoes/,
+        });
+    });
+
+    it('should ignore new subscriptions coming from older messages', () => {
+      const messageId = Long.fromInt(12, true);
+
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeSubscription('roomid', 'nodeid', Long.fromInt(11, true));
+
+      should(state.serialize().rooms).match([
+        {
+          collection: 'collection',
+          filters: JSON.stringify({}),
+          index: 'index',
+          nodes: [
+            { nodeId: 'nodeid', messageId: messageId, subscribers: 23 },
+          ],
+          roomId: 'roomid',
+        },
+      ]);
+    });
+
+    it('should be able to remove a subscription from an existing room', () => {
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid2',
+        subscribers: 42,
+      });
+
+      const newMessageId = Long.fromInt(14, true);
+      state.removeRealtimeSubscription('roomid', 'nodeid', newMessageId);
+
+      should(state.serialize().rooms).match([
+        {
+          collection: 'collection',
+          filters: JSON.stringify({}),
+          index: 'index',
+          nodes: [
+            { nodeId: 'nodeid', messageId: newMessageId, subscribers: 22 },
+            { nodeId: 'nodeid2', messageId: Long.fromInt(12, true), subscribers: 42 },
+          ],
+          roomId: 'roomid',
+        }
+      ]);
+    });
+
+    it('should throw if attempting to unsubscribe from an unknown room', () => {
+      const newMessageId = Long.fromInt(14, true);
+
+
+      should(() => state.removeRealtimeSubscription('roomid', 'nodeid', newMessageId))
+        .throw(InternalError, {
+          id: 'cluster.fatal.desync',
+          message: /room doesn't exist/,
+        });
+    });
+
+    it('should throw if the number of subscribers becomes negative', () => {
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 1,
+      });
+
+      let newMessageId = Long.fromInt(14, true);
+      state.removeRealtimeSubscription('roomid', 'nodeid', newMessageId);
+
+      should(state.serialize().rooms).match([
+        {
+          collection: 'collection',
+          filters: JSON.stringify({}),
+          index: 'index',
+          nodes: [
+            { nodeId: 'nodeid', messageId: newMessageId, subscribers: 0 },
+          ],
+          roomId: 'roomid',
+        }
+      ]);
+
+      newMessageId = newMessageId.add(1);
+
+      should(() => state.removeRealtimeSubscription('roomid', 'nodeid', newMessageId))
+        .throw(InternalError, {
+          id: 'cluster.fatal.desync',
+          message: /negative subscribers count/,
+        });
+    });
+
+    it('should throw if the unsubscribing node is unknown for that room', () => {
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 1,
+      });
+
+      const newMessageId = Long.fromInt(14, true);
+
+      should(() => state.removeRealtimeSubscription('roomid', 'ohnoes', newMessageId))
+        .throw(InternalError, {
+          id: 'cluster.fatal.desync',
+          message: /unknown node ohnoes/,
+        });
+    });
+
+    it('should ignore unsubscriptions coming from older messages', () => {
+      const messageId = Long.fromInt(12, true);
+
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.removeRealtimeSubscription('roomid', 'nodeid', Long.fromInt(11, true));
+
+      should(state.serialize().rooms).match([
+        {
+          collection: 'collection',
+          filters: JSON.stringify({}),
+          index: 'index',
+          nodes: [
+            { nodeId: 'nodeid', messageId: messageId, subscribers: 23 },
+          ],
+          roomId: 'roomid',
+        },
+      ]);
+    });
+
+    it('should be able to remove an entire node from the realtime fullstate', () => {
+      const messageId = Long.fromInt(12, true);
+
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid2', 'index', 'collection', {}, {
+        messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid3', 'index2', 'collection', {}, {
+        messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid4', 'index2', 'collection2', {}, {
+        messageId,
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.removeNode('nodeid');
+
+      should(state.serialize().rooms).be.Array().and.be.empty();
+    });
+  });
+
+  describe('#auth strategies', () => {
+    it('should be able to add authentication strategies to the fullstate', () => {
+      const strategy = {
+        strategyName: 'foo',
+        foo: 'bar',
+      };
+
+      state.addAuthStrategy(strategy);
+
+      should(state.serialize().authStrategies).match([strategy]);
+    });
+
+    it('should be able to remove an authentication strategies from the fullstate', () => {
+      const strategy = {
+        strategyName: 'foo',
+        foo: 'bar',
+      };
+
+      state.addAuthStrategy(strategy);
+      state.addAuthStrategy({ strategyName: 'bar', foo: 'bar' });
+
+      state.removeAuthStrategy('bar');
+      should(state.serialize().authStrategies).match([strategy]);
+
+      state.removeAuthStrategy('foo');
+
+      should(state.serialize().authStrategies).be.Array().and.be.empty();
+    });
+  });
+
+  describe('#loading from a fullstate', () => {
+    it('should be able to load a fullstate from a serialized fullstate', () => {
+      state.addRealtimeRoom('roomid', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid2', 'index', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid3', 'index2', 'collection', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addRealtimeRoom('roomid4', 'index2', 'collection2', {}, {
+        messageId: Long.fromInt(12, true),
+        nodeId: 'nodeid',
+        subscribers: 23,
+      });
+
+      state.addAuthStrategy({ strategyName: 'foo', foo: 'bar' });
+      state.addAuthStrategy({ strategyName: 'bar', foo: 'bar' });
+
+      const newState = new State();
+      newState.loadFullState(state.serialize());
+
+      should(newState.serialize()).match(state.serialize());
     });
   });
 });
