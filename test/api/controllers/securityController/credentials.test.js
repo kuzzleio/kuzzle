e4c33a3dc7b51b88a265c33cfdea804c40848742
Michele Leo <bluedraggy@gmail.com>
2021-05-27 13:46:30 +0200

    New API action: security:searchUsersByCredentials (#1948)
    ## What does this PR do ?
    
    @MathieuVeber 
    
    Thanks to this feature, Kuzzle users will be able to retreive users kuid by credentials. Credentials depend on the chosen authentification strategy, its the username in local strategy for example.
    
    Data that can be searched into as much as information that is retreive are __critical__ :warning:, so this action must be implemented and used with caution.
    
    #### Detailed changes
    - New `security:searchUsersByCredentials` action which relies on `search` method implementation in strategy plugins
    - Documentation for `security:searchUsersByCredentials` and generic `search`
    - `search` method can now be optionally implemented for strategy plugins (avoid breaking changes)
    - New `missing_optional_method` error for strategy plugins
    - Unit tests for `security:searchUsersByCredentials`
    
    ### Other changes
    `search` method has been implemented in our local authentication strategy plugin in this [PR](https://github.com/kuzzleio/kuzzle-plugin-auth-passport-local/pull/76)
    
    ### Example
    
    **POST** `http://localhost:7512/credentials/local/users/_search`
    
    #### Request
    ```json
    {
      "query": {
        "bool": {
          "must": [
            {
              "match": {
                "username":  "test@test.com" // example with the local authentication strategy
              }
            }
          ]
        }
      }
    }
    ```
    
    #### Response
    ```json
    {
      "action": "searchUsersByCredentials",
      "controller": "security",
      "error": null,
      "node": "knode-smooth-hawking-65812",
      "requestId": "52772769-2e15-45f6-b27b-a702fec09c18",
      "result": {
        "hits": [
          {
            "kuid": "kuid",
            "username": "test@test.com" // example with the local authentication strategy
          }
        ],
        "total": 1
      },
      "status": 200,
      "volatile": null
    }
    ```
    
    ### How should this be manually tested?
    
    1. Clone + Run
    2. [Live Test Here](https://hoppscotch.io/?method=POST&url=http://localhost:7512&path=/credentials/local/users/_search&contentType=application/json&rawParams=%7B%0A%20%20%20%20%0A%20%20%22bool%22:%20%7B%0A%20%20%20%20%22must%22:%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22match%22:%20%7B%0A%20%20%20%20%20%20%20%20%20%20%22username%22:%20%20%22test@test.com%22%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%0A%20%20%7D%0A%7D)
    
    Closes #1676

--- test/api/controllers/securityController/credentials.test.js
+++ test/api/controllers/securityController/credentials.test.js
@@ -4,7 +4,11 @@ const rewire = require('rewire');
 const should = require('should');
 const sinon = require('sinon');
 
-const { Request } = require('../../../../index');
+const {
+  BadRequestError,
+  Request,
+  SizeLimitError
+} = require('../../../../index');
 const KuzzleMock = require('../../../mocks/kuzzle.mock');
 
 const SecurityController = rewire('../../../../lib/api/controllers/securityController');
@@ -206,6 +210,84 @@ describe('Test: security controller - credentials', () => {
           should(methodStub.firstCall.args[1]).be.eql('someUserId');
           should(methodStub.firstCall.args[2]).be.eql('someStrategy');
         });
+    });
+  });
+
+  describe('#searchUsersByCredentials', () => {
+    let query;
+    let methodStub;
+    let stubResult;
+
+    beforeEach(() => {
+      query = {
+        bool: {
+          must: [
+            {
+              match: {
+                credentials:  'test@test.com'
+              }
+            }
+          ]
+        }
+      };
+      request = new Request({
+        controller: 'security',
+        action: 'searchUsersByCredentials',
+        strategy: 'someStrategy',
+        body: { query }
+      });
+      securityController.translateKoncorde = sinon.stub().resolves();
+      stubResult = {
+        hits: [{ credentials: 'test@test.com', kuid: 'kuid' }],
+        total: 1
+      };
+      methodStub = sinon.stub().resolves(stubResult);
+      kuzzle.pluginsManager.getStrategyMethod.returns(methodStub);
+    });
+
+    it('should return the result of the appropriate method from the right strategy plugin', async () => {
+      const result = await securityController.searchUsersByCredentials(request);
+
+      should(securityController.translateKoncorde).not.be.called();
+      should(kuzzle.pluginsManager.getStrategyMethod).be.calledOnce();
+      should(kuzzle.pluginsManager.getStrategyMethod).be.calledWith('someStrategy', 'search');
+      should(methodStub).be.calledOnce();
+      should(methodStub.firstCall.args[0]).be.eql({ query });
+      should(result).be.deepEqual(stubResult);
+    });
+
+    it('should throw if the optional method search has not been implemented', () => {
+      kuzzle.pluginsManager.getStrategyMethod.returns(undefined);
+
+      return should(securityController.searchUsersByCredentials(request))
+        .rejectedWith({ id: 'plugin.strategy.missing_optional_method' });
+    });
+
+    it('should reject if the size argument exceeds server configuration', () => {
+      kuzzle.config.limits.documentsFetchCount = 1;
+      request.input.args.size = 10;
+
+      return should(securityController.searchUsersByCredentials(request)).rejectedWith(
+        SizeLimitError,
+        { id: 'services.storage.get_limit_exceeded' });
+    });
+
+    it('should reject if the "lang" is not supported', () => {
+      request.input.args.lang = 'turkish';
+
+      return should(securityController.searchUsersByCredentials(request)).rejectedWith(
+        BadRequestError,
+        { id: 'api.assert.invalid_argument' });
+    });
+
+    it('should call the "translateKoncorde" method if "lang" is "koncorde"', async () => {
+      request.input.body = { query: { equals: { credentials: 'test@test.com' } } };
+      request.input.args.lang = 'koncorde';
+
+      await securityController.searchUsersByCredentials(request);
+
+      should(securityController.translateKoncorde)
+        .be.calledWith({ equals: { credentials: 'test@test.com' } });
     });
   });
 
