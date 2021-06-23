8c6ac492cabefef1a8f32734f7fc0f90c19045d5
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2021-06-23 08:28:36 +0200

    Official support for Node.js 14.17.0 (#1971)

--- .ci/scripts/run-test-arm.sh
+++ .ci/scripts/run-test-arm.sh
@@ -4,8 +4,8 @@ set -ex
 
 if [ -z "$NODE_VERSION" ];
 then
-  echo "Missing NODE_VERSION, use default NODE_12_VERSION"
-  NODE_VERSION=$NODE_12_VERSION
+  echo "Missing NODE_VERSION, use default NODE_14_VERSION"
+  NODE_VERSION=$NODE_14_VERSION
 fi
 
 echo "Testing Kuzzle against node v$NODE_VERSION"
