e8d9908c7a53846b556d1316f11c5278ac90367b
Aschen <adrien@maret.email>
2021-07-20 10:24:27 +0200

    modify cookieAuth handling for http

--- test/core/network/httpRouter/httpRouter.test.js
+++ test/core/network/httpRouter/httpRouter.test.js
@@ -80,6 +80,7 @@ describe('core/network/httpRouter', () => {
         'content-type': 'application/json',
         'Accept-Encoding': 'gzip,deflate,identity',
         'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS,HEAD',
+        'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Content-Encoding, Content-Length, X-Kuzzle-Volatile'
       });
     });
