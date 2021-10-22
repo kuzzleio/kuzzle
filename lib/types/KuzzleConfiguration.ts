cfee8409fb3bd01fe7e86bbff074c38e7aaf308c
matissepage <matisse.page@epitech.eu>
2021-10-22 17:20:29 +0200

    add:[typescript-default-config] create Server and Services type in KuzzleConfiguration

--- lib/types/KuzzleConfiguration.ts
+++ lib/types/KuzzleConfiguration.ts
@@ -5,7 +5,516 @@ import {
   ProfileDefinition
 } from './index';
 
-export interface IKuzzleConfiguration {
+export type Server = {
+  /**
+     * The maximum size of an incoming request.
+     *
+     * Units can be expressed in bytes ("b" or none), kilobytes ("kb"),
+     * megabytes ("mb"), gigabytes ("gb") or terabytes ("tb").
+     *
+     * @default "1mb"
+     */
+   maxRequestSize: string;
+
+   /**
+    * The listening port for HTTP and WebSocket protocols.
+    *
+    * @default 7512
+    */
+   port: number;
+
+   /**
+    * Configuration section for Kuzzle access logs.
+    */
+   logs: {
+     /**
+      * An array of Winston transports configurations to output access
+      * logs.
+      *
+      * Possible transport types are: console, file, elasticsearch and syslog.
+      *
+      * Please refer to https://github.com/winstonjs/winston/blob/master/docs/transports.md
+      * for more information on transports configuration.
+      *
+      * @default
+      *
+      * [
+         {
+           transport: 'console',
+           level: 'info',
+           stderrLevels: [],
+           silent: true
+         }
+       ]
+      *
+      */
+     transports: JSONObject[];
+
+     /**
+      * Access log format.
+      *
+      * Currently supported are "combined" (=Apache combined logs format)
+      * and "logstash".
+      *
+      * "logstash" will output the whole request input to JSON, ready to
+      * be consumed by logstash agent.
+      *
+      * @default "combined"
+      */
+     accessLogFormat: string;
+
+     /**
+      * The offset to use as the client ip, from the FORWARDED-FOR chain,
+      * beginning from the right (0 = the ip address of the last
+      * client|proxy which connected to Kuzzle.
+      *
+      * @default 0
+      */
+     accessLogIpOffset: number
+   },
+
+   /**
+    * protocols accepted by Kuzzle. 
+    * protocols can be extended and configured in this section.
+    */
+   protocols: {
+     http: {
+       /**
+        * Enable support for compressed requests, using the Content-Encoding header 
+        * Currently supported compression algorithms:  gzip, deflate, identity 
+        * Note: "identity" is always an accepted value, even if compression support is disabled
+        * 
+        * @default true
+        */
+        allowCompression: boolean
+
+        /**
+         * Set to "false" to disable HTTP support
+         * 
+         * @default true
+         */
+         enabled: true
+
+         /**
+          * Maximum number of encoding layers that can be applied to an http message, using the Content-Encoding header. 
+          * This parameter is meant to prevent abuses by setting an abnormally large number 
+          * of encodings, forcing Kuzzle to  allocate as many decoders to handle the incoming request.
+          * 
+          * @default 3
+          */
+         maxEncodingLayers: number
+
+         /**
+          * Maximum size of requests sent via http forms
+          * 
+          * @default "1MB"
+          */
+          maxFormFileSize: string
+     }
+     mqtt: {
+       /**
+        * Set to true to enable MQTT support
+        * 
+        * @default false
+        */
+       enabled: boolean
+
+       /**
+        * Allow MQTT pub/sub capabilities or restrict to Kuzzle requests only
+        * 
+        * @default false
+        */
+        allowPubSub: boolean
+
+
+        /**
+         * Switches responseTopic back to a regular public topic
+         * 
+         * @default false
+         */
+        developmentMode: boolean
+
+        /**
+         * Delay in ms to apply between a disconnection notification is 
+         * received and the connection is actually removed
+         * 
+         * @default 250
+         */
+        disconnectDelay: number
+
+        /**
+         * Name of the topic listened by the plugin for requests
+         * 
+         * @default 'Kuzzle/request'
+         */
+        requestTopic: string
+
+        /**
+         * Name of the topic clients should listen to get requests result
+         * 
+         * @default 'Kuzzle/response'
+         */
+         responseTopic: string
+
+         /**
+          * Constructor options passed to underlying MQTT server. 
+          * See aedes documentation for further reference: https://github.com/moscajs/aedes
+          */
+         serveur: {
+           /**
+            * @default 1883
+            */
+           port: number
+         }
+
+         /**
+          * Set to "true" to enable realtime notifications like "TokenExpired" notifications
+          * 
+          * @default true
+          */
+          realtimeNotifications: boolean
+          
+          websocket: {
+            /**
+             * Set to true to enable WebSocket support
+             * 
+             * @default true
+             */
+             enabled: boolean
+
+             /**
+              * The maximum time (in milliseconds) without sending or receiving a message from a client. 
+              * Once reached, the client's socket is forcibly closed. 
+              * If a client socket is inactive for too long, the server will send a PING request before closing the socket. 
+              * Minimum value: 1000 (but it's strongly advised to not set a value this low to forcibly close idle client sockets)
+              * 
+              * @default 60000
+              */
+              idleTimeout: number
+
+              /**
+               * Enable/Disable per message compression
+               * 
+               * @default false
+               */
+               compression: boolean
+
+               /**
+                * The maximum number of messages per second a single socket can
+                * submit to the server.
+                * @default 0
+                */
+                rateLimit: number
+
+                /**
+                 * Set to "true" to enable realtime notifications like "TokenExpired" notifications
+                 * 
+                 * @default true
+                 */
+                 realtimeNotifications: boolean
+          }
+     }
+   }
+}
+
+export type Services = {
+  /**
+     * Time in ms after which a service is considered failing if 
+     * it has not init.
+     * 
+     * @default 120000
+     */
+   defaultInitTimeout: number
+
+   /**
+    * Default interval in ms between Kuzzle tries to init 
+    * the service again on first failure.
+    * 
+    * @default 1000
+    */
+    retryInterval: number
+
+    /**
+     * The database engine used for Kuzzle internal index
+     */
+    internalCache: {
+      /**
+       * The cache service relies on Redis sample settings for Redis service (see also https://github.com/luin/ioredis)
+       * 
+       * 1. using a single Redis database:
+       *  * node:
+       *    * host:
+       *        The host on which Redis can be reached.
+       *        Can take an IP address, an URI or a hostname
+       *    * port:
+       *        The port on which Redis is running its database:
+       *          * (optional, deprecated) database:
+       *        ID of the redis database (default: 0)
+       *        NOTE: this option is deprecated and will be removed in Kuzzle v3.
+       *        Use 'options.db' instead. If both options are set, then
+       *        "options.db" will take precedence.
+       *  * (optional) options:
+       *    Redis specific options compatible with IORedis.
+       *    See Redis client constructor available options:
+       *    https://github.com/luin/ioredis/blob/master/API.md#new-redisport-host-options
+       * 
+       * 2. using a master/slaves Redis instance with Redis sentinels
+       *   (cf. http://redis.io/topics/sentinel):
+       *  * node:
+       *    * sentinels:
+       *        array of sentinels instances:
+       *          * host:
+       *              Host name/address of the sentinel server
+       *              Can be an IP address, an URI or a hostname
+       *          * port:
+       *              Network port opened by Redis on the sentinel server
+       *    * name:
+       *        Group of Redis instances composed of a master and one
+       *        or more slaves
+       *    * (optional, deprecated) database:
+       *        ID of the redis database (default: 0)
+       *        NOTE: this option is deprecated and will be removed in Kuzzle v3.
+       *              Use 'options.db' instead. If both options are set, then
+       *              "options.db" will take precedence.
+       *  * (optional) options:
+       *      Redis specific options compatible with IORedis.
+       *      See Redis client constructor available options:
+       *      https://github.com/luin/ioredis/blob/master/API.md#redis--eventemitter
+       * 
+       * 3. using a redis cluster (cf. http://redis.io/topics/cluster-spec):
+       *   * nodes: array of master nodes of the cluster
+       *     * host:
+       *         Host name/address of a redis cluster node
+       *         Can be an IP address, an URI or a hostname
+       *     * port:
+       *        Network port opened by the redis cluster node
+       *   * (optional, deprecated) database:
+       *       ID of the redis database (default: 0)
+       *       NOTE: this option is deprecated and will be removed in Kuzzle v3.
+       *             Use 'options.db' instead. If both options are set, then
+       *             "options.db" will take precedence.
+       *   * (optional) options:
+       *       Redis specific options compatible with IORedis.
+       *       See Redis client constructor available options:
+       *       https://github.com/luin/ioredis/blob/master/API.md#new-redisport-host-options
+       *   * (optional) clusterOptions:
+       *       Redis Cluster specific options compatible with IORedis.
+       *       See Redis Cluster client constructor available options:
+       *       https://github.com/luin/ioredis/blob/master/API.md#new-clusterstartupnodes-options
+       *   * (optional) overrideDnsLookup:
+       *       Only available when using a Redis Cluster config.
+       *       Use with caution: if set to true, it makes Kuzzle skip DNS validation for TLS certificates
+       *       This is the only way to connect to an AWS Elasticache Cluster with TLS encryption.
+       *       See: https://github.com/luin/ioredis#special-note-aws-elasticache-clusters-with-tls
+       */
+
+      /**
+       * @default 'redis'
+       */
+      backend: string
+
+      clusterOptions: {
+        /**
+         * @default true
+         */
+         enableReadyCheck: boolean
+      }
+
+      /**
+       * @default 0
+       */
+      database: number
+
+      node: {
+        /**
+         * @default "localhost"
+         */
+        host: string
+
+        /**
+         * @default 6379
+         */
+        port: number
+
+        /**
+         * * (optional) options:
+         *    Redis specific options compatible with IORedis.
+         *    See Redis client constructor available options:
+         *    https://github.com/luin/ioredis/blob/master/API.md#new-redisport-host-options
+         * 
+         */
+         options: {}
+
+        /**
+         * Only available when using a Redis Cluster config.
+         * Use with caution: if set to true, it makes Kuzzle skip DNS validation for TLS certificates
+         * This is the only way to connect to an AWS Elasticache Cluster with TLS encryption.
+         *  See: https://github.com/luin/ioredis#special-note-aws-elasticache-clusters-with-tls
+         * @default false
+         */
+         overrideDnsLookup: boolean
+      }
+    }
+    memoryStorage: {
+      /**
+       * @default 'redis'
+       */
+       backend: string
+
+       clusterOptions: {
+         /**
+          * @default true
+          */
+          enableReadyCheck: boolean
+       }
+ 
+       /**
+        * @default 5
+        */
+       database: number
+    }
+    
+    /**
+     * The database engine used for Kuzzle internal index
+     */
+     internalIndex: {
+       /**
+        * Maximum amount of time (in milliseconds)
+        * to wait for a concurrent database bootstrap
+        * 
+        * @default 60000
+        */
+        bootstrapLockTimeout: number
+     }
+
+     /**
+      * The default storage layer is Elasticsearch and it is 
+      * currently the only storage layer we support.
+      */
+      storageEngine: {
+        /**
+         * @default ['storageEngine']
+         */
+        aliases: Array<string>
+
+        /**
+         * @default "elasticsearch"
+         */
+        backend: string
+
+        /**
+         * Elasticsearch constructor options. Use this field to specify your
+         * Elasticsearch config options, this object is passed through to the
+         * Elasticsearch constructor and can contain all options/keys outlined here:
+         * @see https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/client-configuration.html
+         */
+        client: {
+          /**
+           * @default "http://localhost:9200"
+           */
+          node: string
+        }
+        /**
+         * Default policy against new fields that are not referenced in the
+         * collection mapping.
+         * The value of this configuration will change Elasticsearch behavior
+         * on fields that are not declared in the collection mapping.
+         *   - "true": Stores document and update the collection mapping with
+         *     inferred type
+         *   - "false": Stores document and does not update the collection
+         *     mapping (field are not indexed)
+         *   - "strict": Rejects document
+         * @see https://www.elastic.co/guide/en/elasticsearch/reference/7.4/dynamic-mapping.html
+         */
+         commonMapping: {
+           /**
+            * @default "false"
+            */
+           dynamic: string
+
+           properties: {
+            _kuzzle_info: {
+              properties: {
+                /**
+                 * @default 
+                 * 
+                 * [
+                 *   {
+                 *     type: 'keyword',
+                 *   }
+                 * ]
+                 */
+                author: JSONObject
+
+                /**
+                 * @default 
+                 * 
+                 * [
+                 *   {
+                 *     type: 'date',
+                 *   }
+                 * ]
+                 */
+                 createdAt: JSONObject
+
+                 /**
+                 * @default 
+                 * 
+                 * [
+                 *   {
+                 *     type: 'keyword',
+                 *   }
+                 * ]
+                 */
+                  updater: JSONObject
+
+                  /**
+                 * @default 
+                 * 
+                 * [
+                 *   {
+                 *     type: 'date',
+                 *   }
+                 * ]
+                 */
+                   updatedAt: JSONObject
+              }
+            }
+           }
+         }
+
+         internalIndex: {
+           /**
+            * @default "kuzzle"
+            */
+           name: string
+
+           collections: {
+             users: {
+               /**
+                * @default 'false'
+                */
+               dynamic: string
+
+               properties: {
+                 /**
+                    * @default 
+                    * 
+                    * [
+                    *   {
+                    *     type: 'keyword',
+                    *   }
+                    * ]
+                    */
+                  profileIds: JSONObject
+               }
+             }
+           }
+         }
+      }
+}
+
+export type IKuzzleConfiguration = {
   /**
    * The HTTP section lets you configure how Kuzzle should
    * handle HTTP requests.
@@ -18,6 +527,14 @@ export interface IKuzzleConfiguration {
      * @default "*"
      */
     accessControlAllowOrigin: string;
+
+    /**
+     * Sets the default Access-Control-Allow-Origin HTTP
+     * header used to send responses to the client.
+     *
+     * @default false
+     */
+     accessControlAllowOriginUseRegExp: boolean;
 
     /**
      * Sets the default Access-Control-Allow-Method header.
@@ -558,76 +1075,12 @@ export interface IKuzzleConfiguration {
   /**
    * Kuzzle server is the entry point for incoming requests.
    */
-  server: {
-    /**
-     * The maximum size of an incoming request.
-     *
-     * Units can be expressed in bytes ("b" or none), kilobytes ("kb"),
-     * megabytes ("mb"), gigabytes ("gb") or terabytes ("tb").
-     *
-     * @default "1mb"
-     */
-    maxRequestSize: string;
+  server: Server
 
-    /**
-     * The listening port for HTTP and WebSocket protocols.
-     *
-     * @default 7512
-     */
-    port: number;
-
-    /**
-     * Configuration section for Kuzzle access logs.
-     */
-    logs: {
-      /**
-       * An array of Winston transports configurations to output access
-       * logs.
-       *
-       * Possible transport types are: console, file, elasticsearch and syslog.
-       *
-       * Please refer to https://github.com/winstonjs/winston/blob/master/docs/transports.md
-       * for more information on transports configuration.
-       *
-       * @default
-       *
-       * [
-          {
-            transport: 'console',
-            level: 'info',
-            stderrLevels: [],
-            silent: true
-          }
-        ]
-       *
-       */
-      transports: JSONObject[];
-
-      /**
-       * Access log format.
-       *
-       * Currently supported are "combined" (=Apache combined logs format)
-       * and "logstash".
-       *
-       * "logstash" will output the whole request input to JSON, ready to
-       * be consumed by logstash agent.
-       *
-       * @default "combined"
-       */
-      accessLogFormat: string;
-
-      /**
-       * The offset to use as the client ip, from the FORWARDED-FOR chain,
-       * beginning from the right (0 = the ip address of the last
-       * client|proxy which connected to Kuzzle.
-       *
-       * @default 0
-       */
-      accessLogIpOffset: number
-    },
-
-    protocols: {}
-  },
+  /**
+   * Services are the external components Kuzzle relies on.
+   */
+  services: Services
 }
 
 export type KuzzleConfiguration = Partial<IKuzzleConfiguration>
