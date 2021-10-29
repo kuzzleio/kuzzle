c625900a3631ed7b2af811076097074c187abc29
matissepage <matisse.page@epitech.eu>
2021-10-29 16:20:38 +0200

    [plugin_config] add plugin configuration

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -33,3 +33,4 @@ export * from './config/PasswordPolicy';
 export * from './config/KuzzleConfiguration';
 export * from './config/RoleDefinition';
 export * from './config/ProfileDefinition';
+export * from './config/PluginsConfiguration';
