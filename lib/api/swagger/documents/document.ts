082eb5ca3cf28da8f483156ccc640629d29bc06d
clementBolin <clement.bolin@epitech.eu>
2021-11-05 14:42:10 +0100

    add(feat/open_api_2165): add count document action

--- lib/api/swagger/documents/document.ts
+++ lib/api/swagger/documents/document.ts
@@ -1,4 +1,4 @@
 import { readYamlfile } from "../tools";
 
-export const DocumentCreate = readYamlfile('./count.yaml')
-export const DefinitionsDocument = readYamlfile('./definitions.yaml');
+export const DocumentCount = readYamlfile('./lib/api/swagger/documents/count.yaml')
+export const DefinitionsDocument = readYamlfile('./lib/api/swagger/documents/definitions.yaml');
