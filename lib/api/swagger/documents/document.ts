fb258b0e5dc2a94bcb970189bbba126d8126991f
clementBolin <clement.bolin@epitech.eu>
2021-11-05 21:26:11 +0100

    add(feat/open_api_2165): improved performance when loading YAML files

--- lib/api/swagger/documents/document.ts
+++ lib/api/swagger/documents/document.ts
@@ -1,5 +1,9 @@
 import { readYamlfile } from '../tools';
 
-export const DocumentCount = readYamlfile('./lib/api/swagger/documents/count.yaml').DocumentCreate;
-export const DocumentCountComponent = readYamlfile('./lib/api/swagger/documents/count.yaml').components.schemas;
+// reading the description of the Count action in the controller document.
+// The yaml objects are then stored in the variables below
+const countObject = readYamlfile('./lib/api/swagger/documents/count.yaml');
+export const DocumentCount = countObject.DocumentCount;
+export const DocumentCountComponent = countObject.components.schemas;
+// Document definitions (reusable object for KuzzleRequest and KuzzleResponse)
 export const DefinitionsDocument = readYamlfile('./lib/api/swagger/documents/definitions.yaml').definitions;
