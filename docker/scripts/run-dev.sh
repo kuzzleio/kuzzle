b16cea00010445c5c4c42a58b1804fe5f706a580
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2021-11-08 12:06:51 +0100

    Use Traefik instead of Nginx and improve dev stack
    * Use Docker Hearthbeat to schedule properly containers
    * Use a dedicated new container `kuzzle-installer` to install
      dependencies (mandatory for cluster first launch and MacOS/Windows
          usages)

--- docker/scripts/run-dev.sh
+++ docker/scripts/run-dev.sh
@@ -12,19 +12,6 @@ then
   n $NODE_14_VERSION
 fi
 
-if [ -n "$TRAVIS" ] || [ -n "$REBUILD" ]; then
-    npm ci --unsafe-perm
-    chmod -R 777 node_modules/
-    npm rebuild all --unsafe-perm
-    ./docker/scripts/install-plugins.sh
-elif [ ! -d "./node_modules/" ]; then
-    git submodule update --init --recursive
-    npm ci --unsafe-perm
-    ./docker/scripts/install-plugins.sh
-fi
-
-echo "[$(date)] - Starting Kuzzle..."
-
 if [ -n "$KUZZLE_PLUGINS" ];
 then
   ENABLED_PLUGINS="$KUZZLE_PLUGINS,functional-test-plugin"
