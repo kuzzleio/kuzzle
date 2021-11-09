09ebd5229156bf18c138d8dbba05ee620ed3e1d7
Clem <clement.bolin@epitech.eu>
2021-11-09 11:10:05 +0100

    add(feat/open_api_create): create action description

--- lib/api/swagger/documents/document.ts
+++ lib/api/swagger/documents/document.ts
@@ -5,5 +5,10 @@ import { readYamlfile } from '../tools';
 const countObject = readYamlfile('./lib/api/swagger/documents/count.yaml');
 export const DocumentCount = countObject.DocumentCount;
 export const DocumentCountComponent = countObject.components.schemas;
+// reading the description of the Create action in the controller document.
+// The yaml objects are then stored in the variables below
+const createObject = readYamlfile('./lib/api/swagger/documents/create.yaml');
+export const DocumentCreate = createObject.DocumentCreate;
+export const DocumentCreateComponent = createObject.components.schemas;
 // Document definitions (reusable object for KuzzleRequest and KuzzleResponse)
 export const DefinitionsDocument = readYamlfile('./lib/api/swagger/documents/definitions.yaml').definitions;
