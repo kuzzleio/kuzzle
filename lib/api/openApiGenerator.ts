897f67d628247d0e2398cad9811cf5d89aa6e473
clementBolin <clement.bolin@epitech.eu>
2021-11-12 12:45:44 +0100

    add(feat/open_api_replace): replace action for document controller

--- lib/api/openApiGenerator.ts
+++ lib/api/openApiGenerator.ts
@@ -24,7 +24,9 @@ import { version } from './../../package.json';
 import {
   DocumentCount,
   DefinitionsDocument, 
-  DocumentCountComponent, 
+  DocumentCountComponent,
+  DocumentReplace,
+  DocumentReplaceComponent,
 } from './swagger/documents/document';
 
 const routeUrlMatch = /:([^/]*)/g;
@@ -88,11 +90,15 @@ export function generateOpenApi(_request: KuzzleRequest): any {
       '/count': {
         DocumentCount
       },
+      '/replace': {
+        DocumentReplace
+      }
     },
     components: {
       DefinitionsDocument,
       schemas: {
         DocumentCountComponent,
+        DocumentReplaceComponent,
       }
     }
   };
