07337cebac747320bee2aa23d4b1fb4137984860
Aschen <adrien@maret.email>
2021-12-08 20:30:29 +0100

    fix path

--- lib/api/openapi/document/index.ts
+++ lib/api/openapi/document/index.ts
@@ -2,61 +2,61 @@ import { readYamlFile } from '../../../util/readYamlFile';
 
 // reading the description of the Count action in the controller document.
 // The yaml objects are then stored in the variables below
-const countObject = readYamlFile('./lib/api/openapi/documents/count.yaml');
+const countObject = readYamlFile('./lib/api/openapi/document/count.yaml');
 export const OpenApiDocumentCount = countObject.DocumentCount;
 export const OpenApiDocumentCountComponent = countObject.components.schemas;
 
 // reading the description of the Create action in the controller document.
 // The yaml objects are then stored in the variables below
-const createObject = readYamlFile('./lib/api/openapi/documents/create.yaml');
+const createObject = readYamlFile('./lib/api/openapi/document/create.yaml');
 export const OpenApiDocumentCreate = createObject.DocumentCreate;
 export const OpenApiDocumentCreateComponent = createObject.components.schemas;
 
 // reading the description of the CreateOrReplace action in the controller document.
 // The yaml objects are then stored in the variables below
-const createOrReplaceObject = readYamlFile('./lib/api/openapi/documents/createOrReplace.yaml');
+const createOrReplaceObject = readYamlFile('./lib/api/openapi/document/createOrReplace.yaml');
 export const OpenApiDocumentCreateOrReplace = createOrReplaceObject.DocumentCreateOrReplace;
 export const OpenApiDocumentCreateOrReplaceComponent = createOrReplaceObject.components.schemas;
 
 // reading the description of the Get action in the controller document.
 // The yaml objects are then stored in the variables below
-const getObject = readYamlFile('./lib/api/openapi/documents/get.yaml');
+const getObject = readYamlFile('./lib/api/openapi/document/get.yaml');
 export const OpenApiDocumentGet = getObject.DocumentGet;
 export const OpenApiDocumentGetComponent = getObject.components.schemas;
 
 // reading the description of the Replace action in the controller document.
 // The yaml objects are then stored in the variables below
-const replaceObject = readYamlFile('./lib/api/openapi/documents/replace.yaml');
+const replaceObject = readYamlFile('./lib/api/openapi/document/replace.yaml');
 export const OpenApiDocumentReplace = replaceObject.DocumentReplace;
 export const OpenApiDocumentReplaceComponent = replaceObject.components.schemas;
 
 // reading the description of the Exists action in the controller document.
 // The yaml objects are then stored in the variables below
-const existsObject = readYamlFile('./lib/api/openapi/documents/exists.yaml');
+const existsObject = readYamlFile('./lib/api/openapi/document/exists.yaml');
 export const OpenApiDocumentExists = existsObject.DocumentExists;
 export const OpenApiDocumentExistsComponent = existsObject.components.schemas;
 
 // reading the description of the Update action in the controller document.
 // The yaml objects are then stored in the variables below
-const updateObject = readYamlFile('./lib/api/openapi/documents/update.yaml');
+const updateObject = readYamlFile('./lib/api/openapi/document/update.yaml');
 export const OpenApiDocumentUpdate = updateObject.DocumentUpdate;
 export const OpenApiDocumentUpdateComponent = updateObject.components.schemas;
 
 // reading the description of the Scroll action in the controller document.
 // The yaml objects are then stored in the variables below
-const scrollObject = readYamlFile('./lib/api/openapi/documents/scroll.yaml');
+const scrollObject = readYamlFile('./lib/api/openapi/document/scroll.yaml');
 export const OpenApiDocumentScroll = scrollObject.DocumentScroll;
 export const OpenApiDocumentScrollComponent = scrollObject.components.schemas;
 
 // reading the description of the Delete action in the controller document.
 // The yaml objects are then stored in the variables below
-const deleteObject = readYamlFile('./lib/api/openapi/documents/delete.yaml');
+const deleteObject = readYamlFile('./lib/api/openapi/document/delete.yaml');
 export const OpenApiDocumentDelete = deleteObject.DocumentDelete;
 export const OpenApiDocumentDeleteComponent = deleteObject.components.schemas;
 
 // reading the description of the DeleteByQuery action in the controller document.
 // The yaml objects are then stored in the variables below
-const deleteByQueryObject = readYamlFile('./lib/api/openapi/documents/deleteByQuery.yaml');
+const deleteByQueryObject = readYamlFile('./lib/api/openapi/document/deleteByQuery.yaml');
 export const OpenApiDocumentDeleteByQuery = deleteByQueryObject.DocumentDeleteByQuery;
 export const OpenApiDocumentDeleteByQueryComponent = deleteByQueryObject.components.schemas;
 
