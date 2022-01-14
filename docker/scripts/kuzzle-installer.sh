0f736ae22e643caa053f4d13803b2445628295f3
clementbolin <clement.bolin@epitech.eu>
2022-01-14 19:09:14 +0100

    Revert "add(feat/m1_chip_support): script docker:install package.json"
    This reverts commit b76b324d35abbcd3d6d7e6b542cbebd06ff6dbd4.

---
+++ docker/scripts/kuzzle-installer.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ -z "$NODE_VERSION" ];
+then
+  echo "Missing NODE_VERSION, use default NODE_14_VERSION"
+  n "$NODE_14_VERSION"
+fi
+
+echo "Installing dependencies"
+npm ci
+
+if [ -n "$REBUILD" ]; then
+  echo "Force C++ deps rebuilding"
+  npm rebuild all --unsafe-perm
+fi
+
+exit 0
