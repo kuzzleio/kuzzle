06bb18cdbe0b740c7b686943cfbada10d3b9b9ea
Shiranuit <shiranui007@gmail.com>
2022-01-19 11:39:53 +0100

    convert user to typescript

--- lib/model/security/user.ts
+++ lib/model/security/user.ts
@@ -19,17 +19,21 @@
  * limitations under the License.
  */
 
-'use strict';
-
-const Rights = require('./rights');
-const Bluebird = require('bluebird');
-const _ = require('lodash');
-const kerror = require('../../kerror');
+import Rights from './rights';
+import Bluebird from 'bluebird';
+import _ from 'lodash';
+import kerror from '../../kerror';
+import Profile from './profile';
+import { KuzzleRequest } from '../../../index';
+import { Target } from '../../types';
 
 /**
  * @class User
  */
-class User {
+export default class User {
+  public _id: string;
+  public profileIds: string[];
+
   constructor() {
     this._id = null;
     this.profileIds = [];
@@ -38,7 +42,7 @@ class User {
   /**
    * @returns {Promise<Profile[]>}
    */
-  getProfiles() {
+  getProfiles() : Promise<Profile[]> {
     if (!global.kuzzle) {
       return kerror.reject('security', 'user', 'uninitialized', this._id);
     }
@@ -64,7 +68,7 @@ class User {
    * @param {Request} request
    * @returns {Promise.<boolean>}
    */
-  async isActionAllowed(request) {
+  async isActionAllowed(request: KuzzleRequest): Promise<boolean> {
     if (this.profileIds === undefined || this.profileIds.length === 0) {
       return false;
     }
@@ -91,7 +95,7 @@ class User {
    * while skipping the ones that includes a wildcard since they will be expanded
    * later on, based on index and collections authorized for the given user.
    */
-  async _areTargetsAllowed(profiles, targets) {
+  async _areTargetsAllowed(profiles: Profile[], targets: Target[]) {
     const profilesPolicies = await Bluebird.map(profiles, profile => profile.getAllowedPolicies());
 
     // Every target must be allowed by at least one profile
