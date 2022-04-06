549288f00dad7c921f70365315e3f98fac80e664
Shiranuit <shiranui007@gmail.com>
2022-04-06 22:23:36 +0200

    lint asyncMutex

--- lib/util/asyncMutex.ts
+++ lib/util/asyncMutex.ts
@@ -1,42 +1,64 @@
-const mutexes = new Map<String, boolean>();
+const mutexes = new Map<string, boolean>();
 
-async function _yield() {}
+async function _yield (): Promise<void> {
+  // This function needs to remain empty
+  // Its only purpose is to let the event loop switch to another task
+}
 
 export class AsyncMutex {
-  private resource: String;
-  private acquired: boolean;
+  private _resource: string;
+  private _acquired: boolean;
 
-  constructor(resource) {
-    this.resource = resource;
-    this.acquired = false;
+  constructor (resource) {
+    this._resource = resource;
+    this._acquired = false;
   }
 
-  get acquiredByOther(): boolean {
-    return !this.acquired && mutexes.get(this.resource);
+  /**
+   * Returns true if the resource is acquired by an other mutex.
+   */
+  get acquiredByOther (): boolean {
+    return ! this._acquired && mutexes.get(this._resource);
   }
 
-  async lock(wait = true) {
-    if (this.acquired) {
+  /**
+   * Return true if the resource is acquired by this mutex.
+   */
+  get acquired (): boolean {
+    return this._acquired;
+  }
+
+  /**
+   * Acquire the mutex.
+   * @param wait If true, wait until the mutex is available.
+   */
+  async lock (wait = true): Promise<boolean> {
+    if (this._acquired) {
       return true;
     }
 
-    if (mutexes.get(this.resource) && !wait) {
+    if (! wait && mutexes.get(this._resource)) {
       return false;
     }
       
-    while (mutexes.get(this.resource)) {
+    while (mutexes.get(this._resource)) {
+      // Enters and return from an empty async function so the event loop can switch to another task
       await _yield();
     }
 
-    this.acquired = true;
-    mutexes.set(this.resource, true);
+    this._acquired = true;
+    mutexes.set(this._resource, true);
     return true;
   }
 
-  async unlock() {
-    if (this.acquired) {
-      mutexes.delete(this.resource);
-      this.acquired = false;
+  /**
+   * Release the mutex.
+   */
+  unlock (): void {
+    if (! this._acquired) {
+      return;
     }
+    mutexes.delete(this._resource);
+    this._acquired = false;
   }
 }
