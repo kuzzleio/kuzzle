4d4858d50583405b6c19d4d089773a95ea2261f6
Shiranuit <ycombes@kuzzle.io>
2022-05-31 16:44:47 +0200

    Monitor request success, errors and executionTimes

--- lib/core/debug/requestMonitor.ts
+++ lib/core/debug/requestMonitor.ts
@@ -7,9 +7,26 @@ type MonitoringParams = {
   reportProgressInterval?: number; // Milliseconds
 }
 
+type RequestProtocolStatistics = {
+  success: number,
+  errors: number,
+  successTotalExecutionTime: number,
+  errorTotalExecutionTime: number,
+};
+
+type RequestStatistics = {
+  [protocol: string]: RequestProtocolStatistics;
+};
+
+
+type RequestsStatistics = {
+  [request: string]: RequestStatistics
+}
+
 export class RequestMonitor extends DebugModule {
   private monitoringInProgress = false;
-  private requestsStatistics: JSONObject = {};
+  private requestsStatistics: RequestsStatistics = {};
+  private requestExecutionTimers = new Map<string, number>();
   private monitoringInterval: NodeJS.Timeout;
 
   constructor () {
@@ -27,7 +44,15 @@ export class RequestMonitor extends DebugModule {
   }
 
   async init () {
+
     global.kuzzle.on('request:beforeExecution', async (request: KuzzleRequest) => {
+      if (! this.monitoringInProgress) {
+        return;
+      }
+
+      this.requestExecutionTimers.set(request.id, Date.now());
+    });
+    global.kuzzle.on('request:afterExecution', async ({request, success}: {request: KuzzleRequest, success: boolean}) => {
       if (! this.monitoringInProgress) {
         return;
       }
@@ -42,7 +67,34 @@ export class RequestMonitor extends DebugModule {
       }
 
       const protocol = request.context.connection.protocol;
-      actionDataFrame[protocol] = (actionDataFrame[protocol] || 0) + 1;
+      let dataFrame = actionDataFrame[protocol];
+      if (! dataFrame) {
+        dataFrame = {
+          success: 0,
+          errors: 0,
+          successTotalExecutionTime: 0,
+          errorTotalExecutionTime: 0,
+        };
+        actionDataFrame[protocol] = dataFrame;
+      }
+
+      const startDate = this.requestExecutionTimers.get(request.id);
+
+      let ellapsedTime = 0;
+
+      if (startDate !== undefined) {
+        ellapsedTime = Date.now() - startDate;
+        this.requestExecutionTimers.delete(request.id);
+      }
+
+      if (success) {
+        dataFrame.success += 1;
+        dataFrame.successTotalExecutionTime += ellapsedTime;
+      }
+      else {
+        dataFrame.errors += 1;
+        dataFrame.errorTotalExecutionTime += ellapsedTime;
+      }
     });
   }
 
@@ -54,6 +106,18 @@ export class RequestMonitor extends DebugModule {
     this.monitoringInterval = setInterval(() => {
       const statisticsFrame = this.requestsStatistics;
       this.requestsStatistics = {};
+      this.requestExecutionTimers.clear();
+
+      for (const requestStats of Object.values(statisticsFrame)) {
+        for (const protocolStats of Object.values(requestStats)) {
+          const stats = protocolStats as JSONObject;
+          stats.totalCalls = protocolStats.success + protocolStats.errors;
+          stats.averageSuccessExecutionTime = protocolStats.successTotalExecutionTime / protocolStats.success;
+          stats.averageErrorExecutionTime = protocolStats.errorTotalExecutionTime / protocolStats.errors;
+          stats.totalExecutionTime = protocolStats.successTotalExecutionTime + protocolStats.errorTotalExecutionTime;
+          stats.averageExecutionTime = stats.totalExecutionTime / stats.totalCalls;
+        }
+      }
 
       this.emit('monitoringProgress', {
         date: Date.now(),
