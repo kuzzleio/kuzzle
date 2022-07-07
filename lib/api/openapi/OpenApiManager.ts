8a7f0c691e48719c17cecd53b594782bb402090d
CustomEntity <customentity@gmail.com>
2022-07-07 17:16:33 +0200

    Added 'All' scope for OpenApi requests

--- lib/api/openapi/OpenApiManager.ts
+++ lib/api/openapi/OpenApiManager.ts
@@ -119,5 +119,13 @@ export class OpenApiManager {
     global.kuzzle.onAsk('core:api:openapi:kuzzle', () => this.kuzzleDefinition);
 
     global.kuzzle.onAsk('core:api:openapi:app', () => this.applicationDefinition);
+
+    global.kuzzle.onAsk('core:api:openapi:all', () => {
+      let mergedDefinition = ({...this.kuzzleDefinition});
+
+      mergedDefinition.tags = [...mergedDefinition.tags, ...this.applicationDefinition.tags]
+      mergedDefinition.paths = {...mergedDefinition.paths, ...this.applicationDefinition.paths}
+      return mergedDefinition;
+    });
   }
 }
