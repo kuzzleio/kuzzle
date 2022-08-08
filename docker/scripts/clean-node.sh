51907117a92573e0b36905368fb48d42e2328a6e
Aschen <adrien@maret.email>
2022-08-08 18:43:44 +0200

    rename shrinkwrap

--- docker/scripts/clean-node.sh
+++ docker/scripts/clean-node.sh
@@ -9,7 +9,7 @@ npm prune --production
 # @todo remove when https://github.com/tj/node-prune/pull/68 is merged
 rm -rf .git/
 find . -type d -name "man" | xargs rm -rf
-find . -type f \( -name "*.o"  -o -name "*.h" -o -name "npm-shrinkwrap.json" -o -name "*.mk" -o -name "*Makefile*" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" \) -exec rm {} \;
+find . -type f \( -name "*.o"  -o -name "*.h" -o -name "package-lock.json" -o -name "*.mk" -o -name "*Makefile*" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" \) -exec rm {} \;
 
 # Minify JS & JSON
 # I wasn't able to find a portable way to do string comparison with if
