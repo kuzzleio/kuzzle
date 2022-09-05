6c41db064025587b27ebc10d1bb58eddc63d2a87
Shiranuit <ycombes@kuzzle.io>
2022-09-05 17:25:44 +0200

    fix dump generator

--- lib/core/debug/modules/clusterDebugModule.ts
+++ lib/core/debug/modules/clusterDebugModule.ts
@@ -1,5 +1,6 @@
 import { Session } from "inspector";
 import { DebugModule } from "../../../types/DebugModule";
+import * as kerror from "../../../kerror";
 
 export class ClusterDebugModule extends DebugModule {
   constructor() {
@@ -12,7 +13,11 @@ export class ClusterDebugModule extends DebugModule {
     
   }
 
-  async preventNodeEviction(params: { evictionPrevented: boolean }): Promise<void> {
+  async preventNodeEviction(params: { evictionPrevented?: boolean }): Promise<void> {
+    if (params.evictionPrevented === undefined) {
+      throw kerror.get("api", "assert", "missing_argument", "evictionPrevented");
+    }
+
     global.kuzzle.ask("cluster:node:preventEviction", params.evictionPrevented);
   }
   
