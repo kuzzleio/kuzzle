7450ec84f45f0e331019d0fcf0287a3c0064ea18
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-12-02 18:35:30 +0100

    Remove body from tested parameter

--- test/api/request/requestInput.test.js
+++ test/api/request/requestInput.test.js
@@ -76,37 +76,37 @@ describe("#RequestInput", () => {
     }).throw("Input request data must be a non-null object");
   });
 
-  it("should not throw if an other type than object is provided", () => {
+  it("should throw if an invalid data parameter is provided", () => {
     // testing object-only parameters
-    ["volatile", "body"].forEach((k) => {
+    ["volatile"].forEach((k) => {
       should(function () {
         new RequestInput({ [k]: [] });
-      }).not.throw(`Attribute ${k} must be of type "object"`);
+      }).throw(`Attribute ${k} must be of type "object"`);
       should(function () {
         new RequestInput({ [k]: 123 });
-      }).not.throw(`Attribute ${k} must be of type "object"`);
+      }).throw(`Attribute ${k} must be of type "object"`);
       should(function () {
         new RequestInput({ [k]: false });
-      }).not.throw(`Attribute ${k} must be of type "object"`);
+      }).throw(`Attribute ${k} must be of type "object"`);
       should(function () {
         new RequestInput({ [k]: "foobar" });
-      }).not.throw(`Attribute ${k} must be of type "object"`);
+      }).throw(`Attribute ${k} must be of type "object"`);
     });
 
     // testing string-only parameters
     ["controller", "action", "jwt"].forEach((k) => {
       should(function () {
         new RequestInput({ [k]: [] });
-      }).not.throw(`Attribute ${k} must be of type "string"`);
+      }).throw(`Attribute ${k} must be of type "string"`);
       should(function () {
         new RequestInput({ [k]: 123 });
-      }).not.throw(`Attribute ${k} must be of type "string"`);
+      }).throw(`Attribute ${k} must be of type "string"`);
       should(function () {
         new RequestInput({ [k]: false });
-      }).not.throw(`Attribute ${k} must be of type "string"`);
+      }).throw(`Attribute ${k} must be of type "string"`);
       should(function () {
         new RequestInput({ [k]: {} });
-      }).not.throw(`Attribute ${k} must be of type "string"`);
+      }).throw(`Attribute ${k} must be of type "string"`);
     });
   });
 
