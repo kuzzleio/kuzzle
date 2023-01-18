c44cf4942b3a67f93ba14cef2bfb883f25e19c80
Shiranuit <shiranui007@gmail.com>
2023-01-18 12:24:54 +0100

    rename jest commands

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: [
+    'test-functional',
+  ]
 };
--- test-functional/network/http/http-stream.test.ts
+++ test-functional/network/http/http-stream.test.ts
@@ -5,24 +5,40 @@ test('Check duplicated Transfer-Encoding header', async () => {
     // The usage of the port 17510 is to avoid the usage of the proxy
     // because NGINX is sanitizing the headers
     // which prevent us from seing the duplicated Transfer-Encoding header
-    http.get('http://localhost:17510/stream-test/download-chunked', res => {
+    http.get({
+      hostname: 'localhost',
+      port: 17510,
+      path: '/stream-test/download-chunked',
+      headers: {
+        'Connection': 'close'
+      }
+    }, res => {
       resolve(res);
-    });
+    }).end();
   });
   const response = await promise;
   expect(response.headers['content-length']).toBeUndefined();
   expect(response.headers['transfer-encoding']).toBe('chunked');
+  response.socket.destroy();
 });
 
 test('Check Content-Length header on fixed size stream', async () => {
   const promise = new Promise<http.IncomingMessage>(resolve => {
     // The usage of the port 17510 is to avoid the usage of the proxy
     // because NGINX is sanitizing the headers
-    http.get('http://localhost:17510/stream-test/download-fixed', res => {
+    http.get({
+      hostname: 'localhost',
+      port: 17510,
+      path: '/stream-test/download-fixed',
+      headers: {
+        'Connection': 'close'
+      }
+    }, res => {
       resolve(res);
-    });
+    }).end();
   });
   const response = await promise;
   expect(response.headers['content-length']).toBe("10");
   expect(response.headers['transfer-encoding']).toBeUndefined();
+  response.socket.destroy();
 });
