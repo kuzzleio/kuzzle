24730a26a70725ba7337f10f4ad58530e697c047
rolljee <n.riquelmebareiro@gmail.com>
2023-11-20 14:54:18 +0100

    Update CI/CD

---
+++ .ci/scripts/install-armhf-deps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# WARNING: This script needs to be run in sdk-cross:node8-armhf image
+# Install Kuzzle deps using cross build toolchain
+npm ci --target_arch=arm  --target_platform=linux --silent --unsafe-perm \
+  --build-from-source=boost-geospatial-index \
+  --build-from-source=espresso-logic-minimizer \
+  --build-from-source=unix-dgram \
+  --build-from-source=uws \
+  --build-from-source=murmurhash-native \
+  --build-from-source=dumpme \
+  --build-from-source=cucumber-expressions
+npm ci --only=dev --target_arch=arm --target_platfrom=linux --unsafe-perm --silent
