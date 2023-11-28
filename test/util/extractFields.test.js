83535e9045d211990476aa2bc6083c9880ef6074
rolljee <n.riquelmebareiro@gmail.com>
2023-11-28 12:04:00 +0100

    Fix tests

--- test/util/extractFields.test.js
+++ test/util/extractFields.test.js
@@ -19,7 +19,7 @@ describe("util/extractFields", () => {
   it("Should extract fields recursively", () => {
     const result = extractFields(document);
 
-    should(result).match(["foo", "bar.a", "bar.b"]);
+    should(result).match(["bar.a", "bar.b", "foo"]);
   });
 
   it("Should ignore requested fields", () => {
@@ -32,9 +32,9 @@ describe("util/extractFields", () => {
     const result = extractFields(document, { alsoExtractValues: true });
 
     should(result).match([
-      { key: "foo", value: "valueFoo" },
       { key: "bar.a", value: "valueBarA" },
       { key: "bar.b", value: "valueBarB" },
+      { key: "foo", value: "valueFoo" },
     ]);
   });
 });
