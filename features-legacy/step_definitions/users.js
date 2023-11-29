4cafa70b881e1b2b66f9875be9b0d54aeaba5286
rolljee <n.riquelmebareiro@gmail.com>
2023-11-29 15:23:13 +0100

    Revert linting

--- features-legacy/step_definitions/users.js
+++ features-legacy/step_definitions/users.js
@@ -1,96 +1,97 @@
-"use strict";
+'use strict';
 
-const { When, Given, Then } = require("cucumber"),
-  _ = require("lodash"),
-  async = require("async");
+const
+  {
+    When,
+    Given,
+    Then
+  } = require('cucumber'),
+  _ = require('lodash'),
+  async = require('async');
 
 When(/^I get the user mapping$/, function () {
-  return this.api.getUserMapping().then((response) => {
-    if (response.error) {
-      throw new Error(response.error.message);
-    }
+  return this.api.getUserMapping()
+    .then(response => {
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
 
-    if (!response.result) {
-      throw new Error("No result provided");
-    }
+      if (! response.result) {
+        throw new Error('No result provided');
+      }
 
-    if (!response.result.mapping) {
-      throw new Error("No mapping provided");
-    }
+      if (! response.result.mapping) {
+        throw new Error('No mapping provided');
+      }
 
-    this.result = response.result.mapping;
-  });
+      this.result = response.result.mapping;
+    });
 });
 
 Then(/^I change the user mapping$/, function () {
-  return this.api.updateUserMapping().then((body) => {
-    if (body.error !== null) {
-      throw new Error(body.error.message);
-    }
-  });
+  return this.api.updateUserMapping()
+    .then(body => {
+      if (body.error !== null) {
+        throw new Error(body.error.message);
+      }
+    });
 });
 
-When(
-  /^I (can't )?create a (restricted )?user "(.*?)" with id "(.*?)"$/,
-  { timeout: 20000 },
-  function (not, isRestricted, user, id, callback) {
-    var userObject = this.users[user],
-      method;
+When(/^I (can't )?create a (restricted )?user "(.*?)" with id "(.*?)"$/, { timeout: 20000 }, function (not, isRestricted, user, id, callback) {
+  var
+    userObject = this.users[user],
+    method;
 
-    if (isRestricted) {
-      method = "createRestrictedUser";
-    } else {
-      method = "createUser";
-    }
+  if (isRestricted) {
+    method = 'createRestrictedUser';
+  }
+  else {
+    method = 'createUser';
+  }
 
-    id = this.idPrefix + id;
+  id = this.idPrefix + id;
 
-    this.api[method](userObject, id)
-      .then((body) => {
-        if (body.error) {
-          if (not) {
-            return callback();
-          }
-          return callback(new Error(body.error.message));
-        }
-
+  this.api[method](userObject, id)
+    .then(body => {
+      if (body.error) {
         if (not) {
-          return callback(new Error(JSON.stringify(body)));
+          return callback();
         }
-        return callback();
-      })
-      .catch((error) => callback(not ? null : error));
-  }
-);
+        return callback(new Error(body.error.message));
+      }
 
-When(
-  /^I create the first admin with id "(.*?)"( and reset profiles and roles)?$/,
-  { timeout: 20000 },
-  function (id, reset, callback) {
-    var userObject = this.users.useradmin;
+      if (not) {
+        return callback(new Error(JSON.stringify(body)));
+      }
+      return callback();
+    })
+    .catch(error => callback(not ? null : error));
+});
 
-    delete userObject.content.profileIds;
-    id = this.idPrefix + id;
+When(/^I create the first admin with id "(.*?)"( and reset profiles and roles)?$/, { timeout: 20000 }, function (id, reset, callback) {
+  var
+    userObject = this.users.useradmin;
 
-    this.api
-      .createFirstAdmin(userObject, id, reset)
-      .then((body) => {
-        if (body.error) {
-          return callback(new Error(body.error.message));
-        }
+  delete userObject.content.profileIds;
+  id = this.idPrefix + id;
 
-        return callback();
-      })
-      .catch((error) => callback(error));
-  }
-);
+  this.api.createFirstAdmin(userObject, id, reset)
+    .then(body => {
+      if (body.error) {
+        return callback(new Error(body.error.message));
+      }
 
-Then(
-  /^I am able to get the user "(.*?)"(?: matching {(.*)})?$/,
-  function (id, match) {
-    id = this.idPrefix + id;
+      return callback();
+    })
+    .catch(error => callback(error));
+});
 
-    return this.api.getUser(id).then((body) => {
+
+Then(/^I am able to get the user "(.*?)"(?: matching {(.*)})?$/, function (id, match) {
+  id = this.idPrefix + id;
+
+  return this.api.getUser(id)
+    .then(body => {
       if (body.error) {
         throw new Error(body.error.message);
       }
@@ -98,88 +99,60 @@ Then(
       if (match) {
         match = match.replace(/#prefix#/g, this.idPrefix);
 
-        const matchingObject = JSON.parse("{" + match + "}");
+        const matchingObject = JSON.parse('{' + match + '}');
 
-        if (!_.matches(matchingObject)(body.result)) {
-          throw new Error(
-            "Error: " +
-              JSON.stringify(body.result) +
-              " does not match {" +
-              match +
-              "}"
-          );
+        if (! _.matches(matchingObject)(body.result)) {
+          throw new Error('Error: ' + JSON.stringify(body.result) + ' does not match {' + match + '}');
         }
       }
     });
-  }
-);
+});
 
-Then(
-  /^I search for {(.*?)} and find (\d+) users(?: matching {(.*?)})?$/,
-  function (query, count, match, callback) {
-    if (count) {
-      count = parseInt(count);
+Then(/^I search for {(.*?)} and find (\d+) users(?: matching {(.*?)})?$/, function (query, count, match, callback) {
+  if (count) {
+    count = parseInt(count);
+  }
+
+  let run = (cb) => {
+    query = query.replace(/#prefix#/g, this.idPrefix);
+
+    this.api.searchUsers(JSON.parse('{' + query + '}'))
+      .then(body => {
+        if (body.error) {
+          return cb(new Error(body.error.message));
+        }
+
+        if (count !== body.result.total) {
+          return cb(new Error('Expected ' + count + ' results, got ' + body.result.total + '\n' + JSON.stringify(body.result.hits)));
+        }
+
+        if (match) {
+          match = match.replace(/#prefix#/g, this.idPrefix);
+
+          const matchFunc = _.matches(JSON.parse('{' + match + '}'));
+
+          if (! body.result.hits.every(hit => matchFunc(hit))) {
+            return cb(new Error('Error: ' + JSON.stringify(body.result.hits) + ' does not match ' + match));
+          }
+        }
+
+        cb(null);
+      })
+      .catch(error => cb(error));
+  };
+
+  async.retry({ times: 40, interval: 50 }, run, err => {
+    if (err) {
+      return callback(new Error(err.message));
     }
 
-    let run = (cb) => {
-      query = query.replace(/#prefix#/g, this.idPrefix);
-
-      this.api
-        .searchUsers(JSON.parse("{" + query + "}"))
-        .then((body) => {
-          if (body.error) {
-            return cb(new Error(body.error.message));
-          }
-
-          if (count !== body.result.total) {
-            return cb(
-              new Error(
-                "Expected " +
-                  count +
-                  " results, got " +
-                  body.result.total +
-                  "\n" +
-                  JSON.stringify(body.result.hits)
-              )
-            );
-          }
-
-          if (match) {
-            match = match.replace(/#prefix#/g, this.idPrefix);
-
-            const matchFunc = _.matches(JSON.parse("{" + match + "}"));
-
-            if (!body.result.hits.every((hit) => matchFunc(hit))) {
-              return cb(
-                new Error(
-                  "Error: " +
-                    JSON.stringify(body.result.hits) +
-                    " does not match " +
-                    match
-                )
-              );
-            }
-          }
-
-          cb(null);
-        })
-        .catch((error) => cb(error));
-    };
-
-    async.retry({ times: 40, interval: 50 }, run, (err) => {
-      if (err) {
-        return callback(new Error(err.message));
-      }
-
-      return callback();
-    });
-  }
-);
+    return callback();
+  });
+});
 
 Then(/^I replace the user "(.*?)" with data {(.*?)}$/, function (id, data) {
-  return this.api
-    .replaceUser(this.idPrefix + id, JSON.parse("{" + data + "}"))
-    .then((body) => {
+  return this.api.replaceUser(this.idPrefix + id, JSON.parse('{' + data + '}'))
+    .then(body => {
       if (body.error) {
         throw new Error(body.error.message);
       }
@@ -187,66 +160,60 @@ Then(/^I replace the user "(.*?)" with data {(.*?)}$/, function (id, data) {
 });
 
 Then(/^I revoke all tokens of the user "(.*?)"$/, function (id) {
-  return this.api.revokeTokens(this.idPrefix + id).then((body) => {
-    if (body.error) {
-      throw new Error(body.error.message);
-    }
-  });
+  return this.api.revokeTokens(this.idPrefix + id)
+    .then(body => {
+      if (body.error) {
+        throw new Error(body.error.message);
+      }
+    });
 });
 
 Then(/^I delete the user "(.*?)"$/, function (id) {
-  return this.api.deleteUser(this.idPrefix + id, true).then((body) => {
-    if (body.error) {
-      throw new Error(body.error.message);
-    }
-  });
+  return this.api.deleteUser(this.idPrefix + id, true)
+    .then(body => {
+      if (body.error) {
+        throw new Error(body.error.message);
+      }
+    });
 });
 
-Then(
-  /^I am getting the current user, which matches \{(.*?)}$/,
-  function (match) {
-    return this.api.getCurrentUser().then((body) => {
+Then(/^I am getting the current user, which matches \{(.*?)}$/, function (match) {
+  return this.api.getCurrentUser()
+    .then(body => {
       if (body.error) {
         throw new Error(body.error.message);
       }
 
       match = match.replace(/#prefix#/g, this.idPrefix);
-      if (!_.matches(JSON.parse("{" + match + "}"))(body.result)) {
-        throw new Error(
-          "Expected: " + match + "\nGot: " + JSON.stringify(body.result)
-        );
+      if (! _.matches(JSON.parse('{' + match + '}'))(body.result)) {
+        throw new Error('Expected: ' + match + '\nGot: ' + JSON.stringify(body.result));
       }
     });
-  }
-);
+});
 
-Then(
-  /^I'm ?(not)* able to find rights for user "([^"]*)"$/,
-  function (not, id, callback) {
-    id = this.idPrefix + id;
+Then(/^I'm ?(not)* able to find rights for user "([^"]*)"$/, function (not, id, callback) {
+  id = this.idPrefix + id;
 
-    this.api
-      .getUserRights(id)
-      .then((body) => {
-        if (body.error) {
-          return callback(new Error(body.error.message));
-        }
+  this.api.getUserRights(id)
+    .then(body => {
+      if (body.error) {
+        return callback(new Error(body.error.message));
+      }
 
-        if (not) {
-          return callback(new Error(`User with id ${id} exists`));
-        }
+      if (not) {
+        return callback(new Error(`User with id ${id} exists`));
+      }
 
-        callback();
-      })
-      .catch((error) => callback(not ? null : error));
-  }
-);
+      callback();
+    })
+    .catch(error => callback(not ? null : error));
+});
 
-Then(
-  /^I'm ?(not)* able to check the token for current user/,
-  function (not, callback) {
-    this.api.checkToken(this.currentUser.token).then((body) => {
-      if (!body.result.valid) {
+Then(/^I'm ?(not)* able to check the token for current user/, function (not, callback) {
+
+  this.api.checkToken(this.currentUser.token)
+    .then(body => {
+      if (! body.result.valid) {
         if (not) {
           return callback();
         }
@@ -254,49 +221,47 @@ Then(
       }
       callback();
     });
-  }
-);
+});
 
 Then(/^I'm able to find my rights$/, function () {
-  return this.api.getMyRights().then((body) => {
-    if (body.error) {
-      throw new Error(body.error.message);
-    }
-  });
+  return this.api.getMyRights()
+    .then(body => {
+      if (body.error) {
+        throw new Error(body.error.message);
+      }
+    });
 });
 
 Given(/^A scrolled search on users$/, function () {
   this.scrollId = null;
 
-  return this.api.searchUsers({}, { scroll: "2s" }).then((response) => {
-    if (response.error) {
-      throw new Error(response.error.message);
-    }
+  return this.api.searchUsers({}, { scroll: '2s' })
+    .then(response => {
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
 
-    if (!response.result.scrollId) {
-      throw new Error("No scrollId returned by the searchProfile query");
-    }
+      if (! response.result.scrollId) {
+        throw new Error('No scrollId returned by the searchProfile query');
+      }
 
-    this.scrollId = response.result.scrollId;
-  });
+      this.scrollId = response.result.scrollId;
+    });
 });
 
 Then(/^I am able to perform a scrollUsers request$/, function () {
-  if (!this.scrollId) {
-    throw new Error("No previous scrollId found");
+  if (! this.scrollId) {
+    throw new Error('No previous scrollId found');
   }
 
-  return this.api.scrollUsers(this.scrollId).then((response) => {
-    if (response.error) {
-      throw new Error(response.error.message);
-    }
+  return this.api.scrollUsers(this.scrollId)
+    .then(response => {
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
 
-    if (
-      ["hits", "scrollId", "total"].some(
-        (prop) => response.result[prop] === undefined
-      )
-    ) {
-      throw new Error("Incomplete scroll results");
-    }
-  });
+      if (['hits', 'scrollId', 'total'].some(prop => response.result[prop] === undefined)) {
+        throw new Error('Incomplete scroll results');
+      }
+    });
 });
