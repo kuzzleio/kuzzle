228482af41b822c0e064286083811c9aca95e532
rolljee <n.riquelmebareiro@gmail.com>
2024-03-13 11:38:32 +0100

    fix(settings): elasticsearch default setting on imports collection

--- lib/config/default.config.ts
+++ lib/config/default.config.ts
@@ -428,8 +428,8 @@ const defaultConfig: KuzzleConfiguration = {
           imports: {
             settings: {
               // @deprecated : replace undefined by 1
-              number_of_shards: undefined,
-              number_of_replicas: undefined,
+              number_of_shards: 1,
+              number_of_replicas: 1,
             },
             mappings: {
               dynamic: "strict",
