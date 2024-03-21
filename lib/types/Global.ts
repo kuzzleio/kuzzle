b4661b9e1e2d8c169d93e7a17f030338875faed0
Florian Maunier <fmaunier@kuzzle.io>
2024-03-21 17:49:07 +0100

    fix(global): fix global types

--- lib/types/Global.ts
+++ lib/types/Global.ts
@@ -1,19 +1,22 @@
 import { Backend } from "../core/backend";
+import { Kuzzle } from "../kuzzle";
 
-/* eslint-disable @typescript-eslint/no-namespace */
+/**
+ * This file contains global type declarations for Kuzzle.
+ * We need to use `var` so Typescript extends the globalThis type.
+ * See https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#type-checking-for-globalthis
+ */
+
+/* eslint-disable no-var, vars-on-top */
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      kuzzle: any;
-      app: Backend;
-      NODE_ENV: string;
-    }
-  }
+  var app: Backend;
+  var kuzzle: Kuzzle;
+  var NODE_ENV: string;
 }
 
 global.NODE_ENV = process.env.NODE_ENV;
 
-export {};
+export { };
 
-/* eslint-enable @typescript-eslint/no-namespace */
+/* eslint-enable no-var, vars-on-top*/
