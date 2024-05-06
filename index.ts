8b6e4e74f888c59d2d9d485a11951f513b9c63aa
Florian Maunier <fmaunier@kuzzle.io>
2024-05-06 13:50:11 +0200

    feat(index): export Store and ObjectRepository

--- index.ts
+++ index.ts
@@ -1,21 +1,25 @@
-export * from './lib/core/backend';
+export * from "./lib/core/backend";
 
-export * from './lib/types';
+export * from "./lib/types";
 
-export * from './lib/core/plugin/pluginContext';
+export * from "./lib/core/plugin/pluginContext";
 
-export * from './lib/core/shared/sdk/embeddedSdk';
+export * from "./lib/core/shared/sdk/embeddedSdk";
 
-export * from './lib/api/request';
+export * from "./lib/api/request";
 
-export * from './lib/kerror/errors';
+export * from "./lib/kerror/errors";
 
-export * from './lib/util/mutex';
+export * from "./lib/util/mutex";
 
-export * from './lib/util/Inflector';
+export * from "./lib/util/Inflector";
 
-export { NameGenerator } from './lib/util/name-generator';
+export { NameGenerator } from "./lib/util/name-generator";
 
-export * from 'kuzzle-sdk';
+export * from "kuzzle-sdk";
 
-export * from './lib/core/shared/KoncordeWrapper';
+export * from "./lib/core/shared/KoncordeWrapper";
+
+export * from "./lib/core/shared/ObjectRepository";
+
+export * from "./lib/core/shared/store";
