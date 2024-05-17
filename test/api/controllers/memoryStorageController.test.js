2d9285d37569afd80e9a6d25c7c8c4c532957c6b
Juiced66 <juliendecosse30630@gmail.com>
2024-05-17 10:43:54 +0200

    chore: expose mset bug

--- test/api/controllers/memoryStorageController.test.js
+++ test/api/controllers/memoryStorageController.test.js
@@ -1097,6 +1097,7 @@ describe("MemoryStorageController", () => {
             { key: "key1", value: "value1" },
             { key: "key2", value: "value2" },
             { key: "key3", value: "value3" },
+            { key: "key4", value: 0 }
           ],
         },
       });
