47460b7da9ab774e1dbac0de6df91bb4d0cfd67d
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2025-02-14 10:43:01 +0100

    fix: remove some tests that cannot be used in a concurrency context

--- docker/scripts/start-kuzzle-test.ts
+++ docker/scripts/start-kuzzle-test.ts
@@ -491,38 +491,6 @@ app.controller.register("tests", {
         });
       },
     },
-    simulateOutage: {
-      http: [{ verb: "get", path: "/tests/simulate-outage" }],
-      handler: async (request: KuzzleRequest) => {
-        const outageType = request.getString("type");
-
-        switch (outageType) {
-          case "overload":
-            global.kuzzle.funnel.overloaded = true;
-            break;
-          case "nodeNotStarted":
-            global.kuzzle.state = 1;
-            break;
-          case "notEnoughNodes":
-            global.kuzzle.state = 4;
-            break;
-          case "shutdown":
-            global.kuzzle.state = 3;
-            break;
-          default:
-            break;
-        }
-        return;
-      },
-    },
-    clearOutage: {
-      http: [{ verb: "get", path: "/tests/clear-outage" }],
-      handler: async () => {
-        global.kuzzle.funnel.overloaded = false;
-        global.kuzzle.state = 2;
-        return;
-      },
-    },
   },
 });
 
